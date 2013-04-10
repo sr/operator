8399dec7fb2658301b05e9d025d708d5dbb8aa34
ehberg <eric@berg>
2013-04-10 12:31:06 -0400

    solutions bot

---
+++ startSolutionsBot.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Irc connection settings
+export HUBOT_IRC_SERVER="supportbot.pardot.com"
+export HUBOT_IRC_ROOMS="#solutions-team"
+export HUBOT_IRC_PORT="7000"
+export HUBOT_IRC_USESSL="true"
+export HUBOT_IRC_NICK="SolutionsBot"
+export HUBOT_IRC_SERVER_FAKE_SSL="true"
+
+# Ports to listen on
+export HUBOT_CAT_PORT="8000"
+export PORT="8001"
+
+# Database settings
+export DB_USER="root"
+export DB_PASSWORD="poop"
+export QUOTE_DATABASE="support_quotes"
+
+export HUBOT_LOG_LEVEL="debug"  # This helps to see what Hubot is doing
+export HUBOT_IRC_DEBUG="true"
+
+# Supportbot settings
+export SUPPORTBOT_ENABLED="false"
+export SUPPORTBOT_EXECUTABLE="php /var/www/supportq/symfony"
+
+# KPIBot settings
+export KPIBOT_ENABLED="false"
+
+# Release tracker
+export RELEASE_TRACKING_ENABLED="false"
+
+# Fire tracker
+export FIRE_RECORDING_ENABLED="false"
+
+# Finally run:
+./bin/hubot -a irc
