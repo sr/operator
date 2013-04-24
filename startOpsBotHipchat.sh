c7de8a7882fa442d45de57de7732d6452964f0e0
ehberg <eric@berg>
2013-04-24 13:39:47 -0400

    adding new opsbot

---
+++ startOpsBotHipchat.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+export HUBOT_HIPCHAT_JID="45727_306047@chat.hipchat.com"
+export HUBOT_HIPCHAT_NAME="Ops Bot"
+export HUBOT_HIPCHAT_PASSWORD="!Parbot.2013!"
+export HUBOT_HIPCHAT_ROOMS="45727_ops@conf.hipchat.com"
+export HUBOT_HIPCHAT_API_KEY='33965558fc3c09972af5a4e3edf510'
+#export HUBOT_HIPCHAT_DEBUG="true"
+
+# Ports to listen on
+export HUBOT_CAT_PORT="7890"
+export PORT="7894"
+
+# Database settings
+export DB_USER="root"
+export DB_PASSWORD="poop"
+export QUOTE_DATABASE="pardot_quotes"
+export RELEASE_DATABASE="pardot_releases"
+export KPI_DATABASE="pardot_kpis"
+
+export HUBOT_LOG_LEVEL="debug"  # This helps to see what Hubot is doing
+
+# Set the bot type for permission related items
+export BOT_TYPE="parbot"
+
+# Finally run:
+./bin/hubot -a hipchat
