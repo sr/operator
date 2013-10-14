2b93b6bde84bfcbc1d5a519e35d07653f4ed5103
Shawn Veader <shawn@veader.org>
2013-10-14 14:50:59 -0400

    adding capistrano for deployment

---
+++ config/deploy.rb
@@ -0,0 +1,37 @@
+require 'bundler/capistrano'
+
+default_run_options[:pty] = true  # Must be set for the password prompt from git to work
+ssh_options[:forward_agent] = true
+
+set :application, "canoe"
+set :repository,  "git@github.com:Pardot/#{application}.git"
+set :deploy_to,   "/var/#{application}"
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "canoe.test.pardot.com"
+role :app, "canoe.test.pardot.com"
+role :db,  "canoe.test.pardot.com", :primary => true
+
+# fun rbenv setup
+set :default_environment, {
+  'RBENV_ROOT' => '/usr/local/rbenv',
+  'PATH' => '/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH'
+}
+set :bundle_flags, '--deployment --quiet --binstubs --shebang ruby-local-exec'
+
+after 'deploy:restart', 'deploy:cleanup'
+# after 'deploy:create_symlink', 'mesh:link_envvars'
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# passenger setup
+namespace :deploy do
+  task :start do ; end
+  task :stop  do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
