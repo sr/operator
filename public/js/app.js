23c5ef2beb7c99a85596c2901ccddd3112b3acd9
Stuart Lawder <stuart.lawder@pardot.com>
2013-10-17 10:45:18 -0400

    Fix responsive menu js error

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,15 @@ function toggleMenu() {
   }
 }
 
+$.extend($.easing, {
+    easeInOutExpo: function (x, t, b, c, d) {
+        if (t==0) return b;
+        if (t==d) return b+c;
+        if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+        return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+    }
+});
+
 $(function(){
   $nav = $('aside[role="navigation"]');
   $('[role="banner"] .navbar-toggle').click(toggleMenu);
