e61e2abbbc7f5da2aad33d085db4bf641af20ecd
Shawn Veader <shawn@veader.org>
2014-01-02 10:25:35 -0500

    guess sudo call is killing our env pass

--- lib/models/deploy_target.rb
+++ lib/models/deploy_target.rb
@@ -113,8 +113,7 @@ class DeployTarget < ActiveRecord::Base
       cmd_pieces << "sh batch/devResetAll.sh"
     elsif self.name == "test"
       cmd_pieces << "cd ../;" # back up a dir...
-      cmd_pieces << "export CANOE_USER=#{options[:user].email};"
-      cmd_pieces << "sudo sh update-test-reset"
+      cmd_pieces << "sudo sh CANOE_USER=#{options[:user].email} update-test-reset"
     else
       return
     end
