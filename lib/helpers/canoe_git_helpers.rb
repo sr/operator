085a4b305b94939f158e73b475b8a00f3c01f8fc
Shawn Veader <shawn@veader.org>
2014-09-03 11:49:49 -0400

    tweaking on tags interface to hopefully speed it up

--- lib/helpers/canoe_git_helpers.rb
+++ lib/helpers/canoe_git_helpers.rb
@@ -1,10 +1,10 @@
 module Canoe
   module GitHelpers
     # ----------------------------------------------------------------------
-    def tags_for_current_repo(count=50)
-      tags = Octokit.tags(current_repo.full_name)
-      tags = tags.sort_by { |t| t.name.gsub(/^build/,"").to_i }.reverse
-      tags[0,count] # we only really care about the most recent X?
+    def tags_for_current_repo(count=30)
+      tags = Octokit.tags(current_repo.full_name, per_page: count)
+      # tags = tags.sort_by { |t| t.name.gsub(/^build/,"").to_i }.reverse
+      tags
     end
 
     def branches_for_current_repo
