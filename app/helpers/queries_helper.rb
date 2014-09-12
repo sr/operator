0c23a839ad3da0b48edda8e437540112003a17f4
Jan Ulrich <julrich@salesforce.com>
2014-09-12 13:02:43 -0700

    Follow style guidelines

--- app/helpers/queries_helper.rb
+++ app/helpers/queries_helper.rb
@@ -1,9 +1,10 @@
 module QueriesHelper
   def query_form_path
-    @query.account? ? 
-      (@query.id ? account_query_path(@query.account, @query) : account_queries_path(@query.account))
-    : 
-      (@query.id ? global_query_path(@query) : global_queries_path)
+    if @query.account?
+      @query.id ? account_query_path(@query.account, @query) : account_queries_path(@query.account)
+    else
+      @query.id ? global_query_path(@query) : global_queries_path
+    end
   end
 
   def is_active?(type, value, output = " active")
