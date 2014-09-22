ffefb5bab2b40d2c5b601b1a90b3cc9f85e189a9
Jan Ulrich <julrich@salesforce.com>
2014-09-22 12:03:35 -0700

    Account restriction test converage

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,9 +2,9 @@ if Rails.env.test?
   ENV['DB_D_GLOBAL'] = "global_test"
   ENV['DB_D_SHARD1'] = "shard1_test"
   ENV['DB_D_SHARD2'] = "shard2_test"
-  ENV['DB_S_GLOBAL'] = 'sqlite3://db/pardot_global.sqlite3'
-  ENV['DB_S_SHARD1'] = 'sqlite3://db/pardot_shard1.sqlite3'
-  ENV['DB_S_SHARD2'] = 'sqlite3://db/pardot_shard2.sqlite3'
+  ENV['DB_S_GLOBAL'] = "global_test"
+  ENV['DB_S_SHARD1'] = "shard1_test"
+  ENV['DB_S_SHARD2'] = "shard2_test"
 else
   ENV['DB_D_GLOBAL'] = 'mysql2://pardot:pardot@localhost:3306/pardot_global'
   ENV['DB_D_SHARD1'] = 'mysql2://pardot:pardot@localhost:3306/pardot_shard1'
--- test/models/query_test.rb
+++ test/models/query_test.rb
@@ -1,6 +1,19 @@
 require 'test_helper'
 
 class QueryTest < ActiveSupport::TestCase
+  setup do 
+    @gquery = Query.new(
+      database: DB::Global,
+      datacenter: DC::Dallas,
+    )
+    @aquery = Query.new(
+      database: DB::Account,
+      datacenter: DC::Dallas,
+      account_id: 1,
+    )
+    @limit = "LIMIT 10"
+  end
+
   # Extract_table_name
   test "simple lookup" do
     extract_table_name_test("account", "SELECT * FROM `account`")
@@ -40,5 +53,31 @@ class QueryTest < ActiveSupport::TestCase
   test "only backticks work as quotes" do
     assert_raises(ParseError) { SQLParser::Parser.new.scan_str("SELECT \"email_ip_id\" FROM `account` WHERE `id` = 1 LIMIT 10") }
     assert_raises(ParseError) { SQLParser::Parser.new.scan_str("SELECT 'email_ip_id' FROM `account` WHERE `id` = 1 LIMIT 10") }
+  end
+
+  # Parse tests
+  test "Should not add account restrictions to global db" do
+    sql = "SELECT * FROM `account`"
+    assert_equal "#{sql} #{@limit}", @gquery.parse(sql).to_sql
+  end
+
+  test "Should not replace LIMIT already given" do
+    sql = "SELECT * FROM `account` LIMIT 20"
+    assert_equal sql, @gquery.parse(sql).to_sql
+  end
+
+  test "Should add account restrictions to account db" do
+    sql = "SELECT * FROM `account`"
+    assert_equal "#{sql} WHERE `account`.`id` = #{@aquery.account_id} #{@limit}", @aquery.parse(sql).to_sql
+  end
+
+  test "Should add account restrictions to account db if they have account_id column" do
+    sql = "SELECT * FROM `bitly_url`"
+    assert_equal "#{sql} WHERE `account_id` = #{@aquery.account_id} #{@limit}", @aquery.parse(sql).to_sql
+  end
+
+  test "Should only add account restrictions if account_id column exists" do
+    sql = "SELECT * FROM `deleted_object`"
+    assert_equal "#{sql} #{@limit}", @aquery.parse(sql).to_sql
   end
 end
