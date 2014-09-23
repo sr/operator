8501c5623e5e3e5b8dbae4c778a404d3b5f2cda8
Jan Ulrich <julrich@salesforce.com>
2014-09-23 12:49:53 -0700

    Advanced SQL

--- test/models/query_test.rb
+++ test/models/query_test.rb
@@ -43,7 +43,7 @@ class QueryTest < ActiveSupport::TestCase
   end
 
   test "select field query with e in front" do
-    assert_raises(ParseError) { SQLParser::Parser.new.scan_str("SELECT email_ip_id FROM `account` WHERE `id` = 1 LIMIT 10") }
+    assert SQLParser::Parser.new.scan_str("SELECT email_ip_id FROM `account` WHERE `id` = 1 LIMIT 10")
   end
 
   test "quoted select field query with e in front" do
@@ -79,5 +79,22 @@ class QueryTest < ActiveSupport::TestCase
   test "Should only add account restrictions if account_id column exists" do
     sql = "SELECT * FROM `deleted_object`"
     assert_equal "#{sql} #{@limit}", @aquery.parse(sql).to_sql
+  end
+
+  # Advanced tests
+  test "Date test" do
+    sql = 'select email_id from visitor_activity where type=12 and created_at<DATE "2014-07-23"'
+    puts SQLParser::Parser.new.scan_str(sql).to_sql
+    assert SQLParser::Parser.new.scan_str(sql)
+  end
+
+  test "Advanced Join" do
+    sql = 'select va.email_id,e.list_email_id,e.name,va.prospect_id,p.email,va.type,va.created_at from visitor_activity va left join prospect p on p.id=va.prospect_id left join email e on e.id=va.email_id where va.type=12 and va.created_at<"2014-07-23" and va.created_at>"2014-07-22"'
+    assert SQLParser::Parser.new.scan_str(sql)
+  end
+
+  test "Function calls" do
+    sql = 'select prospect_id from piListxProspect where (listx_id=1 or listx_id=2) and account_id=1 and is_mailable=1 order by rand() limit 10'
+    assert SQLParser::Parser.new.scan_str(sql)
   end
 end
