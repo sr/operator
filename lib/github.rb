abb4a8b73344c4c122d868429b27eb4d1008c3a5
Nate Schmoll <nschmoll@salesforce.com>
2015-06-03 01:47:36 -0700

    another method of achieving the same thing

--- lib/github.rb
+++ lib/github.rb
@@ -2,7 +2,9 @@ require "octokit"
 
 Octokit.configure do |c|
   c.api_endpoint = 'https://git.dev.pardot.com/api/v3'
-  c.faraday_config = { ssl: { verify: false } }
+  c.faraday_config do |f|
+    f.instance_variable_set(:@ssl, { verify: false })
+  end
   c.login = ENV['GITHUB_USER']
   c.password = ENV['GITHUB_PASSWORD']
 end
