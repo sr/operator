69fe6320122d4955995f14f9ac93c529b4ab98f5
Nate Schmoll <nschmoll@salesforce.com>
2015-06-03 01:43:01 -0700

    testing

--- lib/github.rb
+++ lib/github.rb
@@ -2,6 +2,7 @@ require "octokit"
 
 Octokit.configure do |c|
   c.api_endpoint = 'https://git.dev.pardot.com/api/v3'
+  c.faraday_config = { ssl: { verify: false } }
   c.login = ENV['GITHUB_USER']
   c.password = ENV['GITHUB_PASSWORD']
 end
