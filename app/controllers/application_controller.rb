b8ff5f7c4e6c0130a849a943c0e668541aafa945
Andy Lindeman <alindeman@salesforce.com>
2015-07-30 17:41:58 -0400

    Starts spiking out authentication routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  before_filter :require_oauth_authentication
+
+  private
+  def require_oauth_authentication
+    redirect_to oauth_url unless current_user.present?
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = session[:user_id] && AuthUser.find_by_id(session[:user_id])
+  end
+  helper_method :current_user
+
+  def current_user=(user)
+    @current_user = user
+    session[:user_id] = user.id
+  end
+
+  def oauth_url
+    case Rails.env
+    when "development" then "/auth/developer"
+    else "/auth/google"
+    end
+  end
 end
