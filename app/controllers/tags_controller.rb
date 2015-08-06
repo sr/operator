88a223a147b09363f578b114d45df215da78f0ed
Andy Lindeman <alindeman@salesforce.com>
2015-08-06 18:11:44 -0400

    Ports over the tag list view

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,11 @@
 class TagsController < ApplicationController
   before_filter :require_repo
 
+  def index
+    @tags = current_repo.tags
+    render template: "repos/show"
+  end
+
   def latest
     tag = current_repo.latest_tag
     @latest_tag = current_repo.tag(tag.name)
