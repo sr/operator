4e21cfbcbd52b1958de7f13ca3194720bb816c84
Andy Lindeman <alindeman@salesforce.com>
2015-08-11 22:47:04 -0400

    The callback might be invoked as a GET or POST

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new] do
     delete :destroy, on: :collection
   end
+  get "/auth/:provider/callback", to: "sessions#create"
   post "/auth/:provider/callback", to: "sessions#create"
   get "/auth/failure", to: "sessions#failure"
 
