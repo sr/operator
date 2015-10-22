1fd912913ef36c2a9ac5c17a3226d7ecb127a3ff
Andy Lindeman <alindeman@salesforce.com>
2015-10-22 09:42:28 -0400

    Temporarily talks to Artifactory via proxy:3128 in production

--- config/initializers/artifactory.rb
+++ config/initializers/artifactory.rb
@@ -3,4 +3,10 @@ Artifactory.configure do |config|
   config.username = ENV['ARTIFACTORY_USERNAME']
   config.password = ENV['ARTIFACTORY_API_KEY']
   config.ssl_verify = true
+
+  # Temporary while we debug comm issues with Artifactory
+  if Rails.env.production?
+    config.proxy_address = 'proxy'
+    config.proxy_port = 3128
+  end
 end
