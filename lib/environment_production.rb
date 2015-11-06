c4d8dd8719ba25dd972020acd0556f99d8c2a59f
Andy Lindeman <alindeman@salesforce.com>
2015-11-06 11:27:59 -0500

    BREAD-341: Only perform job restarts in staging at this point

--- lib/environment_production.rb
+++ lib/environment_production.rb
@@ -1,8 +1,6 @@
 require "environment_base"
 
 class EnvironmentProduction < EnvironmentBase
-  restart_task :restart_pardot_jobs, only: :pardot
-
   def short_name
     "prod"
   end
--- lib/environment_staging.rb
+++ lib/environment_staging.rb
@@ -1,5 +1,5 @@
 require "environment_base"
 
 class EnvironmentStaging < EnvironmentBase
-
+  restart_task :restart_pardot_jobs, only: :pardot
 end
