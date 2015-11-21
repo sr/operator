893f2c2df8f95a4e0ac853a515497c837f96bede
Simon Rozet <me@simonrozet.com>
2015-11-21 18:06:09 +0000

    reorganize commands etc

--- src/operator/operator.go
+++ src/operator/operator.go
@@ -1,19 +1,18 @@
-package main
+package operator
 
 import (
-	"log"
 	"net"
 
 	"github.com/sr/operator/src/gcloud"
 	"google.golang.org/grpc"
 )
 
-func main() {
+func Listen() error {
 	listener, err := net.Listen("tcp", ":3000")
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		return err
 	}
 	server := grpc.NewServer()
 	gcloud.RegisterGCloudServiceServer(server, gcloud.NewAPIServer())
-	server.Serve(listener)
+	return server.Serve(listener)
 }
