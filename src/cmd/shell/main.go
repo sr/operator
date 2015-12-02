270fe7bf5fe943ea9eeab5d152ced9dab252b7bc
Simon Rozet <me@simonrozet.com>
2015-12-02 19:37:46 +0000

    merge gcloud command into shell

--- src/cmd/shell/main.go
+++ src/cmd/shell/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/sr/operator/src/gcloud"
 	"github.com/sr/operator/src/papertrail"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
@@ -16,17 +18,52 @@ func main() {
 	}
 	defer conn.Close()
 
-	papertrailClient := papertrail.NewPapertrailServiceClient(conn)
-	response, err := papertrailClient.Search(
-		context.Background(),
-		&papertrail.SearchRequest{
-			Query: "ssh",
-		},
-	)
-	if err != nil {
-		log.Fatal(err)
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: shell <service> \n")
+		os.Exit(1)
 	}
-	for _, event := range response.LogEvents {
-		fmt.Println(event.LogMessage)
+
+	service := os.Args[1]
+	switch service {
+	case "gcloud":
+		gcloudClient := gcloud.NewGCloudServiceClient(conn)
+		gcloudListInstancesResponse, err := gcloudClient.ListInstances(
+			context.Background(),
+			&gcloud.ListInstancesRequest{
+				ProjectId: "dev-europe-west1",
+			},
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(gcloudListInstancesResponse)
+
+		gcloudListOperationsResponse, err := gcloudClient.ListOperations(
+			context.Background(),
+			&gcloud.ListOperationsRequest{
+				ProjectId: "dev-europe-west1",
+			},
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(gcloudListOperationsResponse)
+	case "logs":
+		papertrailClient := papertrail.NewPapertrailServiceClient(conn)
+		response, err := papertrailClient.Search(
+			context.Background(),
+			&papertrail.SearchRequest{
+				Query: "ssh",
+			},
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, event := range response.LogEvents {
+			fmt.Println(event.LogMessage)
+		}
+	default:
+		fmt.Fprintf(os.Stderr, "no such service: %s\n", "error")
+		os.Exit(1)
 	}
 }
