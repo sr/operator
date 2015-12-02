d81216e410d548b56872e5e6efd92eb531080c01
Simon Rozet <me@simonrozet.com>
2015-12-02 19:20:54 +0000

    papertrail search service is working

--- src/papertrail/api_server.go
+++ src/papertrail/api_server.go
@@ -1,18 +1,40 @@
 package papertrail
 
-import "golang.org/x/net/context"
+import (
+	gopapertrail "github.com/sourcegraph/go-papertrail/papertrail"
+	"golang.org/x/net/context"
+)
 
-type apiServer struct{}
+type apiServer struct {
+	client *gopapertrail.Client
+}
 
-func newAPIServer() *apiServer {
-	return &apiServer{}
+func newAPIServer(client *gopapertrail.Client) *apiServer {
+	return &apiServer{client}
 }
 
 func (s *apiServer) Search(
 	ctx context.Context,
 	request *SearchRequest,
 ) (*SearchResponse, error) {
+	options := gopapertrail.SearchOptions{
+		Query: request.Query,
+	}
+	response, _, err := s.client.Search(options)
+	if err != nil {
+		return nil, err
+	}
+
 	var logEvents []*LogEvent
+	for _, event := range response.Events {
+		logEvents = append(logEvents, &LogEvent{
+			Id:         event.ID,
+			Source:     event.SourceName,
+			Program:    *event.Program,
+			LogMessage: event.Message,
+		})
+	}
+
 	return &SearchResponse{
 		LogEvents: logEvents,
 	}, nil
--- src/papertrail/papertrail.go
+++ src/papertrail/papertrail.go
@@ -1,5 +1,13 @@
 package papertrail
 
-func NewAPIServer() PapertrailServiceServer {
-	return newAPIServer()
+import gopapertrail "github.com/sourcegraph/go-papertrail/papertrail"
+
+type Env struct {
+	APIToken string `env:"PAPERTRAIL_API_TOKEN,required"`
+}
+
+func NewAPIServer(env *Env) PapertrailServiceServer {
+	token := &gopapertrail.TokenTransport{Token: env.APIToken}
+	client := gopapertrail.NewClient(token.Client())
+	return newAPIServer(client)
 }
