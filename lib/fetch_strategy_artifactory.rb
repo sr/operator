98b0cae4e8e9d5199c88e880615e07922e0db342
Andy Lindeman <alindeman@salesforce.com>
2015-12-08 11:53:10 -0500

    BREAD-422: If a proxy is used, talk over HTTP

--- lib/fetch_strategy_artifactory.rb
+++ lib/fetch_strategy_artifactory.rb
@@ -10,15 +10,21 @@ class FetchStrategyArtifactory < FetchStrategyBase
     self.environment = environment
 
     Artifactory.configure do |config|
-      config.endpoint = 'https://artifactory.dev.pardot.com/artifactory'
       config.username = environment.artifactory_user
       config.password = environment.artifactory_token
-      config.ssl_verify = true
 
       proxy = URI(config.endpoint).find_proxy
       if proxy
+        # If we connect through a proxy, we use an HTTP (non-SSL) URL. The proxy
+        # intercepts this request and still talks to the upstream over SSL, but
+        # it allows the response to be cached since it's decrypted by the
+        # _proxy_ instead of _this host_.
+        config.endpoint = 'http://artifactory.dev.pardot.com'
         config.proxy_address = proxy.hostname
         config.proxy_port = proxy.port
+      else
+        config.endpoint = 'https://artifactory.dev.pardot.com'
+        config.ssl_verify = true
       end
     end
   end
