c2f085f9590d3f9f02b65b7fd9c317e23328d03c
Simon Rozet <me@simonrozet.com>
2015-12-10 21:45:25 +0000

    finish first iteration of hubot script protoc generator

--- src/protoc-gen-hubot/generator.go
+++ src/protoc-gen-hubot/generator.go
@@ -1,23 +1,95 @@
 package hubot
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
+	"strings"
 
+	"github.com/gogo/protobuf/proto"
+	"github.com/gogo/protobuf/protoc-gen-gogo/descriptor"
 	plugin "github.com/gogo/protobuf/protoc-gen-gogo/plugin"
 )
 
 type generator struct {
-	request *plugin.CodeGeneratorRequest
+	request        *plugin.CodeGeneratorRequest
+	messagesByName map[string]*google_protobuf.DescriptorProto
 }
 
 func newGenerator(request *plugin.CodeGeneratorRequest) *generator {
-	return &generator{request}
+	return &generator{
+		request,
+		make(map[string]*google_protobuf.DescriptorProto),
+	}
 }
 
-func (c *generator) Generate() (*plugin.CodeGeneratorResponse, error) {
-	if len(c.request.FileToGenerate) == 0 {
+func (g *generator) Generate() (*plugin.CodeGeneratorResponse, error) {
+	numFiles := len(g.request.FileToGenerate)
+	if numFiles == 0 {
 		return nil, errors.New("no file to generate")
 	}
-	response := new(plugin.CodeGeneratorResponse)
+	response := &plugin.CodeGeneratorResponse{}
+	response.File = make([]*plugin.CodeGeneratorResponse_File, numFiles)
+	filesByName := make(map[string]*google_protobuf.FileDescriptorProto, numFiles)
+	for _, file := range g.request.ProtoFile {
+		filesByName[file.GetName()] = file
+	}
+	for i, fileName := range g.request.FileToGenerate {
+		name, content, err := g.generateFile(filesByName[fileName])
+		if err != nil {
+			return nil, err
+		}
+		response.File[i] = &plugin.CodeGeneratorResponse_File{
+			Name:    proto.String(name),
+			Content: proto.String(content),
+		}
+	}
 	return response, nil
+}
+
+func (g *generator) generateFile(
+	protoFile *google_protobuf.FileDescriptorProto,
+) (name string, content string, err error) {
+	for _, message := range protoFile.MessageType {
+		// TODO deal with message.Label (optional, required, repeated)
+		// TODO deal with message.DefaultValue
+		g.messagesByName[message.GetName()] = message
+	}
+	if len(protoFile.Service) != 1 {
+		return "", "", errors.New("can only generate script for exactly one service")
+	}
+	service := protoFile.Service[0]
+	var templateMethods []*templateMethod
+	for _, method := range service.Method {
+		templateMethod := g.newTemplateMethod(protoFile.GetPackage(), method)
+		templateMethods = append(templateMethods, templateMethod)
+	}
+	var buffer bytes.Buffer
+	data := &templateService{
+		Package: protoFile.GetPackage(),
+		Service: service.GetName(),
+		Methods: templateMethods,
+	}
+	if err := scriptTemplate.Execute(&buffer, data); err != nil {
+		return "", "", err
+	}
+	return protoFile.GetPackage(), buffer.String(), nil
+}
+
+func (g *generator) newTemplateMethod(
+	service string,
+	method *google_protobuf.MethodDescriptorProto,
+) *templateMethod {
+	inputMessage := g.messagesByName[strings.Split(method.GetInputType(), ".")[2]]
+	arguments := bytes.NewBufferString("")
+	for _, field := range inputMessage.Field {
+		arguments.WriteString(fmt.Sprintf(" %s=(\\w+)", field.GetName()))
+	}
+	return &templateMethod{
+		Service:   service,
+		Name:      method.GetName(),
+		NameLower: strings.ToLower(method.GetName()),
+		Input:     strings.Split(method.GetInputType(), ".")[2],
+		Arguments: arguments.String(),
+	}
 }
--- src/protoc-gen-hubot/template.go
+++ src/protoc-gen-hubot/template.go
@@ -2,21 +2,36 @@ package hubot
 
 import "text/template"
 
+type templateService struct {
+	Debug   string
+	Package string
+	Service string
+	Methods []*templateMethod
+}
+
+type templateMethod struct {
+	Service   string
+	Name      string
+	NameLower string
+	Input     string
+	Arguments string
+}
+
 var scriptTemplate = template.Must(template.New("script.coffee").Parse(`
 path = require "path"
 grpc = require "grpc"
 protobuf = require "protobufjs"
 
 protodir = path.resolve(__dirname + "/../../src/proto")
-proto = protobuf.loadProtoFile(root: protodir, file: "gcloud.proto")
-gcloud = grpc.loadObject(proto.ns).gcloud
-client = new gcloud.GCloudService("localhost:3000", grpc.Credentials.createInsecure())
+proto = protobuf.loadProtoFile(root: protodir, file: "{{.Service}}.proto")
+{{.Package}} = grpc.loadObject(proto.ns).{{.Package}}
+client = new {{.Package}}.{{.Service}}("localhost:3000", grpc.Credentials.createInsecure())
 
 module.exports = (robot) ->
 {{range .Methods}}
-  robot.respond /{{.ServiceName}} {{.MethodName}}/, (msg) ->
-    request = new {{.ServiceName}}.{{.MethodRequest}}()
-    client.{{.MethodName}}(request) (err, response) ->
-	  msg.Send(response.Output.PlainText)
+  robot.respond /^{{.Service}} {{.NameLower}}{{.Arguments}}$/, (msg) ->
+    request = new client.{{.Input}}()
+    client.{{.Name}}(request) (err, response) ->
+	  msg.send(response.Output.PlainText)
 {{end}}
 `))
