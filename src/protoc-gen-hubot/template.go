7485d2ba0b717c0da344c20b4a509e0d96100871
Simon Rozet <me@simonrozet.com>
2015-12-12 18:14:45 +0000

    make hubot connect to operatord service on k8s

--- src/protoc-gen-hubot/template.go
+++ src/protoc-gen-hubot/template.go
@@ -24,9 +24,12 @@ protobuf = require "protobufjs"
 protodir = path.resolve(__dirname + "/../proto")
 proto = protobuf.loadProtoFile(root: protodir, file: "{{.Package}}.proto")
 {{.Package}} = grpc.loadObject(proto.ns).{{.Package}}
-host = process.env.OPERATORD_PORT_3000_TCP_ADDR
-port = process.env.OPERATORD_PORT_3000_TCP_PORT
-address = "#{host}:#{port}"
+address = process.env.OPERATORD_ADDRESS
+if !address
+  host = process.env.OPERATORD_PORT_3000_TCP_ADDR
+  port = process.env.OPERATORD_PORT_3000_TCP_PORT
+  address = "#{host}:#{port}"
+
 client = new {{.Package}}.{{.Service}}(address, grpc.Credentials.createInsecure())
 
 module.exports = (robot) ->
