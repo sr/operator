447245b80c462b11599e28b0448cb5f3cfc3787f
Simon Rozet <me@simonrozet.com>
2015-12-12 13:19:07 +0000

    miscelleanous `make lint` improvements

--- etc/mk/golang.mk
+++ etc/mk/golang.mk
@@ -20,12 +20,14 @@ install: deps
 
 lint: testdeps
 	go get -v github.com/golang/lint/golint
-	for file in $$(find . -name '*.go' | grep -v '\.pb\.go' | grep -v '\.pb\.gw\.go'); do \
-		golint $${file}; \
-		if [ -n "$$(golint $${file})" ]; then \
-			exit 1; \
-		fi; \
-	done
+	for file in $$(find src -name '*.go' | grep -v '\.pb\.go' | grep -v '\.pb\.gw\.go'); do \
+		bin/golint $${file}; \
+		failure=false; \
+		test -n "$$(bin/golint $${file})" && failure=true; \
+	done; \
+	if $$failure; \
+	then exit 1; \
+	fi
 
 vet: testdeps
 	go vet ./src/...
