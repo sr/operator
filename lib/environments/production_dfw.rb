c0a96647ef8afb656a66fcc5fe3763fad2688d6c
Andy Lindeman <alindeman@salesforce.com>
2016-01-04 14:26:33 -0500

    BREAD-410: pull-agent is responsible for restarting jobs

--- lib/environments/production_dfw.rb
+++ lib/environments/production_dfw.rb
@@ -5,8 +5,14 @@ module Environments
   class ProductionDfw < Production
     include SalesEdgeEnvModule
 
+    restart_task :restart_redis_jobs, only: :pardot
+    restart_task :restart_old_style_jobs, only: :pardot
+    restart_task :restart_autojobs, only: :pardot
+
     after_deploy :restart_pithumbs_service, only: :pithumbs
+
     after_deploy :restart_salesedge, only: :'realtime-frontend'
+
     after_deploy :restart_workflowstats_service, only: :'workflow-stats'
 
     def short_name
