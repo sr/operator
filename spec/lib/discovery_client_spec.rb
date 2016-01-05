fe8a924a5c3c82bf086ebdcbed96521b2ce526b3
Andy Lindeman <alindeman@salesforce.com>
2016-01-05 16:33:14 -0500

    BREAD-505: Adds a discovery client client

---
+++ spec/lib/discovery_client_spec.rb
@@ -0,0 +1,43 @@
+require "spec_helper"
+require "discovery_client"
+
+describe DiscoveryClient do
+  describe "#service" do
+    it "retrieves a list for the specified service" do
+      stub_request(:get, "http://127.0.0.1:8383/v1/service/redis-job-1").
+        to_return(
+          status: 200,
+          body: JSON.dump([
+            {
+              "address" => "job-d1.dev",
+              "id" => "2559d2dc-d1d6-46aa-8913-6d835ac9da99",
+              "name" => "redis-job-1",
+              "payload" => {
+                "datacenter" => "seattle",
+                "role" => "master",
+                "syncCompleted" => false
+              },
+              "port" => 6379,
+              "registrationTimeUTC" => 1450725494953,
+              "serviceType" => "DYNAMIC",
+              "sslPort" => nil,
+              "uriSpec" => nil
+            }
+          ])
+        )
+
+      servers = DiscoveryClient.new.service("redis-job-1")
+      expect(servers.length).to eq(1)
+      expect(servers[0]["address"]).to eq("job-d1.dev")
+    end
+
+    it "raises an error if the service is unavailable" do
+      stub_request(:get, "http://127.0.0.1:8383/v1/service/redis-job-1").
+        to_return(status: 503)
+
+      expect {
+        DiscoveryClient.new.service("redis-job-1")
+      }.to raise_error(DiscoveryClient::Error)
+    end
+  end
+end
