12d256bb6b608afd55feea027455f692452e489e
Andy Lindeman <alindeman@salesforce.com>
2016-01-05 15:03:39 -0500

    Cancel moves each incomplete server to the failed state

--- app/controllers/deploys_controller.rb
+++ app/controllers/deploys_controller.rb
@@ -1,7 +1,7 @@
 class DeploysController < ApplicationController
   before_filter :require_repo
   before_filter :require_target, only: [:new, :create, :index]
-  before_filter :require_deploy, only: [:show, :complete, :cancel]
+  before_filter :require_deploy, only: [:show, :complete, :cancel, :force_to_complete]
   before_filter :require_no_active_deploy, only: [:new, :create]
   before_filter :require_no_active_lock, only: [:new, :create]
 
@@ -68,8 +68,8 @@ class DeploysController < ApplicationController
     end
   end
 
-  def cancel_on_incomplete_servers
-    deploy_workflow_for(current_deploy).cancel_deploy_on_incomplete_servers
+  def force_to_complete
+    deploy_workflow_for(current_deploy).fail_deploy_on_initiated_servers
     redirect_to repo_deploy_path(current_repo.name, current_deploy.id)
   end
 
@@ -79,7 +79,8 @@ class DeploysController < ApplicationController
   end
 
   def cancel
-    current_deploy.cancel! if current_deploy
+    deploy_workflow_for(current_deploy).fail_deploy_on_incomplete_servers
+    current_deploy.cancel!
     redirect_to repo_deploy_path(current_repo.name, current_deploy.id)
   end
 
