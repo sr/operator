5f6b65ddde14086dda2b665cf127beb9e7ebf5f5
Andy Lindeman <alindeman@salesforce.com>
2016-01-06 10:52:25 -0500

    BREAD-410

--- lib/environments/staging.rb
+++ lib/environments/staging.rb
@@ -7,6 +7,12 @@ module Environments
       :restart_redis_jobs,
       only: :pardot
 
+    after_deploy :restart_pithumbs_service, only: :pithumbs
+
+    after_deploy :restart_salesedge, only: :'realtime-frontend'
+
+    after_deploy :restart_workflowstats_service, only: :'workflow-stats'
+
     after_deploy :restart_murdoc, only: :murdoc
   end
 
