f043eaa9e340cc1831f16d3230ceaa75d1657452
Simon Rozet <me@simonrozet.com>
2016-01-08 00:45:15 +0000

    Get rid of unecessary genoperatord package
    This has no value as a reusable library/package.

--- src/cmd/protoc-gen-operatord/main.go
+++ src/cmd/protoc-gen-operatord/main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 
+	"github.com/sr/operator/src/descriptor"
 	"github.com/sr/operator/src/generator"
-	"github.com/sr/operator/src/genoperatord"
 )
 
+const (
+	fileName = "main-gen.go"
+)
+
+func generate(descriptor *descriptor.OperatorDesc) ([]*generator.File, error) {
+	var buffer bytes.Buffer
+	if err := template.Execute(&buffer, descriptor); err != nil {
+		return nil, err
+	}
+	response := []*generator.File{
+		&generator.File{
+			Name:    fileName,
+			Content: buffer.String(),
+		},
+	}
+	return response, nil
+}
+
 func main() {
-	if err := generator.Compile(os.Stdin, os.Stdout, genoperatord.Generate); err != nil {
+	if err := generator.Compile(os.Stdin, os.Stdout, generate); err != nil {
 		fmt.Fprintf(os.Stderr, "protoc-gen-operatord: %s\n", err)
 		os.Exit(1)
 	}
