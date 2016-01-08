6d7b451fa5cfe3bab1c9b891e9a466a668797fa4
Simon Rozet <me@simonrozet.com>
2016-01-08 01:45:47 +0000

    Update protoc-gen-hubot executable to use generator package

---
+++ src/cmd/protoc-gen-operatorhubot/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+
+	"github.com/sr/operator/src/generator"
+)
+
+const (
+	executable = "protoc-gen-operatorhubot"
+	fileExt    = "coffee"
+)
+
+// TODO(sr)
+//  input.WriteString(fmt.Sprintf("%s: msg.match[%d],", field.GetName(), i+1))
+//	arguments.WriteString(fmt.Sprintf(" %s=(\\w+)", field.GetName()))
+func generate(descriptor *generator.Descriptor) ([]*generator.File, error) {
+	var buffer bytes.Buffer
+	response := make([]*generator.File, len(descriptor.Services))
+	for i, service := range descriptor.Services {
+		if err := template.Execute(&buffer, service); err != nil {
+			return nil, err
+		}
+		response[i] = &generator.File{
+			Name:    fmt.Sprintf("%s.%s", service.Name, fileExt),
+			Content: buffer.String(),
+		}
+		buffer.Reset()
+	}
+	return response, nil
+}
+
+func main() {
+	if err := generator.Compile(os.Stdin, os.Stdout, generate); err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %s\n", executable, err)
+		os.Exit(1)
+	}
+}
