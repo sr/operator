3ac1ac3c5bb60ac4bd0bbe61e27d472e65583b7e
Andy Lindeman <alindeman@salesforce.com>
2016-01-18 13:29:30 -0500

    Allow Canoe instances to register with ECS

--- aws/pardotops/canoe.tf
+++ aws/pardotops/canoe.tf
@@ -7,6 +7,37 @@ resource "aws_iam_role" "canoe_ec2_role" {
   assume_role_policy = "${file(\"ec2_instance_trust_relationship.json\")}"
 }
 
+resource "aws_iam_role_policy" "canoe_ec2_ecs_role_policy" {
+  name = "canoe_ecs_role_policy"
+  role = "${aws_iam_role.canoe_ec2_role.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecs:StartTask",
+        "ecs:StopTask",
+        "ecs:RegisterContainerInstance",
+        "ecs:DeregisterContainerInstance",
+        "ecs:DescribeContainerInstances",
+        "ecs:DiscoverPollEndpoint",
+        "ecs:Submit*",
+        "ecs:Poll"
+      ],
+      "Resource": ["*"]
+    },
+    {
+      "Effect": "Allow",
+      "Action": "s3:PutObject",
+      "Resource": "arn:aws:s3:::elasticbeanstalk-*/resources/environments/logs/*"
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_iam_instance_profile" "canoe_ec2_instance_profile" {
   name = "canoe_ec2_instance_profile"
   roles = ["${aws_iam_role.canoe_ec2_role.name}"]
