a689712d11461551417e72437d3531a783dd2f4e
Andy Lindeman <alindeman@salesforce.com>
2016-01-19 12:25:44 -0500

    Adds Canoe load balancer

--- aws/pardotops/canoe.tf
+++ aws/pardotops/canoe.tf
@@ -38,3 +38,44 @@ resource "aws_iam_instance_profile" "canoe_ec2_instance_profile" {
 resource "aws_ecs_cluster" "canoe_production" {
   name = "canoe_production"
 }
+
+resource "aws_elb" "canoe_production" {
+  name = "canoe-production"
+  security_groups = ["${aws_security_group.internal_apps_http_lb.id}"]
+  subnets = [
+    "${aws_subnet.internal_apps_us_east_1a_dmz.id}",
+    "${aws_subnet.internal_apps_us_east_1c_dmz.id}",
+    "${aws_subnet.internal_apps_us_east_1d_dmz.id}",
+    "${aws_subnet.internal_apps_us_east_1e_dmz.id}"
+  ]
+  cross_zone_load_balancing = true
+  connection_draining = true
+  connection_draining_timeout = 30
+
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = 80
+    instance_protocol = "http"
+  }
+
+  listener {
+    lb_port = 443
+    lb_protocol = "https"
+    instance_port = 80
+    instance_protocol = "http"
+    ssl_certificate_id = "arn:aws:iam::364709603225:server-certificate/dev.pardot.com"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 5
+    target = "HTTP:80/_ping"
+    interval = 30
+  }
+
+  tags {
+    Name = "canoe_production"
+  }
+}
