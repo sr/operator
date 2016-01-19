7495211a7cc35115283858731a7151c54d70a92a
Simon Rozet <me@simonrozet.com>
2016-01-19 14:56:43 +0000

    Setup hubot RC and operatord service

--- src/services/controller/controller.go
+++ src/services/controller/controller.go
@@ -1,30 +1,59 @@
 package controller
 
-import (
-	"fmt"
-	"os"
-	"strings"
+import client "k8s.io/kubernetes/pkg/client/unversioned"
 
-	client "k8s.io/kubernetes/pkg/client/unversioned"
+const (
+	OperatordName           = "operatord"
+	OperatordImage          = "gcr.io/dev-europe-west1/operatord:f743959"
+	OperatordCommand        = "/k8s-operatord"
+	OperatordPortName       = "grpc"
+	OperatordPort           = 3000
+	OperatordSecretsSetting = "CONTROLLER_OPERATORD_SECRETS"
+
+	HubotName           = "hubot"
+	HubotImage          = "gcr.io/dev-europe-west1/hubot:cbc02ba"
+	HubotCommand        = "/hubot/bin/k8s-hubot"
+	HubotSecretsSetting = "CONTROLLER_HUBOT_SECRETS"
 )
 
 /// TODO(sr) rename all Env structs to Config
 type Env struct {
 	KubectlProxyURL     string `env:"CONTROLLER_KUBECTL_PROXY_URL,required"`
 	KubernetesNamespace string `env:"CONTROLLER_KUBERNETES_NAMESPACE,required"`
-	Secrets             string `env:"CONTROLLER_SECRETS"`
+	OperatordSecrets    string `env:"CONTROLLER_OPERATORD_SECRETS,required"`
+	HubotSecrets        string `env:"CONTROLLER_HUBOT_SECRETS,required"`
+}
+
+type OperatordConfig struct {
+	Name         string
+	DefaultImage string
+	Command      string
+	Port         int
+	PortName     string
+	Secrets      map[string]string
+}
+
+type HubotConfig struct {
+	Name         string
+	DefaultImage string
+	Command      string
+	Secrets      map[string]string
 }
 
 func NewAPIServer(config *Env) (ControllerServer, error) {
-	secrets := make(map[string]string)
-	if config.Secrets != "" {
-		for _, secret := range strings.Split(config.Secrets, ",") {
-			value, ok := os.LookupEnv(secret)
-			if !ok {
-				return nil, fmt.Errorf("env key not set when included in CONTROLLER_SECRETS: %s", secret)
-			}
-			secrets[secret] = value
-		}
+	operatordSecrets, err := loadSecretsForService(
+		OperatordSecretsSetting,
+		config.OperatordSecrets,
+	)
+	if err != nil {
+		return nil, err
+	}
+	hubotSecrets, err := loadSecretsForService(
+		HubotSecretsSetting,
+		config.HubotSecrets,
+	)
+	if err != nil {
+		return nil, err
 	}
 	client, err := client.New(&client.Config{
 		Host: config.KubectlProxyURL,
@@ -32,5 +61,22 @@ func NewAPIServer(config *Env) (ControllerServer, error) {
 	if err != nil {
 		return nil, err
 	}
-	return newAPIServer(client, config.KubernetesNamespace, secrets), nil
+	return newAPIServer(
+		client,
+		config.KubernetesNamespace,
+		&OperatordConfig{
+			Name:         OperatordName,
+			DefaultImage: OperatordImage,
+			Command:      OperatordCommand,
+			PortName:     OperatordPortName,
+			Port:         OperatordPort,
+			Secrets:      operatordSecrets,
+		},
+		&HubotConfig{
+			Name:         HubotName,
+			DefaultImage: HubotImage,
+			Command:      HubotCommand,
+			Secrets:      hubotSecrets,
+		},
+	), nil
 }
