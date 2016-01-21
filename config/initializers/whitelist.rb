cec184c8756b9876f10196cbd919879888443680
Andy Lindeman <alindeman@salesforce.com>
2016-01-21 14:48:41 -0500

    BREAD-565: Adds outbound proxy whitelists

--- config/initializers/whitelist.rb
+++ config/initializers/whitelist.rb
@@ -2,11 +2,18 @@ require 'ipaddr'
 
 TRUSTED_NETWORKS = [
   '127.0.0.1/32',
-  '204.14.236.0/24', # aloha-east
-  '204.14.239.0/24'  # aloha-west
+  '204.14.236.0/24',   # aloha-east
+  '204.14.239.0/24',   # aloha-west
+  '174.37.191.2/32',   # proxy.dev
+  '169.45.0.88/32',    # squid-d4
+  '136.147.104.20/30'  # pardot-proxyout1-{1,2,3,4}-dfw
 ].map { |i| IPAddr.new(i) }.freeze
 
-Rack::Attack.whitelist('allow from localhost') do |req|
+Rack::Attack.whitelist('ip whitelist') do |req|
   remote_ip = IPAddr.new(req.ip)
   TRUSTED_NETWORKS.any? { |net| net.include?(remote_ip) }
+end
+
+Rack::Attack.blacklist('deny from internet') do |req|
+  true
 end
