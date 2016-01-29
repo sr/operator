24a072b7359cc5b86765533c201c05ca683107f3
Simon Rozet <me@simonrozet.com>
2016-01-29 21:20:42 +0000

    Removed _example directory from vendored operator

--- _example/vendor/src/github.com/sr/operator/_example/vendor/src/github.com/sr/operator/vendor/go.pedge.io/protolog/benchmark/marshal/benchmark_marshal.go
+++
@@ -1,1 +0,0 @@
-package protolog_benchmark_marshal // import "go.pedge.io/protolog/benchmark/marshal"
--- _example/vendor/src/github.com/sr/operator/_example/vendor/src/go.pedge.io/protolog/testing/testing.go
+++
@@ -1,4 +0,0 @@
-/*
-Package protolog_testing defines testing for protolog.
-*/
-package protolog_testing // import "go.pedge.io/protolog/testing"
