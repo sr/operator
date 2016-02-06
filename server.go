62aa08ae1cc8c9a27aa908948df0cc52e1cac9d4
Simon Rozet <me@simonrozet.com>
2016-02-06 17:29:41 +0000

    Couple of minor fixes due to package shuffling

--- server.go
+++ server.go
@@ -4,7 +4,6 @@ import (
 	"net"
 
 	"github.com/sr/grpcinstrument"
-	"github.com/sr/operator/pb"
 
 	"google.golang.org/grpc"
 )
@@ -35,25 +34,25 @@ func newServer(
 func (s *server) Serve() error {
 	listener, err := net.Listen(protocol, s.config.Address)
 	if err != nil {
-		s.logger.Error(&pb.ServerStartupError{err.Error()})
+		s.logger.Error(&ServerStartupError{err.Error()})
 		return err
 	}
-	s.logger.Info(&pb.ServerStartupNotice{Address: s.config.Address, Protocol: protocol})
+	s.logger.Info(&ServerStartupNotice{Address: s.config.Address, Protocol: protocol})
 	err = s.server.Serve(listener)
 	if err != nil {
-		s.logger.Error(&pb.ServerStartupError{err.Error()})
+		s.logger.Error(&ServerStartupError{err.Error()})
 		return err
 	}
 	return nil
 }
 
 func (s *server) LogServiceRegistered(serviceName string) {
-	s.logger.Info(&pb.ServiceRegistered{&pb.Service{Name: serviceName}})
+	s.logger.Info(&ServiceRegistered{&Service{Name: serviceName}})
 }
 
 func (s *server) LogServiceStartupError(serviceName string, err error) {
-	s.logger.Error(&pb.ServiceStartupError{
-		Service: &pb.Service{
+	s.logger.Error(&ServiceStartupError{
+		Service: &Service{
 			Name: serviceName,
 		},
 		Message: err.Error(),
