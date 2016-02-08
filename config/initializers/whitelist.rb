c77bf3d58867df2f9f65e11b7c9cf2529ba27ffc
Andy Lindeman <alindeman@salesforce.com>
2016-02-08 11:30:45 -0500

    BREAD-596: Allows PHX to access Canoe

--- config/initializers/whitelist.rb
+++ config/initializers/whitelist.rb
@@ -7,7 +7,8 @@ TRUSTED_NETWORKS = [
   '173.192.141.222/32', # tools-s1 (prodbot)
   '174.37.191.2/32',    # proxy.dev
   '169.45.0.88/32',     # squid-d4
-  '136.147.104.20/30'   # pardot-proxyout1-{1,2,3,4}-dfw
+  '136.147.104.20/30',  # pardot-proxyout1-{1,2,3,4}-dfw
+  '136.147.96.20/30',   # pardot-proxyout1-{1,2,3,4}-phx
 ].map { |i| IPAddr.new(i) }.freeze
 
 Rack::Attack.whitelist('ip whitelist') do |req|
