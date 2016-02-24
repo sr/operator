fa9e22cb8224ebe4d575824c48f995c2c832dee6
Andy Lindeman <alindeman@salesforce.com>
2016-02-24 11:49:00 -0500

    Auth failure message renders correctly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_filter :require_oauth_authentication, only: [:new, :create]
+  skip_before_filter :require_oauth_authentication, only: [:new, :create, :failure]
 
   # OmniAuth doesn't send us a CSRF token
   skip_before_filter :verify_authenticity_token, only: [:create]
