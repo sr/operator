f40af723d5b99287ec0b4710cadc10d8a20a61c2
Andy Lindeman <alindeman@salesforce.com>
2016-03-03 10:57:55 -0500

    Adds tools_egress VPC

---
+++ aws/pardot-atlassian/authentication.tf
@@ -0,0 +1,11 @@
+variable "pardotatlassian_access_key_id" {
+}
+
+variable "pardotatlassian_secret_access_key" {
+}
+
+provider "aws" {
+  access_key = "${var.pardotatlassian_access_key_id}"
+  secret_key = "${var.pardotatlassian_secret_access_key}"
+  region = "us-east-1"
+}
---
+++ aws/pardot-atlassian/tools_egress.tf
@@ -0,0 +1,131 @@
+# A VPC with the sole purpose of being peered with so egress traffic appears
+# from a whitelisted IP.
+resource "aws_vpc" "tools_egress" {
+  cidr_block = "172.31.0.0/16"
+  tags {
+    Name = "tools_egress"
+  }
+}
+
+resource "aws_subnet" "tools_egress_us_east_1b" {
+  vpc_id = "${aws_vpc.tools_egress.id}"
+  availability_zone = "us-east-1b"
+  cidr_block = "172.31.0.0/19"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "tools_egress_us_east_1c" {
+  vpc_id = "${aws_vpc.tools_egress.id}"
+  availability_zone = "us-east-1c"
+  cidr_block = "172.31.32.0/19"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "tools_egress_us_east_1d" {
+  vpc_id = "${aws_vpc.tools_egress.id}"
+  availability_zone = "us-east-1d"
+  cidr_block = "172.31.64.0/19"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "tools_egress_us_east_1e" {
+  vpc_id = "${aws_vpc.tools_egress.id}"
+  availability_zone = "us-east-1e"
+  cidr_block = "172.31.96.0/19"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "tools_egress_us_east_1b_dmz" {
+  vpc_id = "${aws_vpc.tools_egress.id}"
+  availability_zone = "us-east-1b"
+  cidr_block = "172.31.128.0/19"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "tools_egress_us_east_1c_dmz" {
+  vpc_id = "${aws_vpc.tools_egress.id}"
+  availability_zone = "us-east-1c"
+  cidr_block = "172.31.160.0/19"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "tools_egress_us_east_1d_dmz" {
+  vpc_id = "${aws_vpc.tools_egress.id}"
+  availability_zone = "us-east-1d"
+  cidr_block = "172.31.192.0/19"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "tools_egress_us_east_1e_dmz" {
+  vpc_id = "${aws_vpc.tools_egress.id}"
+  availability_zone = "us-east-1e"
+  cidr_block = "172.31.224.0/19"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "tools_egress_internet_gw" {
+  vpc_id = "${aws_vpc.tools_egress.id}"
+}
+
+resource "aws_eip" "tools_egress_nat_gw" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "tools_egress_nat_gw" {
+  allocation_id = "${aws_eip.tools_egress_nat_gw.id}"
+  subnet_id = "${aws_subnet.tools_egress_us_east_1b_dmz.id}"
+}
+
+resource "aws_route" "tools_egress_route" {
+  route_table_id = "${aws_vpc.tools_egress.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = "${aws_nat_gateway.tools_egress_nat_gw.id}"
+}
+
+resource "aws_route_table" "tools_egress_route_dmz" {
+  vpc_id = "${aws_vpc.tools_egress.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.tools_egress_internet_gw.id}"
+  }
+}
+
+resource "aws_route_table_association" "tools_egress_us_east_1b" {
+  subnet_id = "${aws_subnet.tools_egress_us_east_1b.id}"
+  route_table_id = "${aws_vpc.tools_egress.main_route_table_id}"
+}
+
+resource "aws_route_table_association" "tools_egress_us_east_1c" {
+  subnet_id = "${aws_subnet.tools_egress_us_east_1c.id}"
+  route_table_id = "${aws_vpc.tools_egress.main_route_table_id}"
+}
+
+resource "aws_route_table_association" "tools_egress_us_east_1d" {
+  subnet_id = "${aws_subnet.tools_egress_us_east_1d.id}"
+  route_table_id = "${aws_vpc.tools_egress.main_route_table_id}"
+}
+
+resource "aws_route_table_association" "tools_egress_us_east_1e" {
+  subnet_id = "${aws_subnet.tools_egress_us_east_1e.id}"
+  route_table_id = "${aws_vpc.tools_egress.main_route_table_id}"
+}
+
+resource "aws_route_table_association" "tools_egress_us_east_1b_dmz" {
+  subnet_id = "${aws_subnet.tools_egress_us_east_1b_dmz.id}"
+  route_table_id = "${aws_route_table.tools_egress_route_dmz.id}"
+}
+
+resource "aws_route_table_association" "tools_egress_us_east_1c_dmz" {
+  subnet_id = "${aws_subnet.tools_egress_us_east_1c_dmz.id}"
+  route_table_id = "${aws_route_table.tools_egress_route_dmz.id}"
+}
+
+resource "aws_route_table_association" "tools_egress_us_east_1d_dmz" {
+  subnet_id = "${aws_subnet.tools_egress_us_east_1d_dmz.id}"
+  route_table_id = "${aws_route_table.tools_egress_route_dmz.id}"
+}
+
+resource "aws_route_table_association" "tools_egress_us_east_1e_dmz" {
+  subnet_id = "${aws_subnet.tools_egress_us_east_1e_dmz.id}"
+  route_table_id = "${aws_route_table.tools_egress_route_dmz.id}"
+}
