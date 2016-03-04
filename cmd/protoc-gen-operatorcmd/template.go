6c44ca1949c1b126e5907c197146b37e84d0ec30
Simon Rozet <me@simonrozet.com>
2016-03-04 11:44:55 +0100

    Fix code generated by protoc-gen-operatorcmd to set the source field

--- cmd/protoc-gen-operatorcmd/template.go
+++ cmd/protoc-gen-operatorcmd/template.go
@@ -56,6 +56,7 @@ var cmd = operator.NewCommand(
 						response, err := client.{{.Name}}(
 							context.Background(),
 							&{{$serviceName}}.{{.Input}}{
+								Source: ctx.Source,
 								{{- range .Arguments}}
 								{{camelCase .Name}}: *{{.Name}},
 								{{- end}}
