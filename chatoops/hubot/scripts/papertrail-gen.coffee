d94cd70007c9e201e53e14bafa8c8b902824a925
Simon Rozet <me@simonrozet.com>
2016-03-05 12:51:59 +0100

    Move sr/chatoops to github.com/sr/operator tree

---
+++ chatoops/hubot/scripts/papertrail-gen.coffee
@@ -0,0 +1,36 @@
+# Description:
+#  Undocumented.
+#
+# Commands:
+#   hubot papertrail search  [query=value] - Undocumented.
+#
+# Configuration:
+#   OPERATORD_ADDRESS
+path = require "path"
+grpc = require "grpc"
+protobuf = require "protobufjs"
+
+protodir = path.resolve(__dirname + "/../proto")
+proto = protobuf.loadProtoFile(root: protodir, file: "papertrail.proto")
+papertrail = grpc.loadObject(proto.ns).papertrail
+address = process.env.OPERATORD_ADDRESS
+if !address
+  host = process.env.OPERATORD_PORT_3000_TCP_ADDR
+  port = process.env.OPERATORD_PORT_3000_TCP_PORT
+  address = "#{host}:#{port}"
+
+client = new papertrail.PapertrailService(address, grpc.Credentials.createInsecure())
+
+module.exports = (robot) ->
+
+  robot.respond /papertrail search(.*)/, (msg) ->
+    input = {}
+    for arg in msg.match[1].split(" ")
+      parts = arg.split("=")
+      if parts.length == 2 && parts[0] != "" && parts[1] != ""
+        input[parts[0]] = parts[1]
+    client.search input, (err, response) ->
+      if err
+        msg.send("```\nSearch error: #{err.message}\n```")
+      else
+        msg.send("```\n#{response.output.PlainText}\n```")
---
+++ chatoops/services/buildkite/api_server.go
@@ -0,0 +1,165 @@
+package buildkite
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"strings"
+	"text/tabwriter"
+
+	"github.com/sr/operator"
+	buildkiteapi "github.com/wolfeidau/go-buildkite/buildkite"
+	"golang.org/x/net/context"
+)
+
+const buildsLimit = 10
+
+type apiServer struct {
+	client *buildkiteapi.Client
+}
+
+func newAPIServer(client *buildkiteapi.Client) *apiServer {
+	return &apiServer{client}
+}
+
+func (s *apiServer) Status(
+	ctx context.Context,
+	request *StatusRequest,
+) (*StatusResponse, error) {
+	organizations, err := s.fetchAllOrganizations()
+	if err != nil {
+		return nil, err
+	}
+	output := bytes.NewBufferString("")
+	w := new(tabwriter.Writer)
+	w.Init(output, 0, 8, 1, '\t', 0)
+	fmt.Fprintf(w, "%s\t%s\t%s\t%s\n", "NAME", "STATUS", "BRANCH", "URL")
+	for _, organization := range organizations {
+		projects, err := s.fetchAllOrganizationProjects(organization)
+		if err != nil {
+			return nil, err
+		}
+		for _, project := range projects {
+			fmt.Fprintf(
+				w,
+				"%s\t%s\t%s\t%s\n",
+				fmt.Sprintf("%s/%s", *organization.Slug, *project.Slug),
+				*project.FeaturedBuild.State,
+				*project.FeaturedBuild.Branch,
+				*project.WebURL,
+			)
+		}
+	}
+	if err := w.Flush(); err != nil {
+		return nil, err
+	}
+	return &StatusResponse{
+		Output: &operator.Output{
+			PlainText: output.String(),
+		},
+	}, nil
+}
+
+func (s *apiServer) ListBuilds(
+	ctx context.Context,
+	request *ListBuildsRequest,
+) (*ListBuildsResponse, error) {
+	options := &buildkiteapi.BuildsListOptions{
+		State:       "",
+		Branch:      "",
+		ListOptions: buildkiteapi.ListOptions{Page: 1, PerPage: buildsLimit},
+	}
+	var (
+		builds []buildkiteapi.Build
+		err    error
+	)
+	if request.ProjectSlug == "" {
+		builds, _, err = s.client.Builds.List(options)
+	} else {
+		p := strings.SplitN(request.ProjectSlug, "/", 2)
+		if len(p) != 2 {
+			return nil, errors.New("invalid slug. must be in the form org/repo")
+		}
+		builds, _, err = s.client.Builds.ListByProject(p[0], p[1], options)
+	}
+	if err != nil {
+		return nil, err
+	}
+	output := bytes.NewBufferString("")
+	w := new(tabwriter.Writer)
+	w.Init(output, 0, 8, 1, '\t', 0)
+	fmt.Fprintf(w, "%s\t%s\t%s\t%s\n", "ID", "STATUS", "BRANCH", "URL")
+	for _, build := range builds {
+		fmt.Fprintf(
+			w,
+			"%d\t%s\t%s\t%s\n",
+			*build.Number,
+			*build.State,
+			*build.Branch,
+			*build.WebURL,
+		)
+	}
+	if err := w.Flush(); err != nil {
+		return nil, err
+	}
+	return &ListBuildsResponse{
+		Output: &operator.Output{
+			PlainText: output.String(),
+		},
+	}, nil
+}
+
+func (s *apiServer) fetchAllOrganizations() ([]buildkiteapi.Organization, error) {
+	var organizations []buildkiteapi.Organization
+	pageNumber := 1
+	for {
+		collection, response, err := s.client.Organizations.List(
+			&buildkiteapi.OrganizationListOptions{
+				ListOptions: buildkiteapi.ListOptions{
+					Page:    pageNumber,
+					PerPage: 100,
+				},
+			},
+		)
+		if err != nil {
+			return nil, err
+		}
+		for _, organization := range collection {
+			organizations = append(organizations, organization)
+		}
+		if response.NextPage == 0 {
+			break
+		}
+		pageNumber = pageNumber + 1
+	}
+	return organizations, nil
+}
+
+func (s *apiServer) fetchAllOrganizationProjects(
+	organization buildkiteapi.Organization,
+) ([]buildkiteapi.Project, error) {
+	var projects []buildkiteapi.Project
+	pageNumber := 1
+	for {
+		collection, response, err := s.client.Projects.List(
+			*organization.Slug,
+			&buildkiteapi.ProjectListOptions{
+				ListOptions: buildkiteapi.ListOptions{
+					Page:    pageNumber,
+					PerPage: 100,
+				},
+			},
+		)
+		if err != nil {
+			return nil, err
+		}
+		for _, project := range collection {
+			projects = append(projects, project)
+		}
+		if response.NextPage == 0 {
+			break
+		}
+		pageNumber = pageNumber + 1
+	}
+	return projects, nil
+}
