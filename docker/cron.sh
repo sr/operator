574a65a413bf69f3e55bd62d1a46b577ac1a0adf
Jan Ulrich <julrich@salesforce.com>
2016-03-07 08:32:36 -0800

    Detach cron from shell and run in fg for logging

--- docker/cron.sh
+++ docker/cron.sh
@@ -2,4 +2,4 @@
 touch /var/log/cron.log
 env | grep PULL_HOSTNAME | awk '{print "export " $0;}' > /tmp/.pullhostname.env
 # CMD crond -n # on CentOS, cron -f # on Ubuntu for running in fg
-cron
+exec cron -f
