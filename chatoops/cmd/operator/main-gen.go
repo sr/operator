ad823bd579a577f92fa4d3143c9a8a4922e096ab
Simon Rozet <me@simonrozet.com>
2016-03-09 21:33:44 +0000

    Woops

--- chatoops/cmd/operator/main-gen.go
+++ chatoops/cmd/operator/main-gen.go
@@ -36,8 +36,6 @@ var cmd = operator.NewCommand(
  all projects.`,
 					Run: func(ctx *operator.CommandContext) (string, error) {
 						slug := ctx.Flags.String("slug", "", "")
-						if ctx.Help == true {
-						}
 						if err := ctx.Flags.Parse(ctx.Args); err != nil {
 							return "", err
 						}
