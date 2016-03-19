11fbdbddf872a0f9c74dc860b590dc8c59520218
Simon Rozet <me@simonrozet.com>
2016-03-19 22:43:33 +0000

    Output Hubot scripts in pure JavaScript
    Simpler this way. One less dependency and one less level of indirection.

---
+++ chatoops/hubot/scripts/gcloud-gen.js
@@ -0,0 +1,62 @@
+// Description:
+//  Undocumented.
+//
+// Commands:
+//   hubot gcloud create-dev-instance  - Undocumented.
+//   hubot gcloud list-instances  [project_id=value] - Undocumented.
+//
+// Configuration:
+//   OPERATORD_ADDRESS
+var path = require("path"),
+	grpc = require("grpc"),
+	protobuf = require("protobufjs");
+
+var protodir = path.resolve(__dirname + "/../proto"),
+	proto = protobuf.loadProtoFile({root: protodir, file: "gcloud.proto"})
+	operator = proto.build("operator"),
+	gcloud = grpc.loadObject(proto.ns).gcloud;
+
+var address = process.env.OPERATORD_ADDRESS,
+	client = new gcloud.GcloudService(address, grpc.Credentials.createInsecure());
+
+module.exports = function(robot) {
+
+	robot.respond(/gcloud create-dev-instance(.*)/, function(msg) {
+		var input = {},
+			ref = msg.match[1].split(" ");
+		for (i = 0, len = ref.length; i < len; i++) {
+			var arg = ref[i],
+				parts = arg.split("=");
+			if (parts.length === 2 && parts[0] !== "" && parts[1] !== "") {
+				input[parts[0]] = parts[1];
+			}
+		}
+		return client.createDevInstance(input, function(err, response) {
+			if (err) {
+				return msg.send("```\nCreateDevInstance error: " + err.message + "\n```")
+			} else {
+				return msg.send("```\n" + response.output.PlainText + "\n```")
+			}
+		});
+	});
+
+	robot.respond(/gcloud list-instances(.*)/, function(msg) {
+		var input = {},
+			ref = msg.match[1].split(" ");
+		for (i = 0, len = ref.length; i < len; i++) {
+			var arg = ref[i],
+				parts = arg.split("=");
+			if (parts.length === 2 && parts[0] !== "" && parts[1] !== "") {
+				input[parts[0]] = parts[1];
+			}
+		}
+		return client.listInstances(input, function(err, response) {
+			if (err) {
+				return msg.send("```\nListInstances error: " + err.message + "\n```")
+			} else {
+				return msg.send("```\n" + response.output.PlainText + "\n```")
+			}
+		});
+	});
+
+}
---
+++ chatoops/hubot/scripts/papertrail-gen.js
@@ -0,0 +1,42 @@
+// Description:
+//  Undocumented.
+//
+// Commands:
+//   hubot papertrail search  [query=value] - Undocumented.
+//
+// Configuration:
+//   OPERATORD_ADDRESS
+var path = require("path"),
+	grpc = require("grpc"),
+	protobuf = require("protobufjs");
+
+var protodir = path.resolve(__dirname + "/../proto"),
+	proto = protobuf.loadProtoFile({root: protodir, file: "papertrail.proto"})
+	operator = proto.build("operator"),
+	papertrail = grpc.loadObject(proto.ns).papertrail;
+
+var address = process.env.OPERATORD_ADDRESS,
+	client = new papertrail.PapertrailService(address, grpc.Credentials.createInsecure());
+
+module.exports = function(robot) {
+
+	robot.respond(/papertrail search(.*)/, function(msg) {
+		var input = {},
+			ref = msg.match[1].split(" ");
+		for (i = 0, len = ref.length; i < len; i++) {
+			var arg = ref[i],
+				parts = arg.split("=");
+			if (parts.length === 2 && parts[0] !== "" && parts[1] !== "") {
+				input[parts[0]] = parts[1];
+			}
+		}
+		return client.search(input, function(err, response) {
+			if (err) {
+				return msg.send("```\nSearch error: " + err.message + "\n```")
+			} else {
+				return msg.send("```\n" + response.output.PlainText + "\n```")
+			}
+		});
+	});
+
+}
