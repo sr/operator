9dedf0ee2f19e36da2d2b287670db9ceb9de5132
Simon Rozet <srozet@salesforce.com>
2016-04-04 18:56:33 +0200

    Remove worthless AccountsController test case

--- test/controllers/accounts_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class AccountsControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:accounts)
-  end
-end
