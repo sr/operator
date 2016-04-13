f886fba016ed411e5d436f167ce526ee5cd0a7b0
Simon Rozet <srozet@salesforce.com>
2016-04-13 11:36:14 +0200

    Fix deprecation warning in db/migrate/20131002130914_create_auth_users.rb:8
    DEPRECATION WARNING: `#timestamps` was called without specifying an
    option for `null`. In Rails 5, this behavior will change to `null:
    false`. You should manually specify `null: true` to prevent the behavior
    of your existing migrations from changing. (called from block in change
    at /app/db/migrate/20131002130914_create_auth_users.rb:8)

--- db/migrate/20131002130914_create_auth_users.rb
+++ db/migrate/20131002130914_create_auth_users.rb
@@ -5,7 +5,7 @@ class CreateAuthUsers < ActiveRecord::Migration
       t.string :name
       t.string :uid
       t.string :token
-      t.timestamps
+      t.timestamps, null: false
     end
     add_index :auth_users, :email
   end
