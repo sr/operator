21db15e7a288528c4fb8d406bbf43b8e952066eb
Simon Rozet <srozet@salesforce.com>
2016-04-13 11:45:09 +0200

    Woops fix syntax error

--- db/migrate/20131002130914_create_auth_users.rb
+++ db/migrate/20131002130914_create_auth_users.rb
@@ -5,7 +5,7 @@ class CreateAuthUsers < ActiveRecord::Migration
       t.string :name, null: false
       t.string :uid, null: false
       t.string :token, null: false
-      t.timestamps, null: false
+      t.timestamps null: false
     end
     add_index :auth_users, :email, unique: true
     add_index :auth_users, :token, unique: true
