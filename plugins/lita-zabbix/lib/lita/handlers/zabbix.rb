47ae6246bf6e27e8432c449ca1338a158803fb36
rkiefer <rkiefer@salesforce.com>
2016-04-13 21:02:52 -0700

    BREAD-690 freakin question marks -- I couldn't hit this line of code testing locally :(

--- plugins/lita-zabbix/lib/lita/handlers/zabbix.rb
+++ plugins/lita-zabbix/lib/lita/handlers/zabbix.rb
@@ -133,7 +133,7 @@ module Lita
               log: log
           )
           status = monitor_supervisor.get_paused_monitors.include?(::Zabbix::Zabbixmon::MONITOR_NAME) ? "PAUSED" : "ACTIVE"
-          paging = config.paging_monitors.include?w(::Zabbix::Zabbixmon::MONITOR_NAME) ? "PAGER: #{config.pager.to_s}" : "NOT PAGING"
+          paging = config.paging_monitors.include? (::Zabbix::Zabbixmon::MONITOR_NAME) ? "PAGER: #{config.pager.to_s}" : "NOT PAGING"
           msg += "#{::Zabbix::Zabbixmon::MONITOR_NAME}-#{datacenter}  / #{status} / #{paging}"
           #TODO: Last known status per-datacenter
         end
