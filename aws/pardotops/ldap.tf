b34862fb08a3d3cd3794222366b79ec610211baf
Andy Lindeman <alindeman@salesforce.com>
2016-04-22 10:17:28 -0400

    BREAD-835: Allows internal apps to reach LDAP

--- aws/pardotops/ldap.tf
+++ aws/pardotops/ldap.tf
@@ -33,6 +33,7 @@ resource "aws_security_group" "internal_apps_ldap_server" {
     protocol = "tcp"
     cidr_blocks = [
       "${aws_vpc.internal_apps.cidr_block}",
+      "${aws_eip.internal_apps_nat_gw.public_ip}/32",
       "173.192.141.222/32", # tools-s1 (password.pardot.com)
       "67.228.6.68/32"      # auth-d1
     ]
@@ -43,6 +44,7 @@ resource "aws_security_group" "internal_apps_ldap_server" {
     protocol = "tcp"
     cidr_blocks = [
       "${aws_vpc.internal_apps.cidr_block}",
+      "${aws_eip.internal_apps_nat_gw.public_ip}/32",
       "173.192.141.222/32", # tools-s1 (password.pardot.com)
       "67.228.6.68/32"      # auth-d1
     ]
