8ed41177c80b9c75ae3cde2a6bd2b2db6a914d2f
Simon Rozet <me@simonrozet.com>
2016-04-24 11:46:23 +0200

    Fix github.com/sr/operator/chatoops

--- chatoops/services/buildkite/buildkite.pb.go
+++ chatoops/services/buildkite/buildkite.pb.go
@@ -115,7 +115,7 @@ var _ grpc.ClientConn
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion1
+const _ = grpc.SupportPackageIsVersion2
 
 // Client API for BuildkiteService service
 
@@ -169,28 +169,40 @@ func RegisterBuildkiteServiceServer(s *grpc.Server, srv BuildkiteServiceServer) 
 	s.RegisterService(&_BuildkiteService_serviceDesc, srv)
 }
 
-func _BuildkiteService_Status_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error) (interface{}, error) {
+func _BuildkiteService_Status_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(StatusRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
-	out, err := srv.(BuildkiteServiceServer).Status(ctx, in)
-	if err != nil {
-		return nil, err
+	if interceptor == nil {
+		return srv.(BuildkiteServiceServer).Status(ctx, in)
 	}
-	return out, nil
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/buildkite.BuildkiteService/Status",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(BuildkiteServiceServer).Status(ctx, req.(*StatusRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
-func _BuildkiteService_ListBuilds_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error) (interface{}, error) {
+func _BuildkiteService_ListBuilds_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(ListBuildsRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
-	out, err := srv.(BuildkiteServiceServer).ListBuilds(ctx, in)
-	if err != nil {
-		return nil, err
+	if interceptor == nil {
+		return srv.(BuildkiteServiceServer).ListBuilds(ctx, in)
 	}
-	return out, nil
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/buildkite.BuildkiteService/ListBuilds",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(BuildkiteServiceServer).ListBuilds(ctx, req.(*ListBuildsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 var _BuildkiteService_serviceDesc = grpc.ServiceDesc{
--- chatoops/services/gcloud/gcloud.pb.go
+++ chatoops/services/gcloud/gcloud.pb.go
@@ -181,7 +181,7 @@ var _ grpc.ClientConn
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion1
+const _ = grpc.SupportPackageIsVersion2
 
 // Client API for GcloudService service
 
@@ -244,40 +244,58 @@ func RegisterGcloudServiceServer(s *grpc.Server, srv GcloudServiceServer) {
 	s.RegisterService(&_GcloudService_serviceDesc, srv)
 }
 
-func _GcloudService_CreateDevInstance_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error) (interface{}, error) {
+func _GcloudService_CreateDevInstance_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(CreateDevInstanceRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
-	out, err := srv.(GcloudServiceServer).CreateDevInstance(ctx, in)
-	if err != nil {
-		return nil, err
+	if interceptor == nil {
+		return srv.(GcloudServiceServer).CreateDevInstance(ctx, in)
 	}
-	return out, nil
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/gcloud.GcloudService/CreateDevInstance",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GcloudServiceServer).CreateDevInstance(ctx, req.(*CreateDevInstanceRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
-func _GcloudService_ListInstances_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error) (interface{}, error) {
+func _GcloudService_ListInstances_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(ListInstancesRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
-	out, err := srv.(GcloudServiceServer).ListInstances(ctx, in)
-	if err != nil {
-		return nil, err
+	if interceptor == nil {
+		return srv.(GcloudServiceServer).ListInstances(ctx, in)
 	}
-	return out, nil
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/gcloud.GcloudService/ListInstances",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GcloudServiceServer).ListInstances(ctx, req.(*ListInstancesRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
-func _GcloudService_Stop_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error) (interface{}, error) {
+func _GcloudService_Stop_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(StopRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
-	out, err := srv.(GcloudServiceServer).Stop(ctx, in)
-	if err != nil {
-		return nil, err
+	if interceptor == nil {
+		return srv.(GcloudServiceServer).Stop(ctx, in)
 	}
-	return out, nil
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/gcloud.GcloudService/Stop",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GcloudServiceServer).Stop(ctx, req.(*StopRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 var _GcloudService_serviceDesc = grpc.ServiceDesc{
