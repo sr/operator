64e55f637431f8e060a69921db68a9741ab7ff62
rkiefer <rkiefer@salesforce.com>
2016-04-25 19:20:33 -0700

    BREAD-690 simplifying and logging

--- plugins/lita-zabbix/lib/zabbix/client.rb
+++ plugins/lita-zabbix/lib/zabbix/client.rb
@@ -66,8 +66,8 @@ module Zabbix
       )
     end
 
-    def get_item_by_key_and_lastvalue(key:, lastvalue:)
-      @client.items.get(name: key, lastvalue: lastvalue) unless (key.nil? || lastvalue.nil?)
+    def get_item_by_lastvalue(lastvalue:)
+      @client.items.get(lastvalue: lastvalue) unless (lastvalue.nil?)
     end
 
     private
--- plugins/lita-zabbix/lib/zabbix/zabbixmon.rb
+++ plugins/lita-zabbix/lib/zabbix/zabbixmon.rb
@@ -8,9 +8,8 @@ module Zabbix
     INCIDENT_KEY = "#{MONITOR_NAME}-%s"
     ERR_NON_200_HTTP_CODE = "HAL9000 HTTP'd Zabbix, but the host failed to respond to an HTTP request with the appropriate status code (! HTTP 200)"
     ERR_ZBX_CLIENT_EXCEPTION = "HAL9000 attempted to use the ZabbixApi client, but an exception was thrown/handled: exception"
-    ZABBIX_ITEM_NOT_FOUND = "HAL9000 searched for an iteam w/ a particluar key and value, but did not find it. This is bad."
-    ZBXMON_KEY = 'Zabbix_status'
-    ZBXMON_PAYLOAD_LENGTH = 10
+    ZABBIX_ITEM_NOT_FOUND = "HAL9000 searched for an item w/ a particluar key and value, but did not find it. This is bad."
+    ZBXMON_PAYLOAD_LENGTH = 20
     
     def initialize(redis:, zbx_client:, log:, zbx_host:, zbx_username:, zbx_password:, datacenter:)
       @redis = redis
@@ -38,7 +37,7 @@ module Zabbix
         # the state reported back from this loop is important! soft_fail = keep trying; hard_fail = stop and notify
         sleep retry_interval_seconds
         monitor_success = retrieve_payload payload
-        @log.warn("[#{monitor_name}] FAILED to find #{ZBXMON_KEY} : #{payload} from the zabbix 'item' (#{retry_sz})") unless monitor_success
+        @log.warn("[#{monitor_name}] FAILED to find an item that contains #{payload} from the zabbix (#{retry_sz})") unless monitor_success
         retry_attempt_iterator += 1
         retry_sz = "retry attempt #{(retry_attempt_iterator + 1)} / #{num_retries}"
       end
@@ -73,18 +72,18 @@ module Zabbix
     def retrieve_payload(payload)
       begin # get zabbix item
         success = false
-        zbx_items = @client.get_item_by_key_and_lastvalue(ZBXMON_KEY, payload)
-        @log.debug("[#{monitor_name}] zabbix client 'got_item' successfully")
+        zbx_items = @client.get_item_by_lastvalue(payload)
       rescue => e
         @log.error("[#{monitor_name}] #{ERR_ZBX_CLIENT_EXCEPTION}".gsub('%exception%', e))
         @soft_failures.add("#{ERR_ZBX_CLIENT_EXCEPTION}".gsub('%exception%', e).gsub(@zbx_password, '**************'))
       end rescue StandardError # consume the exception and continue; zbx_items being nil is okay from here
       if zbx_items
         if zbx_items.length > 0  # success case
-          @log.info("[#{monitor_name}] successfully observed '#{ZBXMON_KEY} : #{payload}' from #{@zbx_host} ")
+          @log.info("[#{monitor_name}] successfully observed #{payload} from #{@zbx_host} ")
           success = true
         else # fail case
           @soft_failures.add("#{ZABBIX_ITEM_NOT_FOUND}")
+          @log.error("[#{monitor_name}] zbx_items='#{zbx_items}'")
         end
       else # fail case
         @soft_failures.add("#{ZABBIX_ITEM_NOT_FOUND}")
