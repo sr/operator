109675f9fb2672c760608a80f186ba8aed767151
rkiefer <rkiefer@salesforce.com>
2016-05-04 14:40:59 -0700

    BREAD-891

--- src/pull_agent/lib/pardot/pull_agent/helpers/storm.rb
+++ src/pull_agent/lib/pardot/pull_agent/helpers/storm.rb
@@ -7,14 +7,14 @@ module Pardot
       PROC_KILL_WAIT_TIME = 45
       STORM_BIN = "/opt/storm/current/bin/storm".freeze
 
-      def load_topology(topo, jar)
-        Logger.log(:info, "Loading Topology #{topo_name(topo)} : #{topo_class(topo)}")
+      def load_topology(topo, topo_env, jar)
+        Logger.log(:info, "Loading Topology #{topo_name(topo)} : #{topo_class(topo)} in environment #{topo_env}")
         if active?(topo)
           remove_topology(topo)
           Logger.log(:info, "Topology #{topo_name(topo)} killed! Waiting #{PROC_KILL_WAIT_TIME} seconds to resume deploy")
           sleep PROC_KILL_WAIT_TIME + 2
         end
-        add_topology(topo, jar)
+        add_topology(topo, topo_env, jar)
       end
 
       private
@@ -39,8 +39,8 @@ module Pardot
         topo_active
       end
 
-      def add_topology(topo, jar)
-        add_topo_command = [STORM_BIN, "jar", "-c", "env=prod", jar, "com.pardot.storm.topology.TopologyRunner", "--topo-def=#{topo_class(topo)}", "--name=#{topo_name(topo)}", "--remote"]
+      def add_topology(topo, topo_env, jar)
+        add_topo_command = [STORM_BIN, "jar", "-c", "env=#{topo_env}", jar, "com.pardot.storm.topology.TopologyRunner", "--topo-def=#{topo_class(topo)}", "--name=#{topo_name(topo)}", "--remote"]
         add_topo_output = ShellHelper.sudo_execute(add_topo_command, "storm", err: [:child, :out])
         Logger.log(:info, "Topology Deploy Routine Command:\n#{add_topo_command}\n")
         Logger.log(:info, "Topology Deploy Routine Output:\n#{add_topo_output}\n")
