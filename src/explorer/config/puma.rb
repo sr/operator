cc1e65e6cfb88a8d181be4cd52a901d4166af044
Simon Rozet <srozet@salesforce.com>
2016-05-05 15:08:34 +0200

    Configure Puma to redirect output to log file

--- src/explorer/config/puma.rb
+++ src/explorer/config/puma.rb
@@ -1,12 +1,15 @@
-workers Integer(ENV["WEB_CONCURRENCY"] || 2)
-threads_count = Integer(ENV["MAX_THREADS"] || 5)
+workers Integer(ENV.fetch("WEB_CONCURRENCY", 2))
+threads_count = Integer(ENV.fetch("MAX_THREADS", 5))
 threads threads_count, threads_count
 
 preload_app!
 
 rackup      DefaultRackup
-port        ENV["PORT"]     || 4000
-environment ENV["RACK_ENV"] || "development"
+port        ENV.fetch("PORT", 4000)
+environment ENV.fetch("RACK_ENV", "development")
+
+log = File.expand_path("../../log/#{ENV.fetch("RACK_ENV")}.log", __FILE__)
+redirect_stdout log, log, true
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
