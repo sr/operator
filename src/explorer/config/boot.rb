6a6c584749d58a91216e5f49442b92edeb89dbb4
Simon Rozet <srozet@salesforce.com>
2016-05-11 11:22:49 +0200

    explorer: Load envvars file when present

--- src/explorer/config/boot.rb
+++ src/explorer/config/boot.rb
@@ -1,3 +1,9 @@
+rails_env = ENV.fetch("RAILS_ENV")
+env_file = File.expand_path("../../.envvars_#{rails_env}.rb", __FILE__)
+if File.exist?(env_file)
+  load env_file
+end
+
 # Set up gems listed in the Gemfile.
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../Gemfile", __FILE__)
 
