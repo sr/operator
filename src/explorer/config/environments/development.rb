c7574f77e861bb5afd4d1dbbb65839d7cb11dda1
Andy Lindeman <alindeman@salesforce.com>
2016-06-08 17:34:07 -0400

    Explorer is not making use of Web Console right now

--- src/explorer/config/environments/development.rb
+++ src/explorer/config/environments/development.rb
@@ -1,7 +1,4 @@
 Rails.application.configure do
-  # Allow web-console within Docker.
-  config.web_console.whitelisted_ips = "10.0.2.0/24"
-
   # Temporary workaround for file loading not working correctly in a container.
   config.reload_classes_only_on_change = false
 
