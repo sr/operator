69b86ee5c4878c663a6abd760f3e3ebc51787540
Simon Rozet <srozet@salesforce.com>
2016-06-15 10:29:44 +0200

    Define FakeHipchatNotifier inline

--- src/canoe/app/models/fake_chef_delivery_config.rb
+++ src/canoe/app/models/fake_chef_delivery_config.rb
@@ -9,6 +9,20 @@ class FakeChefDeliveryConfig < ChefDeliveryConfig
     %w[testing].include?(environment) && hostname != "disabled"
   end
 
+  class FakeHipchatNotifier
+    def initialize
+      @messages = []
+    end
+
+    attr_reader :messages
+
+    Message = Struct.new(:room_id, :message)
+
+    def notify_room(room_id, message, color = nil)
+      @messages << Message.new(room_id, message)
+    end
+  end
+
   def notifier
     @notifier ||= FakeHipchatNotifier.new
   end
