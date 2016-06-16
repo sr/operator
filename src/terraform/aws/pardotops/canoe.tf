163d33f4d955c8fad15c90e8f0b54ac3285d822e
Simon Rozet <srozet@salesforce.com>
2016-06-16 21:40:15 +0200

    Allow access at the SG level instead

--- src/terraform/aws/pardotops/canoe.tf
+++ src/terraform/aws/pardotops/canoe.tf
@@ -128,6 +128,16 @@ resource "aws_security_group" "canoe_app_production" {
     ]
   }
 
+  # HTTP from the Chef servers
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    security_groups = [
+      "${aws_security_group.internal_apps_chef_server.id}"
+    ]
+  }
+
   ingress {
     from_port = 80
     to_port = 80
