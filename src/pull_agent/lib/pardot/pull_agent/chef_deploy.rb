89936b258aa1a055500799cb4e2bd100f4b60346
Simon Rozet <srozet@salesforce.com>
2016-06-16 15:26:42 +0200

    Update Chef deploy script to set --skip-berksfile and --environment-file options

--- src/pull_agent/lib/pardot/pull_agent/chef_deploy.rb
+++ src/pull_agent/lib/pardot/pull_agent/chef_deploy.rb
@@ -3,6 +3,12 @@ module Pardot
     class ChefDeploy
       Response = Struct.new(:success, :message)
 
+      CHEF_ENVIRONMENT_FILE = {
+        "dfw" => "environments/dfw/production.rb",
+        "phx" => "environments/phx/production.rb",
+        "ue1.aws" => "environments/aws/production.rb"
+      }
+
       def initialize(script, checkout_path, deploy)
         @script = script
         @checkout_path = checkout_path
@@ -10,12 +16,24 @@ module Pardot
       end
 
       def apply(env)
+        hostname = ShellHelper.hostname
+        datacenter = hostname.split("-")[3]
+        if !datacenter
+          return Response.new(false, "Unable to determine datacenter from hostname: #{hostname.inspect}")
+        end
+
+        chef_environment_file = CHEF_ENVIRONMENT_FILE[datacenter]
+        if !chef_environment_file
+          return Response.new(false, "Unable to determine location of chef environment file for datacenter: #{datacenter.inspect}")
+        end
+
         output = ShellHelper.execute([
           env,
           @script,
           "-d", @checkout_path.to_s,
           "-b", @deploy["branch"],
           "-s", @deploy["sha"],
+          "-f", chef_environment_file,
           "deploy"
         ])
         if !$?.success?
