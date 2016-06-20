f464e7f3feabf3d9e6c9037a8d8efe00cf0638c8
Simon Rozet <srozet@salesforce.com>
2016-06-20 17:55:32 +0200

    Fucking sane script/test output

--- src/canoe/config/load_envvars.rb
+++ src/canoe/config/load_envvars.rb
@@ -3,18 +3,14 @@
   found_env = false
 
   env_filename = File.expand_path("~/.envvars_canoe_#{env}")
-  puts "ENV LOAD: #{env_filename}"
   if File.exist?(env_filename)
-    puts "LOADING...."
     load(env_filename)
     found_env = true
   end
 
   # load local env files, note this will step on anything in user dir
   env_filename = File.expand_path(File.join(File.dirname(__FILE__), "..", ".envvars_#{env}.rb").to_s)
-  puts "ENV LOAD: #{env_filename}"
   if File.exist?(env_filename)
-    puts "LOADING...."
     load(env_filename)
     found_env = true
   end
