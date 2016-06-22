a6939bc38adcb754d869f1bae9a9d1c43626c6a7
Simon Rozet <srozet@salesforce.com>
2016-06-22 18:24:23 +0200

    Enable Chef auto-deploy in DFW

--- src/canoe/app/models/chef_delivery_config.rb
+++ src/canoe/app/models/chef_delivery_config.rb
@@ -2,7 +2,7 @@ class ChefDeliveryConfig
   PRODUCTION = "production".freeze
   DEV = "dev".freeze
   AWS = "ue1.aws".freeze
-  ENABLED = [AWS].freeze
+  ENABLED = [AWS, DFW].freeze
 
   BREAD_ROOM = 42
   OPS_ROOM = 6
