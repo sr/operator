40d7f06bcd14ffc9f927759bcb5386ebb99214d2
Andy Lindeman <alindeman@salesforce.com>
2016-06-22 17:01:44 -0700

    BREAD-972: Avoids giving Cassandra a ton of resources
    Since Cassandra is going away soon, I'm not spending time making these
    configurable. Cassandra is _only_ used in test, and in test we don't need it to
    have production values.
    
    Original-Commit: 5cbe851d3e489f8d7bd0f6227867f4575c950b53
    Original-Commit-URL: https://git.dev.pardot.com/Pardot/docker-library/commit/5cbe851d3e489f8d7bd0f6227867f4575c950b53

--- docker/java-services/cassandra/entrypoint.sh
+++ docker/java-services/cassandra/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
-set -x
 
 PROGRAM="${1-}"
 if [ "$PROGRAM" = "/usr/sbin/cassandra" ]; then
@@ -10,6 +9,8 @@ if [ "$PROGRAM" = "/usr/sbin/cassandra" ]; then
   sed -i'' -E "s/.*start_rpc:.*/start_rpc: true/" /etc/cassandra/conf/cassandra.yaml
   sed -i'' -E "s/(.*)- seeds:.*/\1- seeds: \"${IP}\"/" /etc/cassandra/conf/cassandra.yaml
   sed -i'' -E "s/.*endpoint_snitch:.*/endpoint_snitch: PropertyFileSnitch/" /etc/cassandra/conf/cassandra.yaml
+  sed -i'' -E "s/^#?MAX_HEAP_SIZE=.*/MAX_HEAP_SIZE=\"256M\"/" /etc/cassandra/conf/cassandra-env.sh
+  sed -i'' -E "s/^#?HEAP_NEWSIZE=.*/HEAP_NEWSIZE=\"100M\"/" /etc/cassandra/conf/cassandra-env.sh
 
   cat >/etc/cassandra/conf/cassandra-topology.properties <<EOF
 # Cassandra Node IP=Data Center:Rack
