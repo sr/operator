f681677729e15c755ec4d56c0529b3b8f8c2d43c
Jan Ulrich <julrich@salesforce.com>
2016-07-11 14:11:30 -0700

    BREAD-917 Use constants instead of enums

--- src/explorer/test/models/global_setting_test.rb
+++ src/explorer/test/models/global_setting_test.rb
@@ -8,7 +8,7 @@ class GlobalSettingTest < ActiveSupport::TestCase
   test "secondary_db_id site-switch" do
     assert_equal "prod-s", Datacenter.current.symfony_name
     GlobalSetting.transaction do
-      GlobalSetting.create(setting_key: "prod-s shard3 datacenter", setting_value: 2)
+      GlobalSetting.create(setting_key: "prod-s shard3 datacenter", setting_value: GlobalSetting::SERVER_SETTING_2)
       assert_equal 1, GlobalSetting.secondary_db_id(3)
       raise ActiveRecord::Rollback
     end
@@ -16,7 +16,7 @@ class GlobalSettingTest < ActiveSupport::TestCase
 
   test "secondary_db_id no-failover" do
     GlobalSetting.transaction do
-      GlobalSetting.create(setting_key: "prod-s shard3 datacenter", setting_value: 3)
+      GlobalSetting.create(setting_key: "prod-s shard3 datacenter", setting_value: GlobalSetting::SERVER_SETTING_1_ONLY)
       assert_equal 1, GlobalSetting.secondary_db_id(3)
       raise ActiveRecord::Rollback
     end
