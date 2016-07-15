954d3834d3ecbff73af5d250d313fa7d9b30edd1
rkiefer <rkiefer@salesforce.com>
2016-07-15 11:04:59 -0700

    where Ive landed on policy for now -- trying broad denies at the top next

--- src/terraform/aws/pardotops/ci.tf
+++ src/terraform/aws/pardotops/ci.tf
@@ -9,58 +9,65 @@ resource "aws_iam_user_policy" "bamboo_sysacct_access_rights" {
 {
   "Version": "2012-10-17",
   "Statement": [
-    {
-      "Sid": "BambooAllowedAllInVPC",
-      "Effect": "Allow",
-      "Action": [
-        "ec2:RequestSpot*",
-        "ec2:CancelSpot*",
-        "ec2:AllocateAddress",
-        "ec2:AssociateAddress",
-        "ec2:AttachVolume",
-        "ec2:AuthorizeSecurityGroupIngress",
-        "ec2:CancelSpotInstanceRequests",
-        "ec2:CreateSecurityGroup",
-        "ec2:CreateVolume",
-        "ec2:DeleteVolume",
-        "ec2:DescribeAddresses",
-        "ec2:DescribeAvailabilityZones",
-        "ec2:DescribeInstances",
-        "ec2:DescribeSecurityGroups",
-        "ec2:DescribeVolumes",
-        "ec2:DescribeVpcs",
-        "ec2:GetConsoleOutput",
-        "ec2:ModifyInstanceAttribute",
-        "ec2:RequestSpotInstances",
-        "ec2:RunInstances",
-        "ec2:TerminateInstances",
-        "ec2:StopInstances",
-        "ec2:StartInstances"
-      ],
-      "Resource": "*",
-      "Condition": {
-        "StringEquals": {
-          "ec2:Vpc": "arn:aws:ec2:us-east-1:${var.pardotops_account_number}:vpc/${aws_vpc.pardot_ci.id}"
-        }
+  {
+    "Sid": "AllowBambooToStartNewBuildAgents",
+    "Effect": "Allow",
+    "Action": [
+      "ec2:Describe*",
+      "ec2:RequestSpot*",
+      "ec2:CancelSpot*",
+      "ec2:CancelSpotInstanceRequests",
+      "ec2:CreateKeyPair",
+      "ec2:CreateTags",
+      "ec2:RequestSpotInstances",
+      "ec2:RunInstances"
+    ],
+    "Resource": "*"
+  },
+  {
+    "Sid": "OnlyAllowBambooToAffectWhitelistedSubnets",
+    "Effect": "Allow",
+    "Action": [
+      "ec2:ModifyInstanceAttribute",
+      "ec2:GetConsoleOutput"
+    ],
+    "Resource": "*",
+    "Condition": {
+      "StringEquals": {
+        "ec2:Vpc": "arn:aws:ec2:us-east-1:${var.pardotops_account_number}:vpc/${aws_vpc.pardot_ci.id}"
       }
-    },
-    {
-      "Sid": "BambooNonVPC",
-      "Effect": "Allow",
-      "Action": [
-        "ec2:Describe*",
-        "ec2:DescribeSubnets",
-        "ec2:CreateKeyPair",
-        "ec2:CreateTags",
-        "ec2:DescribeImages",
-        "ec2:DescribeKeyPairs",
-        "ec2:DescribeRegions",
-        "ec2:DescribeSpotInstanceRequests",
-        "ec2:DescribeSpotPriceHistory",
-        "ec2:ReleaseAddress"
-      ],
-      "Resource": "*"
     }
+  },
+  {
+    "Sid": "OnlyAllowBambooToAffectWhitelistedVPCs",
+    "Effect": "Allow",
+    "Action": [
+      "ec2:CreateSecurityGroup",
+      "ec2:DeleteSecurityGroup",
+      "ec2:AuthorizeSecurityGroupIngress"
+    ],
+    "Resource": "*",
+    "Condition": {
+      "StringEquals": {
+        "ec2:Subnet": "arn:aws:ec2:us-east-1:${var.pardotops_account_number}:vpc/${aws_subnet.pardot_ci_us_east_1c_dmz.id}"
+      }
+    }
+  },
+  {
+    "Sid": "OnlyAllowBambooToTerminateBuildAgents",
+    "Effect": "Allow",
+    "Action": [
+      "ec2:TerminateInstances",
+      "ec2:StopInstances",
+      "ec2:StartInstances"
+    ],
+    "Resource": "arn:aws:ec2:us-east-1:${var.pardotops_account_number}:instance/*",
+    "Condition": {
+      "StringEquals": {
+        "ec2:ResourceTag/Name": "bam::pandafood.dev.pardot.com::root"
+      }
+    }
+  }
   ]
 }
 EOF
