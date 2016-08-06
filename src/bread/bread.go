8ad419b0be0fb712f2a5c77dd4ecb8b0c85c1709
Simon Rozet <srozet@salesforce.com>
2016-08-06 17:46:05 +0200

    Introduce bread.ChatClient interface

--- src/bread/bread.go
+++ src/bread/bread.go
@@ -1,6 +1,9 @@
 package bread
 
 import (
+	"errors"
+	"os"
+
 	"github.com/sr/operator"
 	"google.golang.org/grpc"
 )
@@ -11,6 +14,22 @@ const (
 	PublicRoom  = 42  // Build & Automate
 )
 
+var (
+	ErrHipchatTokenMissing = errors.New("required environment variable missing: HIPCHAT_TOKEN")
+)
+
+type ChatClient interface {
+	SendRoomNotification(*ChatRoomNotification) error
+}
+
+type ChatRoomNotification struct {
+	Color         string `json:"color"`
+	From          string `json:"from"`
+	Message       string `json:"message"`
+	MessageFormat string `json:"message_format"`
+	RoomId        int    `json:"-"`
+}
+
 func NewOperatorServer(logger operator.Logger) *grpc.Server {
 	return grpc.NewServer(
 		grpc.UnaryInterceptor(
@@ -20,4 +39,12 @@ func NewOperatorServer(logger operator.Logger) *grpc.Server {
 			),
 		),
 	)
+}
+
+func NewChatClient() (ChatClient, error) {
+	token, ok := os.LookupEnv("HIPCHAT_TOKEN")
+	if !ok {
+		return nil, ErrHipchatTokenMissing
+	}
+	return newHipchatClient(HipchatHost, token), nil
 }
