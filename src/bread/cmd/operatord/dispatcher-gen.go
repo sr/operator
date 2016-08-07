bb490c10426b63187fc9c238652099f8a19d3c29
Simon Rozet <srozet@salesforce.com>
2016-08-07 18:49:30 +0200

    Continue fleshing out webhook support

--- src/bread/cmd/operatord/dispatcher-gen.go
+++ src/bread/cmd/operatord/dispatcher-gen.go
@@ -1,5 +1,26 @@
 package main
 
-func requestDispatcher(path string) bool {
-	return false
+import (
+	"bread/ping"
+
+	"github.com/sr/operator"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+func invoker(conn *grpc.ClientConn, call string, msg *operator.Message) (bool, error) {
+	switch call {
+	case "ping ping":
+		c := breadping.NewPingerClient(conn)
+		_, err := c.Ping(
+			context.Background(),
+			&breadping.PingRequest{
+				Source: msg.Source,
+			},
+		)
+		if err != nil {
+			return true, err
+		}
+	}
+	return false, nil
 }
--- src/bread/cmd/operatord/main.go
+++ src/bread/cmd/operatord/main.go
@@ -8,32 +8,35 @@ import (
 	"net/http"
 	"os"
 
+	"google.golang.org/grpc"
+
 	"github.com/sr/operator"
 	"github.com/sr/operator/hipchat"
 )
 
-func run(builder operator.ServerBuilder, dispatcher operator.RequestDispatcher) error {
-	config := &operator.Config{}
+var (
+	grpcAddr string
+	httpAddr string
+	prefix   string
+)
+
+func run(builder operator.ServerBuilder, invoker operator.Invoker) error {
 	flags := flag.CommandLine
-	flags.StringVar(&config.Address, "listen-addr", operator.DefaultAddress, "Listen address of the operator server")
+	flags.StringVar(&grpcAddr, "grpc-addr", ":9000", "Listen address of the operator gRPC server")
+	flags.StringVar(&httpAddr, "http-addr", ":8080", "Listen address of the HTTP webhook server. Optional.")
+	flags.StringVar(&prefix, "prefix", "!", "The prefix used to denote a command invocation in chat messages")
 	logger := operator.NewLogger()
 	server := bread.NewOperatorServer()
-	recv := http.NewServeMux()
-	recv.Handle(
-		"/hipchat",
-		operator.NewHandler(
-			logger,
-			operator.NewInstrumenter(logger),
-			bread.NewLDAPAuthorizer(),
-			operatorhipchat.NewRequestDecoder(),
-			dispatcher,
-		),
-	)
-	msg := &operator.ServerStartupNotice{Protocol: "tcp"}
+	errC := make(chan error)
+	msg := &operator.ServerStartupNotice{Protocol: "grpc"}
 	services, err := builder(server, flags)
 	if err != nil {
 		return err
 	}
+	if grpcAddr == "" {
+		return fmt.Errorf("required flag missing: grpc-addr")
+	}
+	msg.Address = grpcAddr
 	for svc, err := range services {
 		if err != nil {
 			logger.Error(&operator.ServiceStartupError{
@@ -44,27 +47,44 @@ func run(builder operator.ServerBuilder, dispatcher operator.RequestDispatcher) 
 			msg.Services = append(msg.Services, &operator.Service{Name: svc})
 		}
 	}
-	msg.Address = config.Address
-	if config.Address == "" {
-		return fmt.Errorf("required -listen-addr flag is missing")
-	}
-	errC := make(chan error)
-	listener, err := net.Listen("tcp", config.Address)
+	listener, err := net.Listen("tcp", grpcAddr)
 	if err != nil {
 		return err
 	}
-	logger.Info(msg)
 	go func() {
 		errC <- server.Serve(listener)
 	}()
-	go func() {
-		errC <- http.ListenAndServe(":8000", recv)
-	}()
+	logger.Info(msg)
+	if httpAddr != "" {
+		conn, err := grpc.Dial(grpcAddr, grpc.WithInsecure())
+		if err != nil {
+			return err
+		}
+		disp, err := operator.NewMessageDispatcher(logger, conn, prefix, invoker)
+		if err != nil {
+			return err
+		}
+		mux := http.NewServeMux()
+		mux.Handle(
+			"/hipchat",
+			operator.NewHandler(
+				logger,
+				operator.NewInstrumenter(logger),
+				bread.NewLDAPAuthorizer(),
+				operatorhipchat.NewRequestDecoder(),
+				disp,
+			),
+		)
+		go func() {
+			errC <- http.ListenAndServe(httpAddr, mux)
+		}()
+		logger.Info(&operator.ServerStartupNotice{Protocol: "http", Address: httpAddr})
+	}
 	return <-errC
 }
 
 func main() {
-	if err := run(buildOperatorServer, requestDispatcher); err != nil {
+	if err := run(buildOperatorServer, invoker); err != nil {
 		fmt.Fprintf(os.Stderr, "operatord: %s\n", err)
 		os.Exit(1)
 	}
