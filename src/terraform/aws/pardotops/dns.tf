041a4bbf3636473e9f64f44aa054bafc875d9b5e
Andy Lindeman <alindeman@salesforce.com>
2016-08-10 16:51:15 -0400

    BREAD-1168: Restoring TTL

--- src/terraform/aws/pardotops/dns.tf
+++ src/terraform/aws/pardotops/dns.tf
@@ -42,7 +42,7 @@ resource "aws_route53_record" "artifactory_dev_pardot_com_CNAMErecord" {
   name = "artifactory.${aws_route53_zone.dev_pardot_com.name}"
   records = ["${aws_elb.external_artifact_cache_lb.dns_name}"]
   type = "CNAME"
-  ttl = "60"
+  ttl = "900"
 }
 
 resource "aws_route53_record" "artifactory-origin_dev_pardot_com_Arecord" {
