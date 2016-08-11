b755a0ac9f7dda0b1d0ddfc60791ec5c2e96a919
Simon Rozet <srozet@salesforce.com>
2016-08-11 10:06:20 +0200

    Parse Hipchat webhook JSON payload and add increase test coverage

--- operator.go
+++ operator.go
@@ -4,12 +4,10 @@ import (
 	"errors"
 	"flag"
 	"net/http"
-	"time"
 
 	"google.golang.org/grpc"
 
 	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/ptypes"
 )
 
 const DefaultAddress = "localhost:9000"
@@ -30,14 +28,10 @@ type Logger interface {
 }
 
 type RequestDecoder interface {
-	Decode(*http.Request) (*Request, error)
+	Decode(*http.Request) (*Message, error)
 }
 
-type MessageDispatcher interface {
-	Dispatch(*Message) (bool, error)
-}
-
-type Invoker func(conn *grpc.ClientConn, call string, msg *Message) (bool, error)
+type Invoker func(conn *grpc.ClientConn, req *Request) (bool, error)
 
 type ServerBuilder func(server *grpc.Server, flags *flag.FlagSet) (map[string]error, error)
 
@@ -70,14 +64,6 @@ type MethodCommand struct {
 	Run      func(*CommandContext) (string, error)
 }
 
-type Handler struct {
-	logger       Logger
-	instrumenter Instrumenter
-	authorizer   Authorizer
-	decoder      RequestDecoder
-	dispatcher   MessageDispatcher
-}
-
 func NewCommand(name string, services []ServiceCommand) Command {
 	return Command{name, services}
 }
@@ -95,50 +81,17 @@ func NewHandler(
 	instrumenter Instrumenter,
 	authorizer Authorizer,
 	decoder RequestDecoder,
-	dispatcher MessageDispatcher,
-) http.Handler {
-	return &Handler{
+	prefix string,
+	conn *grpc.ClientConn,
+	invoker Invoker,
+) (http.Handler, error) {
+	return newHandler(
 		logger,
 		instrumenter,
 		authorizer,
 		decoder,
-		dispatcher,
-	}
-}
-
-func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	req, err := h.decoder.Decode(r)
-	if err != nil {
-		// TODO(sr) Log decoding error
-		return
-	}
-	if err := h.authorizer.Authorize(req); err != nil {
-		// TODO(sr) Log unauthorized error
-		return
-	}
-	start := time.Now()
-	ok, err := h.dispatcher.Dispatch(&Message{Source: req.Source, Text: "TODO"})
-	if !ok {
-		// TODO(sr) Log unhandled message
-		return
-	}
-	if err != nil {
-		req.Call.Error = &Error{Message: err.Error()}
-	}
-	req.Call.Duration = ptypes.DurationProto(time.Since(start))
-	h.instrumenter.Instrument(req)
-}
-
-func NewMessageDispatcher(
-	logger Logger,
-	conn *grpc.ClientConn,
-	prefix string,
-	invoker Invoker,
-) (MessageDispatcher, error) {
-	return newMessageDispatcher(
-		logger,
-		conn,
 		prefix,
+		conn,
 		invoker,
 	)
 }
