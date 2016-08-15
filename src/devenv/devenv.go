755e0bb662494c1b970fb237803f1fdfc750e455
Andy Lindeman <alindeman@salesforce.com>
2016-08-15 10:03:56 -0400

    BREAD-1157: Cleanup recommended by code review

--- src/devenv/devenv.go
+++ src/devenv/devenv.go
@@ -1,25 +1,21 @@
 package devenv
 
 import (
-	"fmt"
 	"os"
 	"path"
 )
 
 const (
 	Version = "0.0.0"
+
+	preferencesSubdirectory = ".devenv"
 )
 
 // EnsurePersistentDirectoryCreated ensures a directory underneath ~/.devenv is
 // created and ready for use. If clear is true, the directory is recursively
 // removed and recreated.
 func EnsurePersistentDirectoryCreated(name string, clear bool) (string, error) {
-	home := os.Getenv("HOME")
-	if len(home) <= 0 {
-		return "", fmt.Errorf("unable to retrieve the value of $HOME")
-	}
-
-	pathname := path.Join(home, ".devenv", name)
+	pathname := path.Join(os.Getenv("HOME"), preferencesSubdirectory, name)
 
 	if clear {
 		if err := os.RemoveAll(pathname); err != nil {
