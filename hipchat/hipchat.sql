00d569928225ebc61ff3543d190585e00896f248
Simon Rozet <srozet@salesforce.com>
2016-08-22 02:54:18 +0200

    Bring in SQL schema for hipchat credentials store

---
+++ hipchat/hipchat.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS hipchat_client_credentials (
+	id integer PRIMARY KEY NOT NULL,
+	created_at datetime NOT NULL,
+	deleted_at datetime NULL,
+	addon_id varchar(255) NOT NULL,
+	oauth_id varchar(255) NOT NULL,
+	oauth_secret varchar(255) NOT NULL,
+	UNIQUE(addon_id),
+	UNIQUE(oauth_id),
+	UNIQUE(oauth_secret)
+);
