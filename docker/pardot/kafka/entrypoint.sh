7383ee07eafaa531940248520ea74859ffa5a77d
Andy Lindeman <alindeman@salesforce.com>
2016-08-26 10:17:42 -0400

    BREAD-1079: Retries creating a topic if broker isn't totally ready yet
    Original-Commit: 5f5cf7e490ee296f665e9a9bc7a1ced42781b598
    Original-Commit-URL: https://git.dev.pardot.com/Pardot/docker-library/commit/5f5cf7e490ee296f665e9a9bc7a1ced42781b598

--- docker/pardot/kafka/entrypoint.sh
+++ docker/pardot/kafka/entrypoint.sh
@@ -13,15 +13,29 @@ if [ "$program" = "supervisord" ] && [ ! -e "/opt/kafka/DOCKER-SETUP" ]; then
   trap "kill -TERM ${pid}" SIGTERM
 
   # Wait for the list of topics to come back successfully
-  ${KAFKA_TOPICS_CMD} --list
+  ${KAFKA_TOPICS_CMD} --list &>/dev/null
 
   IFS=' ' read -ra topics_array <<< "$KAFKA_TOPICS"
   if [ "${#topics_array[@]}" -gt 0 ]; then
     for topic in "${topics_array[@]}"; do
-      IFS=':' read -ra arr <<< "$topic"
+      IFS=':' read -ra arr <<<"$topic"
 
       if ! ${KAFKA_TOPICS_CMD} --list | grep -q "^${arr[0]}$"; then
-        ${KAFKA_TOPICS_CMD} --create --partitions=1 --replication-factor=1 --topic="${arr[0]}"
+        while true; do
+          echo "Attempting to create topic: ${arr[0]}"
+          set +e
+          output="$(${KAFKA_TOPICS_CMD} --create --partitions=1 --replication-factor=1 --topic="${arr[0]}")"
+          ret="$?"
+          set -e
+
+          if [ "$ret" -eq 0 ]; then
+            break
+          else
+            echo "Unable to create topic: $output"
+            echo "Retrying ..."
+            sleep 1
+          fi
+        done
       fi
     done
   fi
