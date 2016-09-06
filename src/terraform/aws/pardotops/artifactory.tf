ce9aeeade11f37468b2f6a17c946cbfc600ac535
Andy Lindeman <alindeman@salesforce.com>
2016-09-06 15:35:04 -0400

    BREAD-1204: Removes Softlayer IPs from security groups

--- src/terraform/aws/pardotops/artifactory.tf
+++ src/terraform/aws/pardotops/artifactory.tf
@@ -100,9 +100,6 @@ resource "aws_security_group" "artifactory_dc_only_http_lb" {
     to_port = 80
     protocol = "tcp"
     cidr_blocks = [
-      "173.192.141.222/32", # tools-s1 (prodbot)
-      "174.37.191.2/32",    # proxy.dev
-      "169.45.0.88/32",     # squid-d4
       "136.147.104.20/30",  # pardot-proxyout1-{1,2,3,4}-dfw
       "136.147.96.20/30"    # pardot-proxyout1-{1,2,3,4}-phx
     ]
@@ -113,10 +110,6 @@ resource "aws_security_group" "artifactory_dc_only_http_lb" {
     to_port = 443
     protocol = "tcp"
     cidr_blocks = [
-      "173.192.141.222/32", # tools-s1 (prodbot)
-      "208.43.203.134/32",  # email-d1 (replication check)
-      "174.37.191.2/32",    # proxy.dev
-      "169.45.0.88/32",     # squid-d4
       "136.147.104.20/30",  # pardot-proxyout1-{1,2,3,4}-dfw
       "136.147.96.20/30"    # pardot-proxyout1-{1,2,3,4}-phx
     ]
