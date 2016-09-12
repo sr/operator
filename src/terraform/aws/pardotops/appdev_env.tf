76498d9a4ebe9a905b4e465e1e9d3a7b7ea8fe31
Phoebe Kotliar <pkotliar@salesforce.com>
2016-09-12 12:23:37 -0400

    appdev_env changes

--- src/terraform/aws/pardotops/appdev_env.tf
+++ src/terraform/aws/pardotops/appdev_env.tf
@@ -1031,3 +1031,62 @@ resource "aws_route53_record" "appdev_consul1_arecord" {
   type = "A"
   ttl = "900"
 }
+
+resource "aws_iam_user" "artifactory_sysacct" {
+  name = "artifactorysysacct"
+}
+
+resource "aws_s3_bucket" "artifactory_s3_filestore" {
+  bucket = "artifactory_s3_filestore"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "allow artifactory sysacct",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "${aws_iam_user.artifactory_sysacct.arn}"
+      },
+      "Action": "s3:*",
+      "Resource": [
+        "arn:aws:s3:::artifactory_s3_filestore",
+        "arn:aws:s3:::artifactory_s3_filestore/*"
+      ]
+    }
+  ]
+}
+EOF
+  tags {
+    Name = "artifactory_s3_filestore"
+    terraform = "true"
+  }
+}
+
+resource "aws_s3_bucket" "artifactory_s3_filestore" {
+  bucket = "artifactory_s3_filestore"
+  acl = "private"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "allow artifactory sysacct",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "${aws_iam_user.artifactory_sysacct.arn}"
+      },
+      "Action": "s3:*",
+      "Resource": [
+        "arn:aws:s3:::artifactory_s3_filestore",
+        "arn:aws:s3:::artifactory_s3_filestore/*"
+      ]
+    }
+  ]
+}
+EOF
+  tags {
+    Name = "artifactory_s3_filestore"
+    terraform = "true"
+  }
+}
