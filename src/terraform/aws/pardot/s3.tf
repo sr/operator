dc8bbf3721c63b3d31f56370ef986c2b80c5041f
Jan Ulrich <julrich@salesforce.com>
2016-09-22 10:28:19 -0700

    BREAD-1389 Create a bucket for dev as well

--- src/terraform/aws/pardot/s3.tf
+++ src/terraform/aws/pardot/s3.tf
@@ -2,3 +2,9 @@ resource "aws_s3_bucket" "pardot_pdo" {
   bucket = "pardot-pdo"
   acl = "private"
 }
+
+# And the associated bucket used for dev & testing
+resource "aws_s3_bucket" "pardot_pdo_dev" {
+  bucket = "pardot-pdo-dev"
+  acl = "private"
+}
