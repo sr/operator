f94d1fcbd539939f417bd83a90ce04510d4663b8
Simon Rozet <srozet@salesforce.com>
2016-09-29 03:04:33 +0200

    More debugging

--- hipchat/replier.go
+++ hipchat/replier.go
@@ -2,6 +2,7 @@ package operatorhipchat
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/sr/operator"
 	"golang.org/x/net/context"
@@ -45,5 +46,6 @@ func (s *replier) Reply(ctx context.Context, src *operator.Source, rep string, m
 	if v, ok := msg.Options.(*MessageOptions); ok {
 		notif.MessageOptions = v
 	}
+	fmt.Printf("DEBUG hipchat Reply %#v\n", notif)
 	return client.SendRoomNotification(ctx, notif)
 }
