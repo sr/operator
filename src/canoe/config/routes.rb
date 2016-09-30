03223af6819420c459ba7c3bfe3f57bd67ec4d85
Simon Rozet <srozet@salesforce.com>
2016-09-30 13:28:24 +0200

    Add GET /api/projects Canoe endpoint

--- src/canoe/config/routes.rb
+++ src/canoe/config/routes.rb
@@ -56,6 +56,7 @@ Rails.application.routes.draw do
   post "/api/chef/complete_deploy", to: "api/chef_deploys#complete_deploy"
   post "/api/chef/knife", to: "api/chef_deploys#knife"
   post "/api/targets/:target_name/deploys", to: "api/deploys#create"
+  get "/api/projects", to: "api/projects#index"
 
   get "/_boomtown", to: "projects#boomtown"
   root to: "projects#index"
