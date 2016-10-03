4526cb3916cc496e0e2fd8dfc2a01fc21b97a581
Simon Rozet <srozet@salesforce.com>
2016-10-04 01:46:21 +0200

    Take advantage of subtests

--- src/bread/bread_test.go
+++ src/bread/bread_test.go
@@ -104,34 +104,35 @@ func TestAuthorizer(t *testing.T) {
 		{swe, "bread.Ping", "Otp", validOTP, false, errors.New("could not verify Yubikey OTP: boomtown")},
 		{swe, "bread.Ping", "Otp", "garbage", true, errors.New("could not verify Yubikey OTP: boomtown")},
 	} {
-		if !tc.otpOK {
-			otpVerifier.fail()
-		}
-		err := auth.Authorize(context.Background(), &operator.Request{
-			Call: &operator.Call{
-				Service: tc.service,
-				Method:  tc.method,
-			},
-			Otp: tc.otp,
-			Source: &operator.Source{
-				Type: operator.SourceType_HUBOT,
-				User: &operator.User{
-					Email: tc.user,
+		t.Run(fmt.Sprintf("%s %s", tc.service, tc.method), func(t *testing.T) {
+			if !tc.otpOK {
+				otpVerifier.fail()
+			}
+			err := auth.Authorize(context.Background(), &operator.Request{
+				Call: &operator.Call{
+					Service: tc.service,
+					Method:  tc.method,
 				},
-			},
+				Otp: tc.otp,
+				Source: &operator.Source{
+					Type: operator.SourceType_HUBOT,
+					User: &operator.User{
+						Email: tc.user,
+					},
+				},
+			})
+			if err == nil {
+				if tc.err != nil {
+					t.Errorf("user %#v should not be authorized", tc.user)
+				}
+			} else {
+				if tc.err == nil {
+					t.Errorf("unexpected error: %s", err)
+				} else if err.Error() != tc.err.Error() {
+					t.Errorf("expected error message %#v, got %#v", tc.err.Error(), err.Error())
+				}
+			}
 		})
-		if err == nil {
-			if tc.err != nil {
-				t.Errorf("user %#v should not be authorized to request %s.%s", tc.user, tc.service, tc.method)
-			}
-		} else {
-			if tc.err == nil {
-				t.Fatalf("unexpected error: %s", err)
-			}
-			if err.Error() != tc.err.Error() {
-				t.Errorf("expected error message %#v, got %#v", tc.err.Error(), err.Error())
-			}
-		}
 	}
 }
 
@@ -261,27 +262,28 @@ func TestDeploy(t *testing.T) {
 		{"pardot", "old", "", errors.New("refusing to deploy build")},
 		{"bread", "1", "", errors.New("No such deployment target: bread")},
 	} {
-		resp, err := server.Trigger(context.Background(), &breadpb.TriggerRequest{
-			Request: &operator.Request{Source: &operator.Source{}},
-			Target:  tc.target,
-			Build:   tc.build,
-		})
-		if tc.err == nil {
-			if err != nil {
-				t.Fatalf("expected no error for target=%s build=%s, got: %s", tc.target, tc.build, err)
-			}
-			if resp.Message != tc.resp {
-				t.Fatalf("expected response message `%s` for target=%s build=%s but got: %#v", tc.resp, tc.target, tc.build, resp.Message)
-			}
-		} else {
-			if err == nil {
-				t.Errorf("expect an error for target=%s build=%s but got none", tc.target, tc.build)
-				if resp.Message != tc.resp {
-					t.Fatalf("expected response message `%s` for target=%s build=%s but got: %#v", tc.resp, tc.target, tc.build, resp.Message)
+		t.Run(fmt.Sprintf("%s %s", tc.target, tc.build), func(t *testing.T) {
+			resp, err := server.Trigger(context.Background(), &breadpb.TriggerRequest{
+				Request: &operator.Request{Source: &operator.Source{}},
+				Target:  tc.target,
+				Build:   tc.build,
+			})
+			if tc.err == nil {
+				if err != nil {
+					t.Errorf("expected no error bit got %s", tc.build, err)
+				} else if resp.Message != tc.resp {
+					t.Errorf("expected response message `%s` but got %s", tc.resp, resp.Message)
 				}
-			} else if !strings.Contains(err.Error(), tc.err.Error()) {
-				t.Errorf("expected error for target=%s build=%s to match `%s` but got: %s", tc.target, tc.build, tc.err, err)
+			} else {
+				if err == nil {
+					t.Errorf("expect an error for but got none")
+					if resp.Message != tc.resp {
+						t.Errorf("expected response message `%s` for but got %s", tc.resp, resp.Message)
+					}
+				} else if !strings.Contains(err.Error(), tc.err.Error()) {
+					t.Errorf("expected error to match `%s` but got: %s", tc.err, err)
+				}
 			}
-		}
+		})
 	}
 }
