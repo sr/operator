60cd736fe4377971f64f678ac7fe75ec32cd7765
Scott Beaupied <sbeaupied@salesforce.com>
2016-10-05 08:49:18 -0400

    fixing port for postgres security group

--- src/terraform/aws/pardotops/providence.tf
+++ src/terraform/aws/pardotops/providence.tf
@@ -24,8 +24,8 @@ resource "aws_security_group" "providence_db_production" {
   vpc_id = "${aws_vpc.internal_apps.id}"
 
   ingress {
-    from_port = 3306
-    to_port = 3306
+    from_port = 5432
+    to_port = 5432
     protocol = "tcp"
     security_groups = ["${aws_security_group.internal_apps_providence_server.id}"]
   }
