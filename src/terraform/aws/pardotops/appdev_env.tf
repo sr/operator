0250d507376e30e8e04a8d11a2f8d4e4d1ec0adf
Jan Ulrich <julrich@salesforce.com>
2016-10-07 13:34:11 -0700

    BREAD-1452 Adding containerlab environment

--- src/terraform/aws/pardotops/appdev_env.tf
+++ src/terraform/aws/pardotops/appdev_env.tf
@@ -42,6 +42,7 @@ variable "environment_appdev" {
     num_vault1_hosts = 3
     num_consul1_hosts = 2
     num_indexer1_hosts = 1
+    num_containerlab1_hosts = 3
   }
 }
 
@@ -1334,6 +1335,36 @@ resource "aws_route53_record" "appdev_indexer1_arecord" {
   zone_id = "${aws_route53_zone.appdev_aws_pardot_com_hosted_zone.zone_id}"
   name = "${var.environment_appdev["pardot_env_id"]}-indexer1-${count.index + 1}-${var.environment_appdev["dc_id"]}.${aws_route53_zone.appdev_aws_pardot_com_hosted_zone.name}"
   records = ["${element(aws_instance.appdev_indexer1.*.private_ip, count.index)}"]
+  type = "A"
+  ttl = "900"
+}
+
+resource "aws_instance" "appdev_containerlab1" {
+  key_name = "internal_apps"
+  count = "${var.environment_appdev["num_containerlab1_hosts"]}"
+  ami = "${var.centos_7_hvm_50gb_chefdev_ami}"
+  instance_type = "${var.environment_appdev["app_instance_type"]}"
+  subnet_id = "${aws_subnet.appdev_us_east_1d.id}"
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = "50"
+    delete_on_termination = true
+  }
+  vpc_security_group_ids = [
+    "${aws_security_group.appdev_vpc_default.id}",
+    "${aws_security_group.appdev_apphost.id}"
+  ]
+  tags {
+    Name = "${var.environment_appdev["pardot_env_id"]}-containerlab1-${count.index + 1}-${var.environment_appdev["dc_id"]}"
+    terraform = "true"
+  }
+}
+
+resource "aws_route53_record" "appdev_containerlab1_arecord" {
+  count = "${var.environment_appdev["num_containerlab1_hosts"]}"
+  zone_id = "${aws_route53_zone.appdev_aws_pardot_com_hosted_zone.zone_id}"
+  name = "${var.environment_appdev["pardot_env_id"]}-containerlab1-${count.index + 1}-${var.environment_appdev["dc_id"]}.${aws_route53_zone.appdev_aws_pardot_com_hosted_zone.name}"
+  records = ["${element(aws_instance.appdev_containerlab1.*.private_ip, count.index)}"]
   type = "A"
   ttl = "900"
 }
