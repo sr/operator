496b96bf9cfb0d184957c71a9a3663a0318f2bcc
Simon Rozet <srozet@salesforce.com>
2016-10-09 17:29:05 +0200

    No need to call ParseForm anymore

--- src/bread/repfix.go
+++ src/bread/repfix.go
@@ -15,22 +15,17 @@ func (h *repfixHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		w.WriteHeader(http.StatusOK)
 	case "POST":
-		if err := r.ParseForm(); err != nil {
+		resp, err := h.hal.CreateRepfixError(context.TODO(), &hal9000.CreateRepfixErrorRequest{
+			Hostname:       r.FormValue("hostname"),
+			Error:          r.FormValue("error"),
+			MysqlLastError: r.FormValue("mysql_last_error"),
+		})
+		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			_, _ = w.Write([]byte(err.Error()))
 		} else {
-			resp, err := h.hal.CreateRepfixError(context.TODO(), &hal9000.CreateRepfixErrorRequest{
-				Hostname:       r.FormValue("hostname"),
-				Error:          r.FormValue("error"),
-				MysqlLastError: r.FormValue("mysql_last_error"),
-			})
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				_, _ = w.Write([]byte(err.Error()))
-			} else {
-				w.WriteHeader(int(resp.Status))
-				_, _ = w.Write([]byte(resp.Body))
-			}
+			w.WriteHeader(int(resp.Status))
+			_, _ = w.Write([]byte(resp.Body))
 		}
 	default:
 		w.WriteHeader(http.StatusNotFound)
