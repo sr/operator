995aaf6d48c834b5ea2bc1137850bed13008797d
Simon Rozet <srozet@salesforce.com>
2016-10-18 12:41:36 +0200

    Decomission HAL9000 ECS cluster

--- src/terraform/aws/pardotops/internaltools_sns.tf
+++ src/terraform/aws/pardotops/internaltools_sns.tf
@@ -1,4 +1,3 @@
-/* define SNS email endpoints */
 variable "internal_alert_email_endpoints" {
   type = "map"
 
@@ -8,27 +7,14 @@ variable "internal_alert_email_endpoints" {
   }
 }
 
-/* establish the topic */
 resource "aws_sns_topic" "internaltools" {
   name         = "internaltools"
-  display_name = "IntTools"      /* has a 10 char limit for some reason */
+  display_name = "IntTools"
 }
 
-/* sub to the topic; pick paging or non_paging */
-
-/* !! 'email' protocol NOT CURRENTLY SUPPORTED IN TERRAFORM! :( !!  */
-
-/* resource "aws_sns_topic_subscription" "internaltools_scrip" {
-  topic_arn = "${aws_sns_topic.internaltools.arn}"
-  protocol  = "email"
-  endpoint  = "${var.internal_alert_email_endpoints.non_paging}"
-} */
-
-/* define ASG event based notifiers */
 resource "aws_autoscaling_notification" "internaltools_asg_events_notifier" {
   group_names = [
     "${aws_autoscaling_group.canoe_production.name}",
-    "${aws_autoscaling_group.hal9000_production.name}",
     "${aws_autoscaling_group.operator_production.name}",
   ]
 
