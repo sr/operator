34c4d81c22b6ac81990be66aba0162198409f161
Andy Lindeman <alindeman@salesforce.com>
2016-11-11 09:18:07 -0500

    BREAD-1430: Avoids deploying into directory that already has the requested deployable

--- src/pull_agent/lib/pull_agent/deployers/pardot.rb
+++ src/pull_agent/lib/pull_agent/deployers/pardot.rb
@@ -17,14 +17,19 @@ module PullAgent
       end
 
       def deploy
-        quick_rollback = QuickRollback.new(release_directory, @deploy)
-        unless quick_rollback.perform
-          Dir.mktmpdir do |temp_dir|
-            ArtifactFetcher.new(@deploy.artifact_url).fetch_into(temp_dir)
-            DirectorySynchronizer.new(temp_dir, release_directory.standby_directory).synchronize
+        current_deploy = BuildVersion.load_from_directory(release_directory.current_symlink)
+        if current_deploy && current_deploy.instance_of_deploy?(@deploy)
+          Logger.log(:info, "Requested deploy is already present in #{release_directory.current_symlink}. Skipping fetch step.")
+        else
+          quick_rollback = QuickRollback.new(release_directory, @deploy)
+          unless quick_rollback.perform
+            Dir.mktmpdir do |temp_dir|
+              ArtifactFetcher.new(@deploy.artifact_url).fetch_into(temp_dir)
+              DirectorySynchronizer.new(temp_dir, release_directory.standby_directory).synchronize
 
-            @deploy.to_build_version.save_to_directory(release_directory.standby_directory)
-            release_directory.make_standby_directory_live
+              @deploy.to_build_version.save_to_directory(release_directory.standby_directory)
+              release_directory.make_standby_directory_live
+            end
           end
         end
       end
