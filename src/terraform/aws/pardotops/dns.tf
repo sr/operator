01080c19e86bf23311c9da5a4bca43d16853e31f
Andy Lindeman <alindeman@salesforce.com>
2016-11-28 16:01:52 -0500

    BREAD-1636: Switches over DNS to new GitHub Enterprise instances

--- src/terraform/aws/pardotops/dns.tf
+++ src/terraform/aws/pardotops/dns.tf
@@ -184,7 +184,7 @@ resource "aws_route53_record" "git_dev_pardot_com_CNAMErecord" {
 resource "aws_route53_record" "git-internal_dev_pardot_com_Arecord" {
   zone_id = "${aws_route53_zone.dev_pardot_com.zone_id}"
   name    = "git-internal.${aws_route53_zone.dev_pardot_com.name}"
-  records = ["172.31.57.89"]
+  records = ["${aws_instance.github_enterprise_server_1.private_ip}"]
   type    = "A"
   ttl     = "60"
 }
@@ -192,23 +192,15 @@ resource "aws_route53_record" "git-internal_dev_pardot_com_Arecord" {
 resource "aws_route53_record" "1_git_dev_pardot_com_Arecord" {
   zone_id = "${aws_route53_zone.dev_pardot_com.zone_id}"
   name    = "1.git.${aws_route53_zone.dev_pardot_com.name}"
-  records = ["52.4.132.69"]
+  records = ["${aws_instance.github_enterprise_server_1.public_ip}"]
   type    = "A"
-  ttl     = "30"
+  ttl     = "900"
 }
 
 resource "aws_route53_record" "2_git_dev_pardot_com_Arecord" {
   zone_id = "${aws_route53_zone.dev_pardot_com.zone_id}"
   name    = "2.git.${aws_route53_zone.dev_pardot_com.name}"
-  records = ["52.3.83.197"]
-  type    = "A"
-  ttl     = "30"
-}
-
-resource "aws_route53_record" "backups_git_dev_pardot_com_Arecord" {
-  zone_id = "${aws_route53_zone.dev_pardot_com.zone_id}"
-  name    = "backups.git.${aws_route53_zone.dev_pardot_com.name}"
-  records = ["54.85.203.23"]
+  records = ["${aws_instance.github_enterprise_server_2.public_ip}"]
   type    = "A"
   ttl     = "900"
 }
