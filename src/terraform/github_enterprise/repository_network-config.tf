863509f8976c1962661cbfad0b92412854abc463
Andy Lindeman <alindeman@salesforce.com>
2016-12-01 13:13:29 -0500

    BREAD-1651: Removes Terraform configuration for archived repositories
    We may want to put these under Terraform control again at some point in the future, but I think it's worth focusing on just active ones for now.

--- src/terraform/github_enterprise/repository_network-config.tf
+++
@@ -1,9 +0,0 @@
-resource "github_repository" "network-config" {
-  name          = "network-config"
-  description   = "Pardot Network Configuration"
-  homepage_url  = ""
-  private       = true
-  has_issues    = false
-  has_downloads = true
-  has_wiki      = false
-}
