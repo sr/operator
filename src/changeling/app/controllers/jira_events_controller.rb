b555845a6a280cec1324432b22ceb136f454c331
Andy Lindeman <alindeman@salesforce.com>
2016-12-12 12:25:48 -0500

    changeling: only handle certain event types

--- src/changeling/app/controllers/jira_events_controller.rb
+++ src/changeling/app/controllers/jira_events_controller.rb
@@ -2,6 +2,13 @@ class JiraEventsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
   skip_before_action :require_oauth, only: [:create]
 
+  PROCESSABLE_EVENTS = [
+    "jira:issue_created",
+    "jira:issue_updated",
+    "jira:version_updated",
+    "jira:issue_deleted"
+  ].freeze
+
   cattr_accessor :redis_connection do
     Redis.new(url: ENV["REDIS_URL"])
   end
@@ -10,6 +17,11 @@ class JiraEventsController < ApplicationController
     # The webhook data cannot be trusted. We use the payload only to get the key
     # to reach back to the JIRA API to get authoritative data.
     webhook_payload = JSON.parse(request.body.read.force_encoding("utf-8"))
+    if !PROCESSABLE_EVENTS.include?(webhook_payload["webhookEvent"])
+      render json: {}, status: :ok
+      return
+    end
+
     webhook_issue = JIRAIssue.new(webhook_payload)
     Raven.extra_context(payload: webhook_payload)
 
