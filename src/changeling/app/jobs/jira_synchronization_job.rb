4ba457220bb2eb03473be7ce01aff0e4566c8790
Simon Rozet <srozet@salesforce.com>
2016-12-14 14:00:45 +0100

    changeling: Fix JiraSynchronizationJob to avoid passing down a nil issue key
    https://bread-sentry.dev.pardot.com/pardot/changeling/issues/136/

--- src/changeling/app/jobs/jira_synchronization_job.rb
+++ src/changeling/app/jobs/jira_synchronization_job.rb
@@ -1,9 +1,7 @@
 class JiraSynchronizationJob < ActiveJob::Base
   queue_as :default
 
-  def perform(webhook_payload)
-    Raven.extra_context(payload: webhook_payload)
-    webhook_issue = JIRAIssue.new(webhook_payload)
-    Ticket.synchronize_jira_ticket(webhook_issue.key)
+  def perform(payload)
+    Ticket.synchronize_jira_ticket(payload.fetch("issue").fetch("key"))
   end
 end
