5618ba7e3cddb471a49a3a8ea1e883bd6eb971fd
Andy Lindeman <alindeman@salesforce.com>
2017-01-04 20:00:44 -0500

    Covers the scary notification with a test

--- src/canoe/spec/models/deploy_notification_spec.rb
+++ src/canoe/spec/models/deploy_notification_spec.rb
@@ -16,6 +16,16 @@ RSpec.describe DeployNotification do
       expect(notifier.messages[0].room_id).to eq(notification.hipchat_room_id)
       expect(notifier.messages[0].message).to match(/just began syncing/)
     end
+
+    it "notifies a HipChat room in a flashy way if a pending build is deployed" do
+      deploy.update!(passed_ci: false)
+
+      notification.notify_deploy_start(deploy)
+      expect(notifier.messages.size).to eq(1)
+      expect(notifier.messages[0].room_id).to eq(notification.hipchat_room_id)
+      expect(notifier.messages[0].color).to eq("red")
+      expect(notifier.messages[0].message).to match(/PENDING BUILD/)
+    end
   end
 
   describe "#notify_deploy_complete" do
