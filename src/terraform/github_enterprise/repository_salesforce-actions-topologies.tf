a0a1f3bb32cd1e73e16940acec39a6332a1b6095
Rory K <rkiefer@salesforce.com>
2017-01-06 15:58:34 -0500

    BREAD-1773-RJK-2 removing build dependencies

--- src/terraform/github_enterprise/repository_salesforce-actions-topologies.tf
+++ src/terraform/github_enterprise/repository_salesforce-actions-topologies.tf
@@ -19,12 +19,3 @@ resource "github_team_repository" "salesforce-actions-topologies_service-account
   team_id    = "${github_team.service-accounts-write-only.id}"
   permission = "push"
 }
-
-resource "github_branch_protection" "salesforce-actions-topologies_master" {
-  repository = "${github_repository.salesforce-actions-topologies.name}"
-  branch     = "master"
-
-  include_admins = true
-  strict         = false
-  contexts       = ["Test Jobs"]
-}
