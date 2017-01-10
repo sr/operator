5b2f09058881629e5c2b1d87477a38518b76f7cf
simon.rozet <srozet@salesforce.com>
2017-01-10 14:56:13 +0100

    changeling: Do not require the "Initial Jobs" testing status for Pardot/pardot

--- src/changeling/app/models/pardot_repository.rb
+++ src/changeling/app/models/pardot_repository.rb
@@ -9,7 +9,6 @@ class PardotRepository
   PARDOT = "Pardot/pardot".freeze
   TEAM_OPS = "Pardot/ops".freeze
   TEAM_DEVELOPERS = "Pardot/developers".freeze
-  INITIAL_STATUS = "Initial Jobs".freeze
   TEST_STATUS = "Test Jobs".freeze
   FINAL_STATUS = "Final Jobs".freeze
 
@@ -24,7 +23,7 @@ class PardotRepository
     when BREAD
       [TEST_STATUS]
     when PARDOT
-      [INITIAL_STATUS, TEST_STATUS, FINAL_JOBS]
+      [TEST_STATUS, FINAL_JOBS]
     when CHEF
       [TEST_STATUS, FINAL_STATUS]
     when CHANGELING
