b6439ec1554694f84a8f85d2adb426e71278f453
Simon Rozet <srozet@salesforce.com>
2017-01-13 13:18:11 +0100

    terraform/github_enterprise: Create (incomplete) @Pardot/caerus team

---
+++ src/terraform/github_enterprise/team_caerus.tf
@@ -0,0 +1,11 @@
+resource "github_team" "caerus" {
+  name        = "Caerus"
+  description = "https://confluence.dev.pardot.com/display/PE/Team+Caerus"
+  privacy     = "closed"
+}
+
+resource "github_team_membership" "caerus_andy-fischer" {
+  team_id  = "${github_team.caerus.id}"
+  username = "andy-fischer"
+  role     = "member"
+}
