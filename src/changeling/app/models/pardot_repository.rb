4d92923c6d0e57ce2befc0e00506bdfb5b40eec7
Simon Rozet <srozet@salesforce.com>
2017-01-13 13:08:43 +0100

    changeling: Tweak feature flags and whatnot for rollout to Canoe repositories
    * Require a OWNERS file at the root of repositories under compliance
    * Remove pardot_rollout_phase1_enabled? feature flag
    * Group repositories with the same testing statuses together for clarity

--- src/changeling/app/models/pardot_repository.rb
+++ src/changeling/app/models/pardot_repository.rb
@@ -5,7 +5,7 @@ class PardotRepository
   CHANGELING = "heroku/changeling".freeze
 
   ANSIBLE = "Pardot/ansible".freeze
-  BLUEMESH = "Pardot/blue-mesh".freeze
+  BLUE_MESH = "Pardot/blue-mesh".freeze
   BREAD = "Pardot/bread".freeze
   CHEF = "Pardot/chef".freeze
   PARDOT = "Pardot/pardot".freeze
@@ -13,7 +13,8 @@ class PardotRepository
   TEAM_DEVELOPERS = "Pardot/developers".freeze
   TEST_STATUS = "Test Jobs".freeze
   FINAL_STATUS = "Final Jobs".freeze
-  COMPLIANT_REPOSITORIES = [ANSIBLE, BLUEMESH, BREAD, CHEF, PARDOT].freeze
+
+  COMPLIANT_REPOSITORIES = [ANSIBLE, BLUE_MESH, BREAD, CHEF, PARDOT].freeze
 
   def initialize(nwo)
     @name_with_owner = nwo
@@ -23,18 +24,12 @@ class PardotRepository
 
   def required_testing_statuses
     case name_with_owner
-      when ANSIBLE
-        [TEST_STATUS]
-      when BLUEMESH
-        [TEST_STATUS]
-      when BREAD
-        [TEST_STATUS]
-      when CHEF
-        [TEST_STATUS, FINAL_STATUS]
-      when PARDOT
-        [TEST_STATUS, FINAL_STATUS]
-      when CHANGELING
-        ["ci/bazel", "ci/travis"]
+    when ANSIBLE, BLUE_MESH, BREAD
+      [TEST_STATUS]
+    when CHEF, PARDOT
+      [TEST_STATUS, FINAL_STATUS]
+    when CHANGELING
+      ["ci/bazel", "ci/travis"]
     else
       Rails.logger.info "configuration-missing repo=#{name_with_owner.inspect}"
       []
@@ -55,11 +50,7 @@ class PardotRepository
 
   # Do not create commit statuses for now.
   def update_github_commit_status?
-    if Changeling.config.pardot_rollout_phase1_enabled?
-      COMPLIANT_REPOSITORIES.include?(name_with_owner)
-    else
-      [BREAD].include?(name_with_owner)
-    end
+    COMPLIANT_REPOSITORIES.include?(name_with_owner)
   end
 
   def participating?
