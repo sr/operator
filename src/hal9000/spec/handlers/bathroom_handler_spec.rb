179831fe1b6f615fd076cbd7e7e92e7c820ed899
Justin Roberts <justin.roberts@salesforce.com>
2017-01-24 09:17:58 -0500

    Adding bathroom status, poo, and tests

---
+++ src/hal9000/spec/handlers/bathroom_handler_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+describe BathroomHandler, lita_handler: true do
+  before do
+    expect(Net::HTTP).to receive(:get)
+      .with(URI('https://pardot-pingpong.herokuapp.com/bathrooms.json'))
+      .and_return('[{"id":1,"name":"34 Mens","stalls":[{"number":0,"state":true},{"number":1,"state":true}]}, {"id":2,"name":"34 Womens","stalls":[{"number":0,"state":true},{"number":1,"state":false},{"number":2,"state":true},{"number":3,"state":false}]}]')
+  end
+  describe "!poo" do
+    it "returns the status of the bathrooms" do
+      send_command("poo")
+      expect(replies.last).to eq("Bathroom Status:\n34 Mens: 0 stalls free\n34 Womens: 2 stalls free")
+    end
+  end
+  describe "!bathroom status" do
+    it "returns the status of the bathrooms" do
+      send_command("bathroom status")
+      expect(replies.last).to eq("Bathroom Status:\n34 Mens: 0 stalls free\n34 Womens: 2 stalls free")
+    end
+  end
+end
