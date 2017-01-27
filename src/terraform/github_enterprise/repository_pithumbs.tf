a66ca06466e49718f0f983d6b2c5a35645400ae1
Rory Kiefer <rkiefer@salesforce.com>
2017-01-27 14:34:28 -0500

    bringing compliance to repfix

--- src/terraform/github_enterprise/repository_pithumbs.tf
+++ src/terraform/github_enterprise/repository_pithumbs.tf
@@ -3,9 +3,9 @@ resource "github_repository" "pithumbs" {
   description   = "Home for service to quickly generate thumbnails from HTML"
   homepage_url  = ""
   private       = true
-  has_issues    = true
+  has_issues    = false
   has_downloads = true
-  has_wiki      = true
+  has_wiki      = false
 }
 
 resource "github_team_repository" "pithumbs_developers" {
@@ -44,5 +44,5 @@ resource "github_branch_protection" "pithumbs_master" {
 
   include_admins = false
   strict         = false
-  contexts       = ["Test Jobs"]
+  contexts       = ["compliance"]
 }
