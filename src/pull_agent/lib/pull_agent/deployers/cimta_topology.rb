a1125f91787ac8ed9cfcb1d4dfc6a45fdc019071
Russell Rollins <russell.rollins@salesforce.com>
2017-01-31 11:54:10 -0500

    BREAD-1883 pull-agent adds cimta-topology, extracts topology logic.

---
+++ src/pull_agent/lib/pull_agent/deployers/cimta_topology.rb
@@ -0,0 +1,15 @@
+module PullAgent
+  module Deployers
+    class CimtaTopology < TopologyDeploy
+      DeployerRegistry["cimta-topology"] = self
+
+      protected
+
+      def release_directory
+        @release_directory ||=
+          ReleaseDirectory.new(ENV.fetch("RELEASE_DIRECTORY", "/opt/cimta-topology"))
+      end
+    end
+  end
+end
+    
---
+++ src/pull_agent/lib/pull_agent/deployers/topology.rb
@@ -0,0 +1,55 @@
+module PullAgent
+  module Deployers
+    class TopologyDeploy
+      ReleaseDirectoryError = Class.new(StandardError)
+
+      def initialize(environment, deploy)
+        @environment = environment
+        @deploy = deploy
+      end
+
+      def deploy
+        current_deploy = BuildVersion.load_from_directory(release_directory.current_symlink)
+        if current_deploy && current_deploy.instance_of_deploy?(@deploy)
+          Logger.log(:info, "Requested deploy is already present in #{release_directory.current_symlink}. Skipping fetch step.")
+        else
+          quick_rollback = QuickRollback.new(release_directory, @deploy)
+          unless quick_rollback.perform
+            Dir.mktmpdir do |temp_dir|
+              ArtifactFetcher.new(@deploy.artifact_url).fetch_into(temp_dir)
+              DirectorySynchronizer.new(temp_dir, release_directory.standby_directory).synchronize
+
+              @deploy.to_build_version.save_to_directory(release_directory.standby_directory)
+              release_directory.make_standby_directory_live
+            end
+          end
+        end
+
+        if @deploy.options["topology"].nil?
+          Logger.log(:err, "deploy[topology] not present, can't restart topology")
+        elsif @deploy.options["topo_env"].nil?
+          Logger.log(:err, "deploy[topo_env] not present, can't restart topology")
+        else
+          jarfile = Dir[File.join(release_directory.current_symlink, "*.jar")].first
+          if jarfile.nil?
+            Logger.log(:err, "no *.jar file found in deployment, can't load topology")
+          else
+            Logger.log(:info, "Topology Deployment Param: #{@deploy.options["topology"]}")
+            Logger.log(:info, "Topology Deployment JAR: #{jarfile}")
+            storm = Storm.new(@deploy.options["topology"], @deploy.options["topo_env"], jarfile)
+            storm.load
+          end
+        end
+      end
+
+      def restart
+      end
+
+      protected
+
+      def release_directory
+        raise ReleaseDirectoryError, "each topology deploy must define its own release_directory"
+      end
+    end
+  end
+end
