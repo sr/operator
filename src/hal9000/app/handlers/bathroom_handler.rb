b8016e7e4d453c268356ced7e45f9facdb2e3d65
Justin Roberts <justin.roberts@salesforce.com>
2017-02-08 08:57:30 -0500

    adding offline check

--- src/hal9000/app/handlers/bathroom_handler.rb
+++ src/hal9000/app/handlers/bathroom_handler.rb
@@ -28,6 +28,7 @@ class BathroomHandler < ApplicationHandler
 
     json.each do |bathroom|
       name = bathroom["name"]
+      offline = bathroom["offline"]
       stalls_string = ""
       stalls_in_use = 0
       stall_count = bathroom["stalls"].count
@@ -40,7 +41,7 @@ class BathroomHandler < ApplicationHandler
         end
       end
       available_stall_count = stall_count - stalls_in_use
-      response_string += "\n#{name}: #{stalls_string}#{available_stall_count} stalls free"
+      response_string += "\n#{name}: #{offline ? "OFFLINE - Please check sensor " : ""}#{stalls_string}#{available_stall_count} stalls free"
     end
     response_string
   end
