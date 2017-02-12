1c643bc877231f529ef546df704a33f4d1b7d90f
Simon Rozet <srozet@salesforce.com>
2017-02-12 17:02:59 +0100

    cmd/jiracleaner: Extract jira package

--- cmd/jiracleaner/main.go
+++ cmd/jiracleaner/main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
+	"context"
 	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
 	"strings"
 	"time"
 
-	"github.com/google/go-github/github"
+	"git.dev.pardot.com/Pardot/bread/jira"
 )
 
 const (
@@ -20,6 +17,7 @@ const (
 	defaultProject      = "BREAD"
 	query               = `project = '%s' AND (updatedDate <= "%s" OR updatedDate IS EMPTY) AND status NOT IN (Closed, "Will Not Do", Resolved) ORDER BY updatedDate ASC`
 	defaultTransitionID = 2
+	defaultResolution   = "Won't Fix"
 )
 
 var (
@@ -30,6 +28,7 @@ var (
 	username     string
 	password     string
 	transitionID int
+	resolution   string
 
 	defaultMaxAge = (time.Hour * 24) * 30 * 3
 	comment       = strings.Replace(`In an effort to keep our backlog realistic and up to date we have decided to automatically close stale tickets.
@@ -49,6 +48,7 @@ func run() error {
 	flag.StringVar(&username, "username", "", "JIRA username")
 	flag.StringVar(&password, "password", "", "JIRA password")
 	flag.IntVar(&transitionID, "transition-id", defaultTransitionID, "Transition ID for closing stale tickets")
+	flag.StringVar(&resolution, "resolution", defaultResolution, "Resolution to use for closing stale tickets")
 	flag.Parse()
 	if username == "" {
 		return errors.New("required flag missing: username")
@@ -56,80 +56,27 @@ func run() error {
 	if password == "" {
 		return errors.New("required flag missing: password")
 	}
-	transport := github.BasicAuthTransport{Username: username, Password: password}
-	client := transport.Client()
+	client := jira.NewClient(jiraURL, username, password)
 	now := time.Now()
 	cutOff := now.Add(-maxAge)
-	data := struct {
-		JQL        string   `json:"jql"`
-		Fields     []string `json:"fields"`
-		MaxResults int      `json:"maxResults"`
-	}{
+	issues, err := client.Search(
+		context.TODO(),
 		fmt.Sprintf(query, project, cutOff.Format("2006-01-02")),
 		[]string{"id", "key", "created", "updated", "summary"},
 		100,
-	}
-	b, err := json.Marshal(data)
+	)
 	if err != nil {
 		return err
 	}
-	req, _ := http.NewRequest("POST", jiraURL+"/rest/api/2/search", bytes.NewReader(b))
-	req.Header.Set("Content-Type", "application/json")
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer func() { _ = resp.Body.Close() }()
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("jira search request failed with status %d", resp.StatusCode)
-	}
-	type response struct {
-		Issues []struct {
-			ID     string `json:"id"`
-			Key    string `json:"key"`
-			Fields struct {
-				Updated string `json:"updated"`
-				Summary string `json:"summary"`
-			} `json:"fields"`
-		}
-	}
-	var results response
-	if err := json.NewDecoder(resp.Body).Decode(&results); err != nil {
-		return err
-	}
-	for _, issue := range results.Issues {
+	for _, issue := range issues {
 		fmt.Printf("%s %s %s\n",
 			strings.Split(issue.Fields.Updated, "T")[0],
 			fmt.Sprintf("%s/browse/%s", jiraURL, issue.Key),
 			issue.Fields.Summary,
 		)
 		if apply {
-			req, err := http.NewRequest("POST", jiraURL+"/rest/api/2/issue/"+issue.ID+"/transitions", nil)
-			req.Header.Set("Content-Type", "application/json")
-			if err != nil {
-				return err
-			}
-			req.Body = ioutil.NopCloser(strings.NewReader(fmt.Sprintf(`{
-				"update": {
-					"comment": [
-						{
-							"add": {
-								"body": "%s"
-							}
-						}
-					]
-				},
-				"fields": {
-					"resolution": {
-						"name": "Won't Fix"
-					}
-				},
-				"transition": {
-					"id": "%d"
-				}
-			}`, comment, transitionID)))
-			if _, err := client.Do(req); err != nil {
-				return fmt.Errorf("failed to close issue: %v", err)
+			if err := client.Close(context.TODO(), issue.ID, transitionID, resolution, comment); err != nil {
+				return fmt.Errorf("failed to close %s: %v", issue.Key, err)
 			}
 		}
 	}
