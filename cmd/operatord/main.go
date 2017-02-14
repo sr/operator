sr/operator

commit 4e5ee1d0f74538166c879963ddf017d1c59c1ef3

Move Go packages and commands under new git.dev.pardot.com/Pardot/bread import path

cmd/operatord/main.go
@@ -1,0 +1,354 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"flag"
+	"fmt"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+
+	"git.dev.pardot.com/Pardot/bread"
+	"git.dev.pardot.com/Pardot/bread/pb"
+	"git.dev.pardot.com/Pardot/bread/pb/hal9000"
+
+	"github.com/sr/operator"
+	"github.com/sr/operator/hipchat"
+	"github.com/sr/operator/protolog"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const grpcTimeout = 10 * time.Second
+
+func mustParseURL(s string) *url.URL {
+	u, err := url.Parse(s)
+	if err != nil {
+		panic(err)
+	}
+	return u
+}
+
+type config struct {
+	afy   *bread.ArtifactoryConfig
+	canoe *bread.CanoeConfig
+	ecs   *bread.ECSConfig
+
+	dev             bool
+	devRoomID       int
+	devHipchatToken string
+
+	grpcAddr string
+	halAddr  string
+	httpAddr string
+	timeout  time.Duration
+	timezone string
+
+	ldap *bread.LDAPConfig
+
+	databaseURL string
+	prefix      string
+
+	hipchatNamespace  string
+	hipchatAddonURL   string
+	hipchatWebhookURL string
+
+	event *bread.EventHandlerConfig
+}
+
+func run(invoker operator.InvokerFunc) error {
+	config := &config{
+		afy:   &bread.ArtifactoryConfig{},
+		canoe: &bread.CanoeConfig{},
+		ecs:   &bread.ECSConfig{},
+		event: &bread.EventHandlerConfig{
+			GithubEndpoints: []*url.URL{
+				mustParseURL("https://compliance.dev.pardot.com/webhooks"),
+			},
+			JIRAEndpoints: []*url.URL{
+				mustParseURL("https://compliance.dev.pardot.com/events/jira"),
+			},
+			RequestTimeout: 2.0 * time.Second,
+			MaxRetries:     5,
+			RetryDelay:     1.0 * time.Second,
+		},
+		ldap: &bread.LDAPConfig{},
+	}
+	flags := flag.CommandLine
+	flags.StringVar(&config.grpcAddr, "addr-grpc", ":9000", "Listen address of the gRPC server")
+	flags.StringVar(&config.halAddr, "addr-hal9000", "", "Address of the HAL9000 gRPC server")
+	flags.StringVar(&config.httpAddr, "addr-http", ":8080", "Listen address of the HipChat addon and webhook HTTP server")
+	flags.DurationVar(&config.timeout, "timeout", 10*time.Minute, "Timeout for gRPC requests")
+	flags.StringVar(&config.timezone, "timezone", "America/New_York", "Display dates and times in this timezone")
+	flags.BoolVar(&config.dev, "dev", false, "Enable development mode")
+	flags.IntVar(&config.devRoomID, "dev-room-id", bread.TestingRoom, "Room ID where to send messages")
+	flags.StringVar(&config.devHipchatToken, "dev-hipchat-token", "", "HipChat user token")
+	flags.StringVar(&config.ldap.Addr, "ldap-addr", "localhost:389", "Address of the LDAP server used to authenticate and authorize commands")
+	flags.StringVar(&config.ldap.Base, "ldap-base", bread.LDAPBase, "LDAP Base DN")
+	flags.StringVar(&config.databaseURL, "database-url", "", "database/sql connection string to the database where OAuth credentials are stored")
+	flags.StringVar(&config.prefix, "prefix", "!", "Prefix used to indicate commands in chat messages")
+	flags.StringVar(&config.hipchatNamespace, "hipchat-namespace", "com.pardot.dev.operator", "Namespace used for all installations created via this server")
+	flags.StringVar(&config.hipchatAddonURL, "hipchat-addon-url", "https://operator.dev.pardot.com/hipchat/addon", "HipChat addon installation endpoint URL")
+	flags.StringVar(&config.hipchatWebhookURL, "hipchat-webhook-url", "https://operator.dev.pardot.com/hipchat/webhook", "HipChat webhook endpoint URL")
+	flags.StringVar(&config.event.GithubSecretToken, "github-webhook-secret", "", "Shared secret used to verify the signature of webhook requests received from GitHub")
+	flags.StringVar(&config.afy.URL, "artifactory-url", "https://artifactory.dev.pardot.com/artifactory", "Artifactory URL")
+	flags.StringVar(&config.afy.User, "artifactory-user", "", "Artifactory username")
+	flags.StringVar(&config.afy.APIKey, "artifactory-api-key", "", "Artifactory API key")
+	flags.StringVar(&config.afy.Repo, "artifactory-repo", "pd-docker", "Name of the Artifactory repository where deployable artifacts are stored")
+	flags.StringVar(&config.canoe.URL, "canoe-url", "https://canoe.dev.pardot.com", "")
+	flags.StringVar(&config.canoe.APIKey, "canoe-api-key", "", "Canoe API key")
+	flags.StringVar(&config.ecs.AWSRegion, "ecs-aws-region", "us-east-1", "AWS Region")
+	flags.DurationVar(&config.ecs.Timeout, "ecs-deploy-timeout", 5*time.Minute, "Time to wait for new ECS task definitions to come up")
+	// Allow setting flags via environment variables
+	flags.VisitAll(func(f *flag.Flag) {
+		k := strings.ToUpper(strings.Replace(f.Name, "-", "_", -1))
+		if v := os.Getenv(k); v != "" {
+			if err := f.Value.Set(v); err != nil {
+				panic(err)
+			}
+		}
+	})
+	var host, port string
+	if v, ok := os.LookupEnv("HAL9000_PORT_9001_TCP_ADDR"); ok {
+		host = v
+	}
+	if v, ok := os.LookupEnv("HAL9000_PORT_9001_TCP_PORT"); ok {
+		port = v
+	}
+	if host != "" && port != "" {
+		config.halAddr = fmt.Sprintf("%s:%s", host, port)
+	}
+	if err := flags.Parse(os.Args[1:]); err != nil {
+		return err
+	}
+	if config.grpcAddr == "" {
+		return fmt.Errorf("required flag missing: addr-grpc")
+	}
+	var (
+		httpServer *http.ServeMux
+		logger     protolog.Logger
+		inst       operator.Instrumenter
+		auth       operator.Authorizer
+		sender     operator.Sender
+
+		store    operatorhipchat.ClientCredentialsStore
+		db       *sql.DB
+		hal      hal9000.RobotClient
+		canoeAPI bread.CanoeClient
+
+		err error
+	)
+	httpServer = http.NewServeMux()
+	logger = bread.NewLogger()
+	inst = bread.NewInstrumenter(logger)
+	canoeURL, err := url.Parse(config.canoe.URL)
+	if err != nil {
+		return err
+	}
+	canoeAPI = bread.NewCanoeClient(canoeURL, config.canoe.APIKey)
+	if config.dev {
+		auth = &noopAuthorizer{}
+		if config.devRoomID == 0 {
+			return errors.New("dev mode enabled but required flag missing: dev-room-id")
+		}
+		if v, ok := os.LookupEnv("HIPCHAT_TOKEN"); ok && config.devHipchatToken == "" {
+			config.devHipchatToken = v
+		}
+		if config.devHipchatToken == "" {
+			return errors.New("dev mode enabled but required flag missing: dev-hipchat-token")
+		}
+		cl, err := bread.NewHipchatClient(&operatorhipchat.ClientConfig{
+			Hostname: bread.HipchatHost,
+			Token:    config.devHipchatToken,
+		})
+		if err != nil {
+			return err
+		}
+		sender = &devSender{client: cl, roomID: config.devRoomID}
+	} else {
+		if config.httpAddr == "" {
+			return fmt.Errorf("required flag missing: addr-http")
+		}
+		if config.prefix == "" {
+			return fmt.Errorf("required flag missing: prefix")
+		}
+		if config.hipchatNamespace == "" {
+			return fmt.Errorf("required flag missing: hipchat-namespace")
+		}
+		if config.hipchatAddonURL == "" {
+			return fmt.Errorf("required flag missing: hipchat-addon-url")
+		}
+		if config.hipchatWebhookURL == "" {
+			return fmt.Errorf("required flag missing: hipchat-webhook-url")
+		}
+		if config.databaseURL == "" {
+			return fmt.Errorf("required flag missing: database-url")
+		}
+		db, err = sql.Open("mysql", config.databaseURL)
+		if err != nil {
+			return err
+		}
+		if err := db.Ping(); err != nil {
+			return err
+		}
+		httpServer.Handle("/_ping", bread.NewHandler(logger, bread.NewPingHandler(db)))
+		store = operatorhipchat.NewSQLStore(db, bread.HipchatHost)
+		sender = operatorhipchat.NewSender(store, bread.HipchatHost)
+		if auth, err = bread.NewAuthorizer(config.ldap, canoeAPI, bread.ACL); err != nil {
+			return err
+		}
+	}
+	tz, err := time.LoadLocation(config.timezone)
+	if err != nil {
+		return err
+	}
+	var grpcServer *grpc.Server
+	if grpcServer, err = bread.NewServer(
+		auth,
+		inst,
+		sender,
+		bread.NewDeployServer(
+			sender,
+			bread.NewECSDeployer(config.ecs, config.afy, bread.ECSDeployTargets, canoeAPI),
+			bread.NewCanoeDeployer(canoeAPI, config.canoe),
+			tz,
+		),
+	); err != nil {
+		return err
+	}
+	msg := &breadpb.ServerStartupNotice{Protocol: "grpc", Address: config.grpcAddr}
+	for svc := range grpcServer.GetServiceInfo() {
+		msg.Services = append(msg.Services, svc)
+	}
+	errC := make(chan error)
+	grpcList, err := net.Listen("tcp", config.grpcAddr)
+	if err != nil {
+		return err
+	}
+	go func() {
+		errC <- grpcServer.Serve(grpcList)
+	}()
+	logger.Info(msg)
+	if config.halAddr != "" {
+		if cc, err := grpc.Dial(
+			config.halAddr,
+			grpc.WithBlock(),
+			grpc.WithTimeout(grpcTimeout),
+			grpc.WithInsecure(),
+		); err == nil {
+			hal = hal9000.NewRobotClient(cc)
+		} else {
+			return err
+		}
+		httpServer.Handle("/replication/", bread.NewHandler(logger, bread.NewRepfixHandler(hal)))
+	}
+	if !config.dev {
+		var webhookHandler http.Handler
+		conn, err := grpc.Dial(
+			config.grpcAddr,
+			grpc.WithBlock(),
+			grpc.WithTimeout(grpcTimeout),
+			grpc.WithInsecure(),
+		)
+		if err != nil {
+			return err
+		}
+		const pkg = "git.dev.pardot.com/Pardot/bread"
+		if webhookHandler, err = bread.NewHipchatHandler(
+			context.Background(),
+			inst,
+			operatorhipchat.NewRequestDecoder(store),
+			sender,
+			invoker,
+			conn,
+			grpcServer.GetServiceInfo(),
+			hal,
+			config.timeout,
+			config.prefix,
+			pkg,
+		); err != nil {
+			return err
+		}
+		addonURL, err := url.Parse(config.hipchatAddonURL)
+		if err != nil {
+			return err
+		}
+		webhookURL, err := url.Parse(config.hipchatWebhookURL)
+		if err != nil {
+			return err
+		}
+		httpServer.Handle(
+			"/hipchat/addon",
+			bread.NewHandler(
+				logger,
+				operatorhipchat.NewAddonHandler(
+					store,
+					&operatorhipchat.AddonConfig{
+						Namespace:     config.hipchatNamespace,
+						URL:           addonURL,
+						Homepage:      bread.RepoURL,
+						WebhookURL:    webhookURL,
+						WebhookPrefix: config.prefix,
+					},
+				),
+			),
+		)
+		httpServer.Handle("/hipchat/webhook", bread.NewHandler(logger, webhookHandler))
+	}
+	if config.event.GithubSecretToken != "" {
+		httpServer.Handle(
+			"/",
+			bread.NewHandler(logger, bread.NewEventHandler(logger, config.event)),
+		)
+	}
+	if config.httpAddr != "" {
+		logger.Info(&breadpb.ServerStartupNotice{
+			Protocol: "http",
+			Address:  config.httpAddr,
+			Hal9000:  config.halAddr,
+		})
+		go func() {
+			errC <- http.ListenAndServe(config.httpAddr, httpServer)
+		}()
+	}
+	return <-errC
+}
+
+func main() {
+	if err := run(invoker); err != nil {
+		fmt.Fprintf(os.Stderr, "operatord: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+type noopAuthorizer struct{}
+
+func (a *noopAuthorizer) Authorize(_ context.Context, _ *operator.Request) error {
+	return nil
+}
+
+type devSender struct {
+	client operatorhipchat.Client
+	roomID int
+}
+
+func (r *devSender) Send(ctx context.Context, src *operator.Source, rep string, msg *operator.Message) error {
+	notif := &operatorhipchat.RoomNotification{RoomID: int64(r.roomID)}
+	if msg.HTML != "" {
+		notif.MessageFormat = "html"
+		notif.Message = msg.HTML
+	} else {
+		notif.MessageFormat = "text"
+		notif.Message = msg.Text
+	}
+	if v, ok := msg.Options.(*operatorhipchat.MessageOptions); ok {
+		notif.MessageOptions = v
+	}
+	return r.client.SendRoomNotification(ctx, notif)
+}

cmd/breadsignal/main.go
@@ -1,0 +1,97 @@
+// Command breadsignal sends a message to the BREAD chat room.
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+
+	"git.dev.pardot.com/Pardot/bread"
+	"github.com/sr/operator/hipchat"
+	"golang.org/x/net/context"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "breadsignal: %v\n", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func run() error {
+	terraformAction := flag.String("terraform", "", "Notify the BREAD team of a terraform actions (either plan or apply)")
+	terraformVersion := flag.String("terraform-version", "", "Installed terraform version")
+	terraformPath := flag.String("terraform-path", "", "the path being acted upon")
+	gitBranch := flag.String("branch", "unknown", "Checked out BREAD repo branch")
+	testing := flag.Bool("testing", true, "Send all notifications to the BREAD Testing room when true")
+	flag.Parse()
+	if *terraformAction == "" {
+		return nil
+	}
+	if *terraformVersion == "" {
+		return errors.New("required flag missing: terraform-version")
+	}
+	token, ok := os.LookupEnv("HIPCHAT_TOKEN")
+	if !ok {
+		return errors.New("required environment variable missing: HIPCHAT_TOKEN")
+	}
+	client, err := bread.NewHipchatClient(&operatorhipchat.ClientConfig{
+		Hostname: bread.HipchatHost,
+		Token:    token,
+	})
+	if err != nil {
+		return err
+	}
+	var (
+		color   string
+		message string
+		room    int
+	)
+	if *testing {
+		room = bread.TestingRoom
+	} else {
+		room = bread.PublicRoom
+	}
+	switch *terraformAction {
+	case "plan":
+		color = "gray"
+		message = fmt.Sprintf(
+			"planning terraform changes on branch <code>%s</code> with <code>%s</code> to terraform path <code>%s</code>",
+			*gitBranch,
+			*terraformVersion,
+			*terraformPath,
+		)
+	case "apply":
+		color = "yellow"
+		message = fmt.Sprintf(
+			"deploying terraform branch <code>%s</code> with <code>%s</code> to terraform path <code>%s</code>",
+			*gitBranch,
+			*terraformVersion,
+			*terraformPath,
+		)
+	case "push":
+		color = "red"
+		message = fmt.Sprintf(
+			"pushing terraform remote configuration from branch <code>%s</code> with <code>%s</code> to terraform path <code>%s</code>",
+			*gitBranch,
+			*terraformVersion,
+			*terraformPath,
+		)
+	default:
+		return nil
+	}
+	return client.SendRoomNotification(
+		context.Background(),
+		&operatorhipchat.RoomNotification{
+			MessageOptions: &operatorhipchat.MessageOptions{
+				Color: color,
+				From:  "breadsignal",
+			},
+			Message:       message,
+			MessageFormat: "html",
+			RoomID:        int64(room),
+		},
+	)
+}

cmd/devenv/main.go
@@ -1,0 +1,163 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"log/syslog"
+	"os"
+	"strings"
+	"syscall"
+	"time"
+
+	"git.dev.pardot.com/Pardot/bread/devenv"
+	"github.com/fsouza/go-dockerclient"
+	"golang.org/x/net/context"
+)
+
+const (
+	DockerBinary  = "/usr/local/bin/docker"
+	ComposeBinary = "/usr/local/bin/docker-compose"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "devenv: %v\n", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func run() error {
+	logger := buildLogger()
+
+	args := os.Args[1:]
+	if len(args) <= 0 || args[0] == "--help" || args[0] == "-h" {
+		usage()
+	} else if args[0] == "version" {
+		fmt.Printf("%s\n", devenv.Version)
+	} else if args[0] == "docker" || args[0] == "compose" {
+		errors := devenv.UnsatisfiedRequirements()
+		if len(errors) > 0 {
+			fmt.Fprintf(os.Stderr, "devenv cannot start until the following requirements are satisfied:\n")
+			for e := range errors {
+				fmt.Fprintf(os.Stderr, "  * %v\n", e)
+			}
+			return fmt.Errorf("unresolved requirements")
+		}
+
+		client, err := docker.NewClientFromEnv()
+		if err != nil {
+			return err
+		}
+
+		forwarder := devenv.NewSSHForwarder(client, logger)
+		if forwarder.IsStarted() {
+			authSock, err := forwarder.DockerSSHAuthSock()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Warning: Unable to setup SSH auth socket. This might mean the ssh-forwarder service didn't start properly. The error was: %v\n", err)
+				return err
+			}
+			if err := os.Setenv("DOCKER_SSH_AUTH_SOCK", authSock); err != nil {
+				return err
+			}
+
+			volume, err := forwarder.DockerVolume()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Warning: Unable to setup SSH auth socket. This might mean the ssh-forwarder service didn't start properly. The error was: %v\n", err)
+			}
+			if err := os.Setenv("DOCKER_SSH_AUTH_VOLUME", volume); err != nil {
+				return err
+			}
+		} else {
+			fmt.Fprintf(os.Stderr, "Warning: The ssh-forwarder container is not started. Have you started the service? Try: brew services start devenv\n")
+		}
+
+		if args[0] == "docker" {
+			if args[1] == "run" {
+				newArgs := []string{args[0], args[1]}
+				newArgs = append(newArgs, "-e", "DOCKER_SSH_AUTH_SOCK")
+				if sshAuthVolume := os.Getenv("DOCKER_SSH_AUTH_VOLUME"); sshAuthVolume != "" {
+					newArgs = append(newArgs, "-v", sshAuthVolume)
+				}
+				newArgs = append(newArgs, args[2:]...)
+				args = newArgs
+			}
+
+			if err := syscall.Exec(DockerBinary, args, os.Environ()); err != nil {
+				return err
+			}
+		} else if args[0] == "compose" {
+			// If left unspecified by the user, set COMPOSE_FILE to include
+			// docker-compose.dev.yml
+			if os.Getenv("COMPOSE_FILE") == "" {
+				cwd, err := os.Getwd()
+				if err != nil {
+					return err
+				}
+
+				composeFiles := make([]string, 0, 2)
+				if file := devenv.FindDockerComposeFile("docker-compose.yml", cwd); file != "" {
+					composeFiles = append(composeFiles, file)
+				}
+				if file := devenv.FindDockerComposeFile("docker-compose.dev.yml", cwd); file != "" {
+					composeFiles = append(composeFiles, file)
+				}
+				if err = os.Setenv("COMPOSE_FILE", strings.Join(composeFiles, ":")); err != nil {
+					return err
+				}
+			}
+
+			if err := syscall.Exec(ComposeBinary, args, os.Environ()); err != nil {
+				return err
+			}
+		}
+	} else if args[0] == "ssh-forwarder" {
+		client, err := docker.NewClientFromEnv()
+		if err != nil {
+			return err
+		}
+
+		// TODO: Make timeout configurable
+		ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+		defer cancel()
+
+		forwarder := devenv.NewSSHForwarder(client, logger)
+		if err := forwarder.Run(ctx); err != nil {
+			logger.Printf("ssh-forwarder: %v\n", err)
+			return err
+		}
+	} else {
+		usage()
+		return fmt.Errorf("unknown command: %v", args[0])
+	}
+	return nil
+}
+
+func usage() {
+	fmt.Println("Usage: devenv COMMAND [arg...]")
+	fmt.Println("       devenv [--help]")
+	fmt.Println("")
+	fmt.Println("A developer-friendly wrapper for Docker")
+	fmt.Println("")
+	fmt.Println("Commands:")
+	fmt.Println("  version Displays the installed version of `devenv`")
+	fmt.Println("  docker  Sets up a Docker environment and executes `docker`")
+	fmt.Println("  compose Sets up a Docker environment and executes `docker-compose`")
+	fmt.Println("")
+	fmt.Println("Examples:")
+	fmt.Println("  devenv docker ps")
+	fmt.Println("  devenv compose up")
+	fmt.Println("")
+}
+
+func buildLogger() *log.Logger {
+	var w io.Writer
+
+	w, err := syslog.New(syslog.LOG_NOTICE|syslog.LOG_DAEMON, "devenv")
+	if err != nil {
+		return log.New(os.Stderr, "devenv", log.LstdFlags)
+	}
+
+	return log.New(w, "", 0)
+}

cmd/jib/main.go
@@ -1,0 +1,128 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+
+	"git.dev.pardot.com/Pardot/bread/jib/github"
+
+	"git.dev.pardot.com/Pardot/bread/jib"
+)
+
+type urlFlag struct {
+	URL *url.URL
+}
+
+func (f *urlFlag) Set(s string) error {
+	url, err := url.Parse(s)
+	if err != nil {
+		return err
+	}
+
+	*f.URL = *url
+	return nil
+}
+func (f *urlFlag) String() string {
+	if f.URL == nil {
+		return ""
+	}
+	return f.URL.String()
+}
+
+type config struct {
+	port           int
+	githubBaseURL  *url.URL
+	githubUser     string
+	githubAPIToken string
+	githubOrg      string
+	pollDuration   time.Duration
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	config := &config{
+		githubBaseURL: &url.URL{
+			Scheme: "https",
+			Host:   "git.dev.pardot.com",
+			Path:   "/api/v3/",
+		},
+	}
+	flags := flag.CommandLine
+	flags.IntVar(&config.port, "port", 8080, "Listen port for the web service")
+	flags.Var(&urlFlag{URL: config.githubBaseURL}, "github-base-url", "Base URL for the GitHub API")
+	flags.StringVar(&config.githubUser, "github-user", "", "GitHub username")
+	flags.StringVar(&config.githubAPIToken, "github-api-token", "", "GitHub API token")
+	flags.StringVar(&config.githubOrg, "github-org", "", "GitHub organization name")
+	flags.DurationVar(&config.pollDuration, "poll-duration", 30*time.Second, "Duration between polls of open pull requests")
+	// Allow setting flags via environment variables
+	flags.VisitAll(func(f *flag.Flag) {
+		k := strings.ToUpper(strings.Replace(f.Name, "-", "_", -1))
+		if v := os.Getenv(k); v != "" {
+			if err := f.Value.Set(v); err != nil {
+				panic(err)
+			}
+		}
+	})
+
+	if err := flags.Parse(os.Args[1:]); err != nil {
+		return err
+	}
+	if config.githubUser == "" {
+		return errors.New("required flag missing: github-user")
+	} else if config.githubAPIToken == "" {
+		return errors.New("required flag missing: github-api-token")
+	} else if config.githubOrg == "" {
+		return errors.New("required flag missing: github-org")
+	} else if config.githubBaseURL == nil {
+		return errors.New("required flag missing: github-base-url")
+	}
+
+	handlers := []jib.PullRequestHandler{
+		jib.InfoHandler,
+		jib.StaleHandler,
+		jib.MergeCommandHandler,
+	}
+	log := log.New(os.Stdout, "", log.LstdFlags)
+	gh := github.NewClient(config.githubBaseURL, config.githubUser, config.githubAPIToken)
+
+	// TODO(alindeman): Implement a real webhook server
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", config.port),
+		Handler: http.NotFoundHandler(),
+	}
+	go func() { panic(server.ListenAndServe()) }()
+
+	for {
+		openPRs, err := gh.GetOpenPullRequests(config.githubOrg)
+		if err != nil {
+			// TODO(alindeman): report to sentry
+			fmt.Fprintf(os.Stderr, "error fetching open PRs, will retry: %v\n", err)
+		} else {
+			for _, pr := range openPRs {
+				for _, handler := range handlers {
+					err := handler(log, gh, pr)
+					if err != nil {
+						// An error in a handler is worrisome, but not fatal
+						// TODO(alindeman): report to sentry
+						fmt.Fprintf(os.Stderr, "error in %v handler: %v\n", handler, err)
+					}
+				}
+			}
+		}
+
+		<-time.After(config.pollDuration)
+	}
+}

cmd/privet/main.go
@@ -1,0 +1,229 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"git.dev.pardot.com/Pardot/bread/privet"
+)
+
+type stringListFlag []string
+
+func (f *stringListFlag) String() string {
+	return fmt.Sprintf("%v", *f)
+}
+func (f *stringListFlag) Set(value string) error {
+	*f = append(*f, value)
+	return nil
+}
+
+var (
+	testManifest        string
+	previousResultsDir  string
+	numWorkers          int
+	targetDuration      time.Duration
+	defaultTestDuration time.Duration
+	planFile            string
+	commandPath         string
+	worker              int
+	parallelism         int
+	envVars             stringListFlag
+)
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options] COMMAND\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "Commands:\n")
+		fmt.Fprintf(os.Stderr, "  plan    Creates a plan\n")
+		fmt.Fprintf(os.Stderr, "  execute Executes a plan\n")
+		fmt.Fprintf(os.Stderr, "  help    Shows this help\n")
+		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "Options:\n")
+		flag.PrintDefaults()
+	}
+	flag.StringVar(&testManifest, "test-files-file", "", "A file containing a JSON array of test files to be run. The format is documented in the Privet examples directory.")
+	flag.StringVar(&testManifest, "test-manifest", "", "A file containing a JSON array of test files to be run. The format is documented in the Privet examples directory.")
+	flag.StringVar(&previousResultsDir, "previous-results-dir", "", "A directory containing JUnit-formatted XML files from a previous build. A SHASUMS file is also expected to exist containing fingerprints of the test files from this previous build. (optional, but helpful for plan)")
+	flag.IntVar(&numWorkers, "num-workers", 0, "The number of workers to plan (required for plan)")
+	flag.DurationVar(&targetDuration, "target-duration", 30*time.Second, "The target duration for one test batch. Should be balanced between too short (where startup/teardown time becomes an issue) and too long (where one worker might run much longer than the rest)")
+	flag.DurationVar(&defaultTestDuration, "default-test-duration", 1*time.Second, "The default duration assumed for a test, if it cannot be found in the previous results")
+	flag.StringVar(&planFile, "plan-file", "", "Path to the plan file (required for execute)")
+	flag.StringVar(&commandPath, "command-path", "", "Command to execute for each test batch (required for execute)")
+	flag.IntVar(&worker, "worker", -1, "Worker identifier (required for execute)")
+	flag.IntVar(&parallelism, "parallelism", 1, "Number of independent processes to spawn to process this worker's queue")
+	flag.Var(&envVars, "env", "Environment variable (foo=bar) to pass to the command (can be specified multiple times)")
+	flag.Parse()
+
+	switch flag.Arg(0) {
+	case "plan":
+		if err := doPlan(); err != nil {
+			fmt.Fprintf(os.Stderr, "privet error: %v\n", err)
+			os.Exit(1)
+		}
+	case "execute":
+		success, err := doExecute()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "privet error: %v\n", err)
+			fmt.Fprintf(os.Stderr, "The build failed due to an unexpected failure. Please open a ticket on https://jira.dev.pardot.com/browse/BREAD for the BREAD team to look into.")
+			os.Exit(1)
+		} else if !success {
+			fmt.Fprintf(os.Stderr, "The build failed because at least one test job failed.")
+			os.Exit(1)
+		} else {
+			os.Exit(0)
+		}
+	case "help":
+		flag.Usage()
+		os.Exit(0)
+	default:
+		flag.Usage()
+		os.Exit(1)
+	}
+}
+
+func doPlan() error {
+	opts := &privet.PlanCreationOpts{}
+
+	if testManifest == "" {
+		return errors.New("test-manifest is required")
+	}
+	testFiles, err := loadTestManifest(testManifest)
+	if err != nil {
+		return err
+	}
+	opts.TestFiles = testFiles
+
+	if previousResultsDir != "" {
+		testRunResults, err := loadResultsDirectory(previousResultsDir)
+		if err != nil {
+			return err
+		}
+		opts.PreviousResults = testRunResults
+	}
+
+	if numWorkers <= 0 {
+		return errors.New("num-workers is required")
+	}
+	opts.NumWorkers = numWorkers
+
+	opts.TargetDuration = targetDuration
+	opts.DefaultTestDuration = defaultTestDuration
+
+	plan, err := privet.CreatePlan(opts)
+	if err != nil {
+		return err
+	}
+
+	encoder := json.NewEncoder(os.Stdout)
+	if err := encoder.Encode(plan); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func doExecute() (bool, error) {
+	opts := &privet.PlanExecutionOpts{}
+
+	if commandPath == "" {
+		return false, errors.New("command-path is required")
+	}
+	opts.CommandPath = commandPath
+
+	if planFile == "" {
+		return false, errors.New("plan-file is required")
+	}
+	plan, err := loadPlan(planFile)
+	if err != nil {
+		return false, err
+	}
+
+	if worker < 0 {
+		return false, errors.New("worker is required")
+	}
+	opts.Worker = worker
+	opts.Parallelism = parallelism
+	opts.Env = envVars
+
+	return privet.ExecutePlan(plan, opts)
+}
+
+func loadPlan(file string) (*privet.Plan, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = f.Close() }()
+
+	decoder := json.NewDecoder(f)
+
+	var plan privet.Plan
+	if err := decoder.Decode(&plan); err != nil {
+		return nil, err
+	}
+	return &plan, nil
+}
+
+func loadTestManifest(file string) ([]*privet.TestFile, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = f.Close() }()
+
+	decoder := json.NewDecoder(f)
+
+	var testFiles []*privet.TestFile
+	if err := decoder.Decode(&testFiles); err != nil {
+		return nil, err
+	}
+
+	return testFiles, nil
+}
+
+func loadResultsDirectory(directory string) (privet.TestRunResults, error) {
+	files, err := filepath.Glob(filepath.Join(directory, "*.xml"))
+	if err != nil {
+		return nil, err
+	}
+
+	results := privet.TestRunResults{}
+	for _, file := range files {
+		f, err := os.Open(file)
+		if err != nil {
+			return nil, err
+		}
+
+		fileResults, err := privet.ParseJunitResult(f)
+		if err != nil {
+			_ = f.Close()
+			return nil, err
+		}
+
+		results.Merge(fileResults)
+		_ = f.Close()
+	}
+
+	shasumFile := filepath.Join(directory, "SHASUMS")
+	if _, err := os.Stat(shasumFile); err == nil {
+		f, err := os.Open(shasumFile)
+		if err != nil {
+			return nil, err
+		}
+
+		if err := privet.PopulateFingerprintsFromShasumsFile(results, f); err != nil {
+			_ = f.Close()
+			return nil, err
+		}
+
+		_ = f.Close()
+	}
+
+	return results, nil
+}

commit 3d1f49a8149ea1c16678d6fbdf49574980bdafc3

cmd/operatord: Fix name of the proto package used when matching chatops commands to RPCs
This is name of the *protobuf* package, not Go package. In other words,
the command "!deploy trigger" translates to the protobuf "Trigger" RPC
on the bread.Deploy service.

I broke this in 4e5ee1d0f74538166c879963ddf017d1c59c1ef3.

cmd/operatord/main.go
@@ -259,7 +259,7 @@ func run(invoker operator.InvokerFunc) error {
 		if err != nil {
 			return err
 		}
-		const pkg = "git.dev.pardot.com/Pardot/bread"
+		const pkg = "bread"
 		if webhookHandler, err = bread.NewHipchatHandler(
 			context.Background(),
 			inst,