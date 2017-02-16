452cbaf226fa7507ad19536e1d0181379f1fb64f
Jan Ulrich <julrich@salesforce.com>
2017-02-16 11:31:09 -0800

    W-3698393 Adding logging to debug the hanging deployment

--- src/pull_agent/lib/pull_agent/deployers/pardot.rb
+++ src/pull_agent/lib/pull_agent/deployers/pardot.rb
@@ -24,10 +24,13 @@ module PullAgent
           quick_rollback = QuickRollback.new(release_directory, @deploy)
           unless quick_rollback.perform
             Dir.mktmpdir do |temp_dir|
+              Logger.log(:debug, "Fetching #{@deploy.artifact_url} to #{temp_dir}")
               ArtifactFetcher.new(@deploy.artifact_url).fetch_into(temp_dir)
+              Logger.log(:debug, "Done Fetching, now syncing to #{release_directory.standby_directory}")
               DirectorySynchronizer.new(temp_dir, release_directory.standby_directory).synchronize
 
               @deploy.to_build_version.save_to_directory(release_directory.standby_directory)
+              Logger.log(:debug, "Making standby directory live")
               release_directory.make_standby_directory_live
             end
           end
