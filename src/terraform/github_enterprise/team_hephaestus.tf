0b75198532582a239048e1430c299c1bcc8f4863
Dave Leach <dleach@salesforce.com>
2017-02-23 10:36:30 -0500

    W-3689260: Added Maphet as a Hephaestus maintainer

--- src/terraform/github_enterprise/team_hephaestus.tf
+++ src/terraform/github_enterprise/team_hephaestus.tf
@@ -4,32 +4,14 @@ resource "github_team" "hephaestus" {
   privacy     = "closed"
 }
 
-resource "github_team_membership" "hephaestus_dave-leach" {
+resource "github_team_membership" "hephaestus_nathan-maphet" {
   team_id  = "${github_team.hephaestus.id}"
-  username = "dave-leach"
-  role     = "maintainer"
-}
-
-resource "github_team_membership" "hephaestus_john-ulman" {
-  team_id  = "${github_team.hephaestus.id}"
-  username = "john-ulman"
+  username = "nathan-maphet"
   role     = "maintainer"
 }
 
 resource "github_team_membership" "hephaestus_oliver-albrecht" {
   team_id  = "${github_team.hephaestus.id}"
   username = "oliver-albrecht"
-  role     = "maintainer"
-}
-
-resource "github_team_membership" "hephaestus_samuel-kim" {
-  team_id  = "${github_team.hephaestus.id}"
-  username = "samuel-kim"
-  role     = "maintainer"
-}
-
-resource "github_team_membership" "hephaestus_steve-schraudner" {
-  team_id  = "${github_team.hephaestus.id}"
-  username = "steve-schraudner"
   role     = "maintainer"
 }
