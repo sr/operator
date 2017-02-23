68d60e3217f4faf9157791f0468ebdb809b9ecf7
Dave Leach <dleach@salesforce.com>
2017-02-23 09:40:22 -0500

    W-3689260: Defined Hephaestus maintainers

--- src/terraform/github_enterprise/team_hephaestus.tf
+++ src/terraform/github_enterprise/team_hephaestus.tf
@@ -4,8 +4,32 @@ resource "github_team" "hephaestus" {
   privacy     = "closed"
 }
 
+resource "github_team_membership" "hephaestus_dave-leach" {
+  team_id  = "${github_team.hephaestus.id}"
+  username = "dave-leach"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "hephaestus_john-ulman" {
+  team_id  = "${github_team.hephaestus.id}"
+  username = "john-ulman"
+  role     = "maintainer"
+}
+
 resource "github_team_membership" "hephaestus_oliver-albrecht" {
   team_id  = "${github_team.hephaestus.id}"
   username = "oliver-albrecht"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "hephaestus_samuel-kim" {
+  team_id  = "${github_team.hephaestus.id}"
+  username = "samuel-kim"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "hephaestus_steve-schraudner" {
+  team_id  = "${github_team.hephaestus.id}"
+  username = "steve-schraudner"
   role     = "maintainer"
 }
