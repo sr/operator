45fa74a9d5b9ee3060a04e7b4be7fb7d1b44a7ce
Andy Lindeman <alindeman@salesforce.com>
2017-03-13 12:30:57 -0400

    Adds build plans for the rest of the repositories

--- terraform/bamboo/build_plans.tf
+++ terraform/bamboo/build_plans.tf
@@ -277,3 +277,291 @@ resource "bamboo_build_plan" "PDT-WFST" {
   name                  = "Workflow Stats"
   default_repository_id = "${bamboo_repository.workflow-stats.id}"
 }
+
+resource "bamboo_build_plan" "PDT-SYM" {
+  key                   = "PDT-SYM"
+  name                  = "symfony"
+  default_repository_id = "${bamboo_repository.symfony.id}"
+}
+
+resource "bamboo_build_plan" "BREAD-BEI" {
+  key                   = "BREAD-BEI"
+  name                  = "bamboo-elastic-instance"
+  default_repository_id = "${bamboo_repository.bamboo-elastic-instance.id}"
+}
+
+resource "bamboo_build_plan" "PDT-SYMDIC" {
+  key                   = "PDT-SYMDIC"
+  name                  = "symfony-dic"
+  default_repository_id = "${bamboo_repository.symfony-dic.id}"
+}
+
+resource "bamboo_build_plan" "PDT-DC" {
+  key                   = "PDT-DC"
+  name                  = "Discovery-Client"
+  default_repository_id = "${bamboo_repository.Discovery-Client.id}"
+}
+
+resource "bamboo_build_plan" "PDT-DA" {
+  key                   = "PDT-DA"
+  name                  = "Discovery-Agent"
+  default_repository_id = "${bamboo_repository.Discovery-Agent.id}"
+}
+
+resource "bamboo_build_plan" "PDT-JSNIP" {
+  key                   = "PDT-JSNIP"
+  name                  = "java-snippets"
+  default_repository_id = "${bamboo_repository.java-snippets.id}"
+}
+
+resource "bamboo_build_plan" "PDT-AMQPLIB" {
+  key                   = "PDT-AMQPLIB"
+  name                  = "php-amqplib"
+  default_repository_id = "${bamboo_repository.php-amqplib.id}"
+}
+
+resource "bamboo_build_plan" "PDT-ASKEET" {
+  key                   = "PDT-ASKEET"
+  name                  = "askeet"
+  default_repository_id = "${bamboo_repository.askeet.id}"
+}
+
+resource "bamboo_build_plan" "PDT-MURDA" {
+  key                   = "PDT-MURDA"
+  name                  = "murda"
+  default_repository_id = "${bamboo_repository.murda.id}"
+}
+
+resource "bamboo_build_plan" "PDT-ATB" {
+  key                   = "PDT-ATB"
+  name                  = "all-the-bacon"
+  default_repository_id = "${bamboo_repository.all-the-bacon.id}"
+}
+
+resource "bamboo_build_plan" "PDT-KENDO" {
+  key                   = "PDT-KENDO"
+  name                  = "kendo"
+  default_repository_id = "${bamboo_repository.kendo.id}"
+}
+
+resource "bamboo_build_plan" "PDT-SRLP" {
+  key                   = "PDT-SRLP"
+  name                  = "SalesReachLicenseProvisioning"
+  default_repository_id = "${bamboo_repository.SalesReachLicenseProvisioning.id}"
+}
+
+resource "bamboo_build_plan" "PDT-EM" {
+  key                   = "PDT-EM"
+  name                  = "encryptionmanager"
+  default_repository_id = "${bamboo_repository.encryptionmanager.id}"
+}
+
+resource "bamboo_build_plan" "PDT-CSRF" {
+  key                   = "PDT-CSRF"
+  name                  = "csrf-php"
+  default_repository_id = "${bamboo_repository.csrf-php.id}"
+}
+
+resource "bamboo_build_plan" "PDT-GMAILC" {
+  key                   = "PDT-GMAILC"
+  name                  = "gmail-chrome"
+  default_repository_id = "${bamboo_repository.gmail-chrome.id}"
+}
+
+resource "bamboo_build_plan" "PDT-LDA" {
+  key                   = "PDT-LDA"
+  name                  = "lead-deck-app"
+  default_repository_id = "${bamboo_repository.lead-deck-app.id}"
+}
+
+resource "bamboo_build_plan" "PDT-SDEMOP" {
+  key                   = "PDT-SDEMOP"
+  name                  = "salesforce-demo-package"
+  default_repository_id = "${bamboo_repository.salesforce-demo-package.id}"
+}
+
+resource "bamboo_build_plan" "PDT-YUBILDAP" {
+  key                   = "PDT-YUBILDAP"
+  name                  = "yubikey-ldap"
+  default_repository_id = "${bamboo_repository.yubikey-ldap.id}"
+}
+
+resource "bamboo_build_plan" "PDT-BO" {
+  key                   = "PDT-BO"
+  name                  = "breakout"
+  default_repository_id = "${bamboo_repository.breakout.id}"
+}
+
+resource "bamboo_build_plan" "PDT-SFDEPL" {
+  key                   = "PDT-SFDEPL"
+  name                  = "SFdeploy"
+  default_repository_id = "${bamboo_repository.SFdeploy.id}"
+}
+
+resource "bamboo_build_plan" "PDT-AMQPLIB2" {
+  key                   = "PDT-AMQPLIB2"
+  name                  = "php-amqplib2"
+  default_repository_id = "${bamboo_repository.php-amqplib2.id}"
+}
+
+resource "bamboo_build_plan" "PDT-KBARTICLE" {
+  key                   = "PDT-KBARTICLE"
+  name                  = "kb-articles"
+  default_repository_id = "${bamboo_repository.kb-articles.id}"
+}
+
+resource "bamboo_build_plan" "PDT-HOMEBREW" {
+  key                   = "PDT-HOMEBREW"
+  name                  = "pd-homebrew"
+  default_repository_id = "${bamboo_repository.pd-homebrew.id}"
+}
+
+resource "bamboo_build_plan" "PDT-STACKI" {
+  key                   = "PDT-STACKI"
+  name                  = "ops-stacki"
+  default_repository_id = "${bamboo_repository.ops-stacki.id}"
+}
+
+resource "bamboo_build_plan" "PDT-FRAMEJS" {
+  key                   = "PDT-FRAMEJS"
+  name                  = "frame-js"
+  default_repository_id = "${bamboo_repository.frame-js.id}"
+}
+
+resource "bamboo_build_plan" "PDT-PARMET" {
+  key                   = "PDT-PARMET"
+  name                  = "ParMeter"
+  default_repository_id = "${bamboo_repository.ParMeter.id}"
+}
+
+resource "bamboo_build_plan" "PDT-RPMS" {
+  key                   = "PDT-RPMS"
+  name                  = "rpms"
+  default_repository_id = "${bamboo_repository.rpms.id}"
+}
+
+resource "bamboo_build_plan" "PDT-MSP" {
+  key                   = "PDT-MSP"
+  name                  = "mesh-sync-package"
+  default_repository_id = "${bamboo_repository.mesh-sync-package.id}"
+}
+
+resource "bamboo_build_plan" "PDT-KSLDS" {
+  key                   = "PDT-KSLDS"
+  name                  = "kendo-slds"
+  default_repository_id = "${bamboo_repository.kendo-slds.id}"
+}
+
+resource "bamboo_build_plan" "PDT-PSTORMEX" {
+  key                   = "PDT-PSTORMEX"
+  name                  = "pardot-storm-example"
+  default_repository_id = "${bamboo_repository.pardot-storm-example.id}"
+}
+
+resource "bamboo_build_plan" "PDT-PMAP" {
+  key                   = "PDT-PMAP"
+  name                  = "poor-mans-asset-pipeline"
+  default_repository_id = "${bamboo_repository.poor-mans-asset-pipeline.id}"
+}
+
+resource "bamboo_build_plan" "PDT-PCSWG" {
+  key                   = "PDT-PCSWG"
+  name                  = "pcswg"
+  default_repository_id = "${bamboo_repository.pcswg.id}"
+}
+
+resource "bamboo_build_plan" "PDT-WAVE" {
+  key                   = "PDT-WAVE"
+  name                  = "wave"
+  default_repository_id = "${bamboo_repository.wave.id}"
+}
+
+resource "bamboo_build_plan" "PDT-TD" {
+  key                   = "PDT-TD"
+  name                  = "testdata"
+  default_repository_id = "${bamboo_repository.testdata.id}"
+}
+
+resource "bamboo_build_plan" "PDT-CRUMB" {
+  key                   = "PDT-CRUMB"
+  name                  = "crumb"
+  default_repository_id = "${bamboo_repository.crumb.id}"
+}
+
+resource "bamboo_build_plan" "PDT-II" {
+  key                   = "PDT-II"
+  name                  = "insert_ignore"
+  default_repository_id = "${bamboo_repository.insert_ignore.id}"
+}
+
+resource "bamboo_build_plan" "PDT-KT" {
+  key                   = "PDT-KT"
+  name                  = "kafka-tools"
+  default_repository_id = "${bamboo_repository.kafka-tools.id}"
+}
+
+resource "bamboo_build_plan" "PDT-AD" {
+  key                   = "PDT-AD"
+  name                  = "alert-dispatcher"
+  default_repository_id = "${bamboo_repository.alert-dispatcher.id}"
+}
+
+resource "bamboo_build_plan" "PDT-WAX" {
+  key                   = "PDT-WAX"
+  name                  = "wax"
+  default_repository_id = "${bamboo_repository.wax.id}"
+}
+
+resource "bamboo_build_plan" "PDT-SM" {
+  key                   = "PDT-SM"
+  name                  = "swiftmailer"
+  default_repository_id = "${bamboo_repository.swiftmailer.id}"
+}
+
+resource "bamboo_build_plan" "PDT-HBPHP" {
+  key                   = "PDT-HBPHP"
+  name                  = "pd-homebrew-php"
+  default_repository_id = "${bamboo_repository.pd-homebrew-php.id}"
+}
+
+resource "bamboo_build_plan" "PDT-CUMUCI" {
+  key                   = "PDT-CUMUCI"
+  name                  = "CumulusCI"
+  default_repository_id = "${bamboo_repository.CumulusCI.id}"
+}
+
+resource "bamboo_build_plan" "PDT-PARDOVE" {
+  key                   = "PDT-PARDOVE"
+  name                  = "ParDove"
+  default_repository_id = "${bamboo_repository.ParDove.id}"
+}
+
+resource "bamboo_build_plan" "PDT-PESP" {
+  key                   = "PDT-PESP"
+  name                  = "pardot-es-parser"
+  default_repository_id = "${bamboo_repository.pardot-es-parser.id}"
+}
+
+resource "bamboo_build_plan" "PDT-GEOIP" {
+  key                   = "PDT-GEOIP"
+  name                  = "geoip-api-php"
+  default_repository_id = "${bamboo_repository.geoip-api-php.id}"
+}
+
+resource "bamboo_build_plan" "PDT-SSVT" {
+  key                   = "PDT-SSVT"
+  name                  = "StormSupervisorValidationTool"
+  default_repository_id = "${bamboo_repository.StormSupervisorValidationTool.id}"
+}
+
+resource "bamboo_build_plan" "PDT-SAT" {
+  key                   = "PDT-SAT"
+  name                  = "salesforce-actions-topologies"
+  default_repository_id = "${bamboo_repository.salesforce-actions-topologies.id}"
+}
+
+resource "bamboo_build_plan" "PDT-RR" {
+  key                   = "PDT-RR"
+  name                  = "redis-roaring"
+  default_repository_id = "${bamboo_repository.redis-roaring.id}"
+}
