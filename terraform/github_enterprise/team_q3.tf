557e41acf915ab1ed9872ff26a898ec8bd503397
troy.hill <troy.hill@salesforce.com>
2017-03-14 12:03:34 -0400

    Update team_q3.tf
    fixed team name

--- terraform/github_enterprise/team_q3.tf
+++ terraform/github_enterprise/team_q3.tf
@@ -1,36 +1,36 @@
 
 resource "github_team" "q3" {
-  name        = "Q3"
+  name        = "q3"
   description = "https://gus.lightning.force.com/one/one.app#/sObject/a00B0000005MSsZIAW/view"
   privacy     = "closed"
 }
 
 resource "github_team_membership" "q3_troy-hill" {
-  team_id  = "${github_team.daleks.id}"
+  team_id  = "${github_team.q3.id}"
   username = "troy-hill"
   role     = "maintainer"
 }
 
 resource "github_team_membership" "q3_heather-dartz" {
-  team_id  = "${github_team.daleks.id}"
+  team_id  = "${github_team.q3.id}"
   username = "heather-dartz"
   role     = "maintainer"
 }
 
 resource "github_team_membership" "q3_tinny-washington" {
-  team_id  = "${github_team.daleks.id}"
+  team_id  = "${github_team.q3.id}"
   username = "tinny-washington"
   role     = "maintainer"
 }
 
 resource "github_team_membership" "q3_bernalpatrick" {
-  team_id  = "${github_team.daleks.id}"
+  team_id  = "${github_team.q3.id}"
   username = "bernalpatrick"
   role     = "maintainer"
 }
 
 resource "github_team_membership" "q3_jeff-elrod" {
-  team_id  = "${github_team.daleks.id}"
+  team_id  = "${github_team.q3.id}"
   username = "jeff-elrod"
   role     = "maintainer"
 }
