065b4a807d0cdae3b3d68c5e5c7bf640de7fe7a0
Andy Lindeman <alindeman@salesforce.com>
2017-03-23 08:11:50 -0400

    Improves formatting

--- bamboo-elastic-instance/provisioners/bin/ci-vault.sh
+++ bamboo-elastic-instance/provisioners/bin/ci-vault.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+set -euo pipefail
 
 readonly VAULT_SERVER="https://vault.dev.pardot.com"
 readonly ARTIFACTORY_ENV_FILE="/etc/env/artifactoryenv"
@@ -39,15 +40,18 @@ for dir in /etc/env /etc/aws /etc/mvn /etc/mvn/settings_xmls /etc/clover /etc/do
 done
 
 # populate files
-echo "PROTOCOL=https" > $ARTIFACTORY_ENV_FILE
-# shellcheck disable=SC2129
-echo "ARTIFACTORYHOST=artifactory-internal.dev.pardot.com" >> $ARTIFACTORY_ENV_FILE
-echo "CONTEXTPATH=artifactory" >> $ARTIFACTORY_ENV_FILE
-echo "USERNAME=\"$(echo "$ci_secrets" | jq -r .data.artifactory.username)\"" >> $ARTIFACTORY_ENV_FILE
-echo "PASSWORD=\"$(echo "$ci_secrets" | jq -r .data.artifactory.password)\"" >> $ARTIFACTORY_ENV_FILE
+{
+  echo "PROTOCOL=https"
+  echo "ARTIFACTORYHOST=artifactory-internal.dev.pardot.com"
+  echo "CONTEXTPATH=artifactory"
+  echo "USERNAME=\"$(echo "$ci_secrets" | jq -r .data.artifactory.username)\""
+  echo "PASSWORD=\"$(echo "$ci_secrets" | jq -r .data.artifactory.password)\""
+} >"$ARTIFACTORY_ENV_FILE"
 
-echo "BAMBOO_USERNAME='$(echo "$ci_secrets" | jq -r .data.bamboo.username)'" > $BAMBOO_ENV_FILE
-echo "BAMBOO_PASSWORD='$(echo "$ci_secrets" | jq -r .data.bamboo.password)'" >> $BAMBOO_ENV_FILE
+{
+  echo "BAMBOO_USERNAME='$(echo "$ci_secrets" | jq -r .data.bamboo.username)'"
+  echo "BAMBOO_PASSWORD='$(echo "$ci_secrets" | jq -r .data.bamboo.password)'"
+} >"$BAMBOO_ENV_FILE"
 
 mkdir -p "$BAMBOO_HOME/.ssh"
 echo "$ci_secrets" | jq -r .data.bamboo.rsa | base64 --decode > "$BAMBOO_HOME/.ssh/id_rsa"
@@ -61,27 +65,33 @@ chown bamboo: "$ROOT_HOME/.ssh" "$ROOT_HOME/.ssh/id_rsa"
 chmod 0700 "$ROOT_HOME/.ssh"
 chmod 0600 "$ROOT_HOME/.ssh/id_rsa"
 
-echo "[default]" > $SA_BREAD_ENV_FILE
-echo "aws_access_key_id = $(echo "$ci_secrets" | jq -r .data.aws_sa_bread.access_key)" >> $SA_BREAD_ENV_FILE
-echo "aws_secret_access_key = $(echo "$ci_secrets" | jq -r .data.aws_sa_bread.secret_key)" >> $SA_BREAD_ENV_FILE
+{
+  echo "[default]"
+  echo "aws_access_key_id = $(echo "$ci_secrets" | jq -r .data.aws_sa_bread.access_key)"
+  echo "aws_secret_access_key = $(echo "$ci_secrets" | jq -r .data.aws_sa_bread.secret_key)"
+} >"$SA_BREAD_ENV_FILE"
 
-echo "[pardotops]" > $AWS_CRED_FILE
-echo "aws_access_key_id = $(echo "$ci_secrets" | jq -r .data.aws_pardotops.access_key)" >> $AWS_CRED_FILE
-echo "aws_secret_access_key = $(echo "$ci_secrets" | jq -r .data.aws_pardotops.secret_key)" >> $AWS_CRED_FILE
+{
+  echo "[pardotops]"
+  echo "aws_access_key_id = $(echo "$ci_secrets" | jq -r .data.aws_pardotops.access_key)"
+  echo "aws_secret_access_key = $(echo "$ci_secrets" | jq -r .data.aws_pardotops.secret_key)"
+} >"$AWS_CRED_FILE"
 
-echo "HIPCHAT_TOKEN=\"$(echo "$ci_secrets" | jq -r .data.hipchat.token)\"" > $HIPCHAT_FILE
-# shellcheck disable=SC2129
-echo "HIPCHAT_ROOM_ID=\"82\"" >> $HIPCHAT_FILE
-echo "HIPCHAT_FROM=\"Bamboo\"" >> $HIPCHAT_FILE
-echo "HIPCHAT_COLOR=\"random\"" >> $HIPCHAT_FILE
-echo "HIPCHAT_FORMAT=\"html\"" >> $HIPCHAT_FILE
-echo "HIPCHAT_MESSAGE=\"unconfigured hipchat message\"" >> $HIPCHAT_FILE
-echo "HIPCHAT_NOTIFY=\"false\"" >> $HIPCHAT_FILE
-echo "HIPCHAT_HOST=\"hipchat.dev.pardot.com\"" >> $HIPCHAT_FILE
-echo "HIPCHAT_API=\"v1\"" >> $HIPCHAT_FILE
+{
+  echo "HIPCHAT_TOKEN=\"$(echo "$ci_secrets" | jq -r .data.hipchat.token)\""
+  # shellcheck disable=SC2129
+  echo "HIPCHAT_ROOM_ID=\"82\""
+  echo "HIPCHAT_FROM=\"Bamboo\""
+  echo "HIPCHAT_COLOR=\"random\""
+  echo "HIPCHAT_FORMAT=\"html\""
+  echo "HIPCHAT_MESSAGE=\"unconfigured hipchat message\""
+  echo "HIPCHAT_NOTIFY=\"false\""
+  echo "HIPCHAT_HOST=\"hipchat.dev.pardot.com\""
+  echo "HIPCHAT_API=\"v1\""
+} >"$HIPCHAT_FILE"
 
-echo "$ci_secrets" | jq -r .data.mvn.settings.sa_bamboo | base64 --decode > $MVN_SETTINGS_FILE
+echo "$ci_secrets" | jq -r .data.mvn.settings.sa_bamboo | base64 --decode >"$MVN_SETTINGS_FILE"
 
-echo "$ci_secrets" | jq -r .data.clover.license | base64 --decode > $CLOVER_FILE
+echo "$ci_secrets" | jq -r .data.clover.license | base64 --decode >"$CLOVER_FILE"
 
-echo "$ci_secrets" | jq -r .data.docker.config | base64 --decode > $DOCKER_CONFIG_FILE
+echo "$ci_secrets" | jq -r .data.docker.config | base64 --decode >"$DOCKER_CONFIG_FILE"
