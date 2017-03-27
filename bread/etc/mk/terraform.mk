4acc167b4e943050f2907ef09deb867d80e5b588
Andy Lindeman <alindeman@salesforce.com>
2017-03-27 14:06:40 -0400

    Updates terra and terra2 to deal with new state files

--- bread/etc/mk/terraform.mk
+++ bread/etc/mk/terraform.mk
@@ -22,19 +22,9 @@ apply: $(TERRAFORM) $(TERRAFORM_DIR) $(TERRAFORM_PLAN) remote-state
 		$< apply $(TERRAFORM_OPTS) $(TERRAFORM_PLAN)
 	rm -f $(TERRAFORM_PLAN)
 
-plan: $(TERRAFORM) $(TERRAFORM_DIR) validate remote-state
+pull: $(TERRAFORM) $(TERRAFORM_DIR)
 	cd $(TERRAFORM_DIR) && \
-		$< plan -out $(TERRAFORM_PLAN) -var-file=$(TERRAFORM_VAR_FILE) $(TERRAFORM_OPTS)
-
-pull: $(TERRAFORM) $(TERRAFORM_DIR) remote-state
-	cd $(TERRAFORM_DIR) && \
-		cp .terraform/terraform.tfstate .terraform/terraform.tfstate.$(shell date +%Y%m%d%H%M%S); \
-		$< remote pull $(TERRAFORM_OPTS)
-
-push: $(TERRAFORM) $(TERRAFORM_DIR)
-	cd $(TERRAFORM_DIR) && \
-		cp .terraform/terraform.tfstate .terraform/terraform.tfstate.$(shell date +%Y%m%d%H%M%S); \
-		$< remote push $(TERRAFORM_OPTS)
+		$< state pull $(TERRAFORM_OPTS)
 
 import: $(TERRAFORM) $(TERRAFORM_DIR) $(TERRAFORM_VAR_FILE) remote-state
 	cd $(TERRAFORM_DIR) && \
@@ -43,28 +33,7 @@ import: $(TERRAFORM) $(TERRAFORM_DIR) $(TERRAFORM_VAR_FILE) remote-state
 refresh: $(TERRAFORM) $(TERRAFORM_DIR) $(TERRAFORM_VAR_FILE) remote-state
 	$< refresh -var-file=$(TERRAFORM_VAR_FILE) $(TERRAFORM_OPTS) $(TERRAFORM_DIR)
 
-remote-state: $(TERRAFORM) $(TERRAFORM_DIR)
-	cd $(TERRAFORM_DIR) && \
-		$< remote config \
-			-backend=artifactory \
-			-backend-config="username=$(ARTIFACTORY_USERNAME)" \
-			-backend-config="password=$(ARTIFACTORY_ENCRYPTED_PASSWORD)" \
-			-backend-config="url=$(ARTIFACTORY_URL)" \
-			-backend-config="repo=$(ARTIFACTORY_REPO)" \
-			-backend-config="subpath=$(TERRAFORM_PROJECT)"
-
-validate: $(TERRAFORM)
-	find . -type d -mindepth 2 -not -name '.terraform' -a -not -path './_vendor/*' -print0 | \
-		xargs -0 -n1 echo
-
-$(TERRAFORM):
-	GOPATH="$(BREAD)/../terraform/_vendor" GOBIN="$(GOBIN)" $(GO) install -v github.com/hashicorp/terraform
-
 .PHONY: \
-	apply \
-	plan \
-	remote-state \
-	refresh \
-	validate \
 	pull \
-	push
+	import \
+	refresh
