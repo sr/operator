2097934868f1502128496a178bd7d2cf4408213d
Andy Lindeman <alindeman@salesforce.com>
2017-03-27 15:30:50 -0400

    Removes legacy remote-state setup

--- bread/etc/mk/terraform.mk
+++ bread/etc/mk/terraform.mk
@@ -12,25 +12,17 @@ TERRAFORM_VAR_FILE ?= terraform.tfvars
 
 ARTIFACTORY_USERNAME ?=
 ARTIFACTORY_PASSWORD ?=
-
 -include artifactory.env
-ARTIFACTORY_URL := https://artifactory.dev.pardot.com/artifactory
-ARTIFACTORY_REPO := pd-terraform
-
-apply: $(TERRAFORM) $(TERRAFORM_DIR) $(TERRAFORM_PLAN) remote-state
-	cd $(TERRAFORM_DIR) && \
-		$< apply $(TERRAFORM_OPTS) $(TERRAFORM_PLAN)
-	rm -f $(TERRAFORM_PLAN)
 
 pull: $(TERRAFORM) $(TERRAFORM_DIR)
 	cd $(TERRAFORM_DIR) && \
 		$< state pull $(TERRAFORM_OPTS)
 
-import: $(TERRAFORM) $(TERRAFORM_DIR) $(TERRAFORM_VAR_FILE) remote-state
+import: $(TERRAFORM) $(TERRAFORM_DIR) $(TERRAFORM_VAR_FILE)
 	cd $(TERRAFORM_DIR) && \
 		$< import -var-file=$(TERRAFORM_VAR_FILE) $(TERRAFORM_OPTS)
 
-refresh: $(TERRAFORM) $(TERRAFORM_DIR) $(TERRAFORM_VAR_FILE) remote-state
+refresh: $(TERRAFORM) $(TERRAFORM_DIR) $(TERRAFORM_VAR_FILE)
 	$< refresh -var-file=$(TERRAFORM_VAR_FILE) $(TERRAFORM_OPTS) $(TERRAFORM_DIR)
 
 .PHONY: \
