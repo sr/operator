5990f490a435f67858fe4fa34ecc5ba0293155d9
Andy Lindeman <alindeman@salesforce.com>
2017-03-29 11:56:04 -0400

    Uses subtests

--- bread/jib/fork_handler_test.go
+++ bread/jib/fork_handler_test.go
@@ -16,12 +16,13 @@ func TestForkHandler(t *testing.T) {
 	}
 
 	cases := []struct {
+		name                 string
 		pullRequest          *github.PullRequest
 		expectedReplyComment *issueReplyCommentMatcher
 		expectedToClose      bool
 	}{
-		// PR with the same head user as base user. Should not be closed.
 		{
+			name: "PR with the same head user as base user. Should not be closed.",
 			pullRequest: &github.PullRequest{
 				Org:        "pardot",
 				Repository: "bread",
@@ -33,8 +34,8 @@ func TestForkHandler(t *testing.T) {
 			},
 			expectedToClose: false,
 		},
-		// PR with a different head user as base user. Should be closed.
 		{
+			name: "PR with a different head user as base user. Should be closed.",
 			pullRequest: &github.PullRequest{
 				Org:        "pardot",
 				Repository: "bread",
@@ -47,7 +48,7 @@ func TestForkHandler(t *testing.T) {
 			expectedReplyComment: &issueReplyCommentMatcher{
 				Context: struct{}{},
 				BodyRegexps: []*regexp.Regexp{
-					regexp.MustCompile(`@user.*pull requests originating from forked repositories are not allowed`),
+					regexp.MustCompile(`@user.*pull requests originating from forked repositories are not mergeable`),
 					regexp.MustCompile(`reopen the pull request against that branch`),
 				},
 			},
@@ -56,38 +57,41 @@ func TestForkHandler(t *testing.T) {
 	}
 
 	for _, tc := range cases {
-		client := &github.FakeClient{
-			OpenPullRequests: []*github.PullRequest{tc.pullRequest},
-		}
-		log := log.New(ioutil.Discard, "", 0)
-		jiber := jib.New(log, client, &jib.Config{})
-
-		if err := jiber.Fork(tc.pullRequest); err != nil {
-			t.Fatal(err)
-		}
-
-		if tc.expectedToClose {
-			if len(client.ClosedIssues) != 1 {
-				t.Fatalf("expected to close 1 issue, but %d were closed", len(client.ClosedIssues))
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			client := &github.FakeClient{
+				OpenPullRequests: []*github.PullRequest{tc.pullRequest},
 			}
-			if client.ClosedIssues[0] != tc.pullRequest.Number {
-				t.Fatalf("expected to close issue number %d, but was %d", tc.pullRequest.Number, client.ClosedIssues[0])
-			}
-		} else if len(client.ClosedIssues) != 0 {
-			t.Fatalf("expected to close 0 issues, but %d were closed", len(client.ClosedIssues))
+			log := log.New(ioutil.Discard, "", 0)
+			jiber := jib.New(log, client, &jib.Config{})
 
-		}
-
-		if tc.expectedReplyComment != nil {
-			if len(client.PostedComments) != 1 {
-				t.Fatalf("expected 1 comment to be posted, but %d were posted", len(client.PostedComments))
-			}
-
-			if err := assertIssueReplyMatches(tc.expectedReplyComment, client.PostedComments[0]); err != nil {
+			if err := jiber.Fork(tc.pullRequest); err != nil {
 				t.Fatal(err)
 			}
-		} else if len(client.PostedComments) != 0 {
-			t.Fatalf("expected 0 comments to be posted, but %d were posted", len(client.PostedComments))
-		}
+
+			if tc.expectedToClose {
+				if len(client.ClosedIssues) != 1 {
+					t.Fatalf("expected to close 1 issue, but %d were closed", len(client.ClosedIssues))
+				}
+				if client.ClosedIssues[0] != tc.pullRequest.Number {
+					t.Fatalf("expected to close issue number %d, but was %d", tc.pullRequest.Number, client.ClosedIssues[0])
+				}
+			} else if len(client.ClosedIssues) != 0 {
+				t.Fatalf("expected to close 0 issues, but %d were closed", len(client.ClosedIssues))
+
+			}
+
+			if tc.expectedReplyComment != nil {
+				if len(client.PostedComments) != 1 {
+					t.Fatalf("expected 1 comment to be posted, but %d were posted", len(client.PostedComments))
+				}
+
+				if err := assertIssueReplyMatches(tc.expectedReplyComment, client.PostedComments[0]); err != nil {
+					t.Fatal(err)
+				}
+			} else if len(client.PostedComments) != 0 {
+				t.Fatalf("expected 0 comments to be posted, but %d were posted", len(client.PostedComments))
+			}
+		})
 	}
 }
