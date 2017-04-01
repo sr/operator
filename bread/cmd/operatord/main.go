07b08e5111cc4e886569a038cf03eb21dd202586
Simon Rozet <srozet@salesforce.com>
2017-04-01 20:59:05 +0200

    operatord: Remove dev mode

--- bread/cmd/operatord/main.go
+++ bread/cmd/operatord/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -17,7 +16,6 @@ import (
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/sr/operator"
 	operatorhipchat "github.com/sr/operator/hipchat"
-	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
 	"git.dev.pardot.com/Pardot/infrastructure/bread"
@@ -31,10 +29,6 @@ const (
 
 type config struct {
 	canoe *breadapi.CanoeConfig
-
-	dev             bool
-	devRoomID       int
-	devHipchatToken string
 
 	grpcAddr string
 	halAddr  string
@@ -61,9 +55,6 @@ func run(invoker operator.InvokerFunc) error {
 	flags.StringVar(&config.halAddr, "addr-hal9000", "", "Address of the HAL9000 gRPC server")
 	flags.StringVar(&config.httpAddr, "addr-http", ":8080", "Listen address of the HipChat addon and webhook HTTP server")
 	flags.DurationVar(&config.timeout, "timeout", 10*time.Minute, "Timeout for gRPC requests")
-	flags.BoolVar(&config.dev, "dev", false, "Enable development mode")
-	flags.IntVar(&config.devRoomID, "dev-room-id", bread.TestingRoom, "Room ID where to send messages")
-	flags.StringVar(&config.devHipchatToken, "dev-hipchat-token", "", "HipChat user token")
 	flags.StringVar(&config.ldap.Addr, "ldap-addr", "localhost:389", "Address of the LDAP server used to authenticate and authorize commands")
 	flags.StringVar(&config.ldap.Base, "ldap-base", bread.LDAPBase, "LDAP Base DN")
 	flags.StringVar(&config.databaseURL, "database-url", "", "database/sql connection string to the database where OAuth credentials are stored")
@@ -118,57 +109,36 @@ func run(invoker operator.InvokerFunc) error {
 		return err
 	}
 	canoeAPI = bread.NewCanoeClient(canoeURL, config.canoe.APIKey)
-	if config.dev {
-		auth = &noopAuthorizer{}
-		if config.devRoomID == 0 {
-			return errors.New("dev mode enabled but required flag missing: dev-room-id")
-		}
-		if v, ok := os.LookupEnv("HIPCHAT_TOKEN"); ok && config.devHipchatToken == "" {
-			config.devHipchatToken = v
-		}
-		if config.devHipchatToken == "" {
-			return errors.New("dev mode enabled but required flag missing: dev-hipchat-token")
-		}
-		cl, err := bread.NewHipchatClient(&operatorhipchat.ClientConfig{
-			Hostname: bread.HipchatHost,
-			Token:    config.devHipchatToken,
-		})
-		if err != nil {
-			return err
-		}
-		sender = &devSender{client: cl, roomID: config.devRoomID}
-	} else {
-		if config.httpAddr == "" {
-			return fmt.Errorf("required flag missing: addr-http")
-		}
-		if config.prefix == "" {
-			return fmt.Errorf("required flag missing: prefix")
-		}
-		if config.hipchatNamespace == "" {
-			return fmt.Errorf("required flag missing: hipchat-namespace")
-		}
-		if config.hipchatAddonURL == "" {
-			return fmt.Errorf("required flag missing: hipchat-addon-url")
-		}
-		if config.hipchatWebhookURL == "" {
-			return fmt.Errorf("required flag missing: hipchat-webhook-url")
-		}
-		if config.databaseURL == "" {
-			return fmt.Errorf("required flag missing: database-url")
-		}
-		db, err = sql.Open("mysql", config.databaseURL)
-		if err != nil {
-			return err
-		}
-		if err := db.Ping(); err != nil {
-			return err
-		}
-		httpServer.Handle("/_ping", bread.NewHandler(logger, bread.NewPingHandler(db)))
-		store = operatorhipchat.NewSQLStore(db, bread.HipchatHost)
-		sender = operatorhipchat.NewSender(store, bread.HipchatHost)
-		if auth, err = bread.NewLDAPAuthorizer(config.ldap, canoeAPI, bread.ACL); err != nil {
-			return err
-		}
+	if config.httpAddr == "" {
+		return fmt.Errorf("required flag missing: addr-http")
+	}
+	if config.prefix == "" {
+		return fmt.Errorf("required flag missing: prefix")
+	}
+	if config.hipchatNamespace == "" {
+		return fmt.Errorf("required flag missing: hipchat-namespace")
+	}
+	if config.hipchatAddonURL == "" {
+		return fmt.Errorf("required flag missing: hipchat-addon-url")
+	}
+	if config.hipchatWebhookURL == "" {
+		return fmt.Errorf("required flag missing: hipchat-webhook-url")
+	}
+	if config.databaseURL == "" {
+		return fmt.Errorf("required flag missing: database-url")
+	}
+	db, err = sql.Open("mysql", config.databaseURL)
+	if err != nil {
+		return err
+	}
+	if err := db.Ping(); err != nil {
+		return err
+	}
+	httpServer.Handle("/_ping", bread.NewHandler(logger, bread.NewPingHandler(db)))
+	store = operatorhipchat.NewSQLStore(db, bread.HipchatHost)
+	sender = operatorhipchat.NewSender(store, bread.HipchatHost)
+	if auth, err = bread.NewLDAPAuthorizer(config.ldap, canoeAPI, bread.ACL); err != nil {
+		return err
 	}
 
 	var grpcServer *grpc.Server
@@ -200,58 +170,56 @@ func run(invoker operator.InvokerFunc) error {
 		}
 		httpServer.Handle("/replication/", bread.NewHandler(logger, newRepfixHandler(hal)))
 	}
-	if !config.dev {
-		var webhookHandler http.Handler
-		conn, err := grpc.Dial(
-			config.grpcAddr,
-			grpc.WithBlock(),
-			grpc.WithTimeout(grpcTimeout),
-			grpc.WithInsecure(),
-		)
-		if err != nil {
-			return err
-		}
-		const pkg = "bread"
-		if webhookHandler, err = bread.NewHipchatHandler(
-			logger,
-			operatorhipchat.NewRequestDecoder(store),
-			sender,
-			conn,
-			invoker,
-			grpcServer.GetServiceInfo(),
-			hal,
-			config.timeout,
-			config.prefix,
-			pkg,
-		); err != nil {
-			return err
-		}
-		addonURL, err := url.Parse(config.hipchatAddonURL)
-		if err != nil {
-			return err
-		}
-		webhookURL, err := url.Parse(config.hipchatWebhookURL)
-		if err != nil {
-			return err
-		}
-		httpServer.Handle(
-			"/hipchat/addon",
-			bread.NewHandler(
-				logger,
-				operatorhipchat.NewAddonHandler(
-					store,
-					&operatorhipchat.AddonConfig{
-						Namespace:     config.hipchatNamespace,
-						URL:           addonURL,
-						Homepage:      bread.RepoURL,
-						WebhookURL:    webhookURL,
-						WebhookPrefix: config.prefix,
-					},
-				),
-			),
-		)
-		httpServer.Handle("/hipchat/webhook", bread.NewHandler(logger, webhookHandler))
+	var webhookHandler http.Handler
+	conn, err := grpc.Dial(
+		config.grpcAddr,
+		grpc.WithBlock(),
+		grpc.WithTimeout(grpcTimeout),
+		grpc.WithInsecure(),
+	)
+	if err != nil {
+		return err
 	}
+	const pkg = "bread"
+	if webhookHandler, err = bread.NewHipchatHandler(
+		logger,
+		operatorhipchat.NewRequestDecoder(store),
+		sender,
+		conn,
+		invoker,
+		grpcServer.GetServiceInfo(),
+		hal,
+		config.timeout,
+		config.prefix,
+		pkg,
+	); err != nil {
+		return err
+	}
+	addonURL, err := url.Parse(config.hipchatAddonURL)
+	if err != nil {
+		return err
+	}
+	webhookURL, err := url.Parse(config.hipchatWebhookURL)
+	if err != nil {
+		return err
+	}
+	httpServer.Handle(
+		"/hipchat/addon",
+		bread.NewHandler(
+			logger,
+			operatorhipchat.NewAddonHandler(
+				store,
+				&operatorhipchat.AddonConfig{
+					Namespace:     config.hipchatNamespace,
+					URL:           addonURL,
+					Homepage:      bread.RepoURL,
+					WebhookURL:    webhookURL,
+					WebhookPrefix: config.prefix,
+				},
+			),
+		),
+	)
+	httpServer.Handle("/hipchat/webhook", bread.NewHandler(logger, webhookHandler))
 	if config.httpAddr != "" {
 		logger.Printf("http server listening on %s", config.httpAddr)
 		go func() {
@@ -266,30 +234,4 @@ func main() {
 		fmt.Fprintf(os.Stderr, "operatord: %s\n", err)
 		os.Exit(1)
 	}
-}
-
-type noopAuthorizer struct{}
-
-func (a *noopAuthorizer) Authorize(_ context.Context, _ *bread.RPC, _ string) error {
-	return nil
-}
-
-type devSender struct {
-	client operatorhipchat.Client
-	roomID int
-}
-
-func (r *devSender) Send(ctx context.Context, src *operator.Source, rep string, msg *operator.Message) error {
-	notif := &operatorhipchat.RoomNotification{RoomID: int64(r.roomID)}
-	if msg.HTML != "" {
-		notif.MessageFormat = "html"
-		notif.Message = msg.HTML
-	} else {
-		notif.MessageFormat = "text"
-		notif.Message = msg.Text
-	}
-	if v, ok := msg.Options.(*operatorhipchat.MessageOptions); ok {
-		notif.MessageOptions = v
-	}
-	return r.client.SendRoomNotification(ctx, notif)
 }
